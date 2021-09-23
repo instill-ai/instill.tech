adae53a2f6d5b3fec442da983138c414b760905e
totuslink <summerbud@totuslink.com>
2021-09-23 16:46:54 +0800

    🎉 feat(page): add career main page

---
+++ pages/career/index.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import CustomLink from '../../components/CustomLink';
+import CareerPositionList from '../../components/lists/CareerPositionList';
+import { getInstillOpenPositions } from '../../lib/airtable';
+import { AirtablePositionRecord } from '../../types/airtable';
+
+interface Props {
+  positions: AirtablePositionRecord[];
+}
+
+const CareerPage: FC<Props> = ({ positions }) => {
+  return (
+    <div className="flex flex-col">
+      <h1>Instill AI Careers</h1>
+      <CareerPositionList positions={positions} />
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const positions = await getInstillOpenPositions();
+  return {
+    props: {
+      positions,
+    },
+  };
+};
+
+export default CareerPage;
