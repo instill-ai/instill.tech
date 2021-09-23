7061475424921a869bdf1e098d5a6cb87e870803
totuslink <eric525282@gmail.com>
2021-09-23 22:06:12 +0800

    feat(component): add Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react';
+import CustomLink from './CustomLink';
+
+interface Props {}
+
+const Footer: FC<Props> = () => {
+  return (
+    <div className="flex flex-row h-20 px-6 font-sans font-normal text-gray-700">
+      <div className="flex flex-row gap-x-4 mr-auto my-auto">
+        <CustomLink href={'/privacy'}>
+          <div>Privacy</div>
+        </CustomLink>
+        <div>|</div>
+        <CustomLink href={'mailto:hello@instill.tech'}>
+          <div>Contact</div>
+        </CustomLink>
+        <div>|</div>
+        <CustomLink href={'https://eepurl.com/hfHl6D'}>
+          <div>Newsletter</div>
+        </CustomLink>
+        <div>|</div>
+        <CustomLink href={'/career'}>
+          <div>Career</div>
+        </CustomLink>
+      </div>
+      <div className="ml-auto my-auto">© 2021 Instill AI Ltd. All rights reserved.</div>
+    </div>
+  );
+};
+
+export default Footer;
