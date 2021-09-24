67f398ba936926aba2049eb0658869ae9eb4c212
totuslink <eric525282@gmail.com>
2021-09-24 09:29:01 +0800

    fix(component): fix simple nav bar name

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,7 @@ import InstillLogo from './InstillLogo';
 
 interface Props {}
 
-const SimpleNavBar: FC<Props> = () => {
+const NavBar: FC<Props> = () => {
   return (
     <div className="sticky top-0 z-50 flex flex-row w-full h-20 px-6">
       <div className="mr-auto my-auto">
@@ -19,4 +19,4 @@ const SimpleNavBar: FC<Props> = () => {
   );
 };
 
-export default SimpleNavBar;
+export default NavBar;
--- stories/SimpleNavbar.stories.tsx
+++ stories/SimpleNavbar.stories.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import SimpleNavBar from '../components/SimpleNavBar';
+import NavBar from '../components/NavBar';
 
 export default {
   title: 'Components/Simple navbar',
-  component: SimpleNavBar,
-} as ComponentMeta<typeof SimpleNavBar>;
+  component: NavBar,
+} as ComponentMeta<typeof NavBar>;
 
-const Template: ComponentStory<typeof SimpleNavBar> = (args) => <SimpleNavBar {...args} />;
+const Template: ComponentStory<typeof NavBar> = (args) => <NavBar {...args} />;
 
 export const Light = Template.bind({});
