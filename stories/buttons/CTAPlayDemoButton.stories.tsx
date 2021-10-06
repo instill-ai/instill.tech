f54a7c572d4074d74cc4a119c4cc0aba86f82a67
pochun-chiu <pochun.chiu@instill.tech>
2021-10-06 22:24:48 +0800

    feat: add CTAPlayDemoButton component

---
+++ stories/buttons/CTAPlayDemoButton.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import CTAPlayDemoButton from '../../components/buttons/CTAPlayDemoButton';
+
+export default {
+  title: 'Components/Button/CTAPlayDemoButton',
+  component: CTAPlayDemoButton,
+} as ComponentMeta<typeof CTAPlayDemoButton>;
+
+const Template: ComponentStory<typeof CTAPlayDemoButton> = (args) => <CTAPlayDemoButton {...args} />;
+
+export const Default = Template.bind({});
