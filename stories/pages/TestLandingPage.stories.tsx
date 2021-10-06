994d92ce0f5543013a5edfcb14fb8974be6d30f2
pochun-chiu <pochun.chiu@instill.tech>
2021-10-06 20:00:29 +0800

    feat: add landing page test page

---
+++ stories/pages/TestLandingPage.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import TestLandingPage from '../../pages/test-landing-page';
+
+export default {
+  title: 'Pages/TestLandingPage/index',
+  component: TestLandingPage,
+
+} as ComponentMeta<typeof TestLandingPage>;
+
+const Template: ComponentStory<typeof TestLandingPage> = (args) => <TestLandingPage {...args} />;
+
+export const Default = Template.bind({});
