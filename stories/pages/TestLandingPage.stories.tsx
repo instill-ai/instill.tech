8a63aade5b8b117803b20904dc58c829dd9fa852
pochun-chiu <pochun.chiu@instill.tech>
2021-10-06 23:31:53 +0800

    fix: fix code style

--- stories/pages/TestLandingPage.stories.tsx
+++ stories/pages/TestLandingPage.stories.tsx
@@ -5,7 +5,6 @@ import TestLandingPage from '../../pages/test-landing-page';
 export default {
   title: 'Pages/TestLandingPage/index',
   component: TestLandingPage,
-
 } as ComponentMeta<typeof TestLandingPage>;
 
 const Template: ComponentStory<typeof TestLandingPage> = (args) => <TestLandingPage {...args} />;
