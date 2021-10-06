fa4673edbaebc56c5153c2c648b84eeb891f50af
pochun-chiu <pochun.chiu@instill.tech>
2021-10-06 10:51:43 +0800

    feat: add HeroAnimation story

---
+++ stories/animations/HeroAnimation.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import HeroAnimation from '../../components/animations/HeroAnimation';
+
+export default {
+  title: 'Components/Animation/HeroAnimation',
+  component: HeroAnimation,
+} as ComponentMeta<typeof HeroAnimation>;
+
+const Template: ComponentStory<typeof HeroAnimation> = (args) => (
+  <div className="flex bg-[#063477]">
+    <HeroAnimation {...args} />
+  </div>
+);
+
+export const light = Template.bind({});
