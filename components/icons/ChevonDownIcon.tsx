9b32981edc10a2b58e283f057bfb2bfbfbebacaf
pochun-chiu <pochun.chiu@instill.tech>
2021-10-06 14:57:40 +0800

    fix: fix fill-rule to fillRule

--- components/icons/ChevonDownIcon.tsx
+++ components/icons/ChevonDownIcon.tsx
@@ -10,7 +10,7 @@ const ChevonDownIcon: FC<Props> = ({ className }) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" className={iconStyle}>
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
       />
     </svg>
