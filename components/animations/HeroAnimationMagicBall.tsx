84ea00a3b80ee813d41f5cf16e43ac5720db6b5a
pochun-chiu <pochun.chiu@instill.tech>
2021-10-06 13:12:37 +0800

    fix: refacor HeroAnimationMagicBall code style

--- components/animations/HeroAnimationMagicBall.tsx
+++ components/animations/HeroAnimationMagicBall.tsx
@@ -1,11 +1,11 @@
-import { FC } from "react";
+import { FC } from 'react';
 import MagicBallSVG from '../../assets/animation/hero-animation-magic-ball.svg';
 
 interface Props {}
 
 const HeroAnimationMagicBall: FC<Props> = () => {
   return (
-    <div style={{ gridArea: "1/1", zIndex: 3 }}>
+    <div style={{ gridArea: '1/1', zIndex: 3 }}>
       <MagicBallSVG />
       <style global jsx>{`
         .MagicBall {
@@ -37,7 +37,7 @@ const HeroAnimationMagicBall: FC<Props> = () => {
         }
       `}</style>
     </div>
-  )
-}
+  );
+};
 
-export default HeroAnimationMagicBall
+export default HeroAnimationMagicBall;
