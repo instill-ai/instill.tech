ea9318570c5fe490544b2cfc4e343fb84d09d830
pochun-chiu <pochun.chiu@instill.tech>
2021-10-08 15:15:33 +0800

    fix: fix react/jsx-closing-tag-location lint issue

--- components/animations/HeroAnimationMagicBall.tsx
+++ components/animations/HeroAnimationMagicBall.tsx
@@ -7,35 +7,37 @@ const HeroAnimationMagicBall: FC<Props> = () => {
   return (
     <div style={{ gridArea: '1/1', zIndex: 3 }}>
       <MagicBallSVG />
-      <style global jsx>{`
-        .MagicBall {
-          animation: MagicballAnimation 2s linear 1s infinite;
-          -webkit-animation: MagicballAnimation 2s linear 1s infinite;
-        }
+      <style global jsx>
+        {`
+          .MagicBall {
+            animation: MagicballAnimation 2s linear 1s infinite;
+            -webkit-animation: MagicballAnimation 2s linear 1s infinite;
+          }
 
-        @keyframes MagicballAnimation {
-          0% {
-            cy: 327.5px;
+          @keyframes MagicballAnimation {
+            0% {
+              cy: 327.5px;
+            }
+            50% {
+              cy: 320px;
+            }
+            100% {
+              cy: 327.5px;
+            }
           }
-          50% {
-            cy: 320px;
+          @-webkit-keyframes MagicballAnimation {
+            0% {
+              cy: 327.5px;
+            }
+            50% {
+              cy: 320px;
+            }
+            100% {
+              cy: 327.5px;
+            }
           }
-          100% {
-            cy: 327.5px;
-          }
-        }
-        @-webkit-keyframes MagicballAnimation {
-          0% {
-            cy: 327.5px;
-          }
-          50% {
-            cy: 320px;
-          }
-          100% {
-            cy: 327.5px;
-          }
-        }
-      `}</style>
+        `}
+      </style>
     </div>
   );
 };
