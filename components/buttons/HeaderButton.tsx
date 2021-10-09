3ed80ec9b3dd1ccb611434f4d5f495a104ec2dba
pochun-chiu <pochun.chiu@instill.tech>
2021-10-09 21:50:36 +0800

    fix: refactor button components to follow new code style

--- components/buttons/HeaderButton.tsx
+++ components/buttons/HeaderButton.tsx
@@ -1,18 +1,16 @@
-import * as React from 'react';
-import ChevonDownIcon from '../icons/ChevonDownIcon';
+import * as React from "react";
+import { ChevonDownIcon } from "../icons/ChevonDownIcon";
 
 interface Props {
   withChevon: boolean;
   name: string;
 }
 
-const HeaderButton: React.FC<Props> = ({ withChevon, name }) => {
+export const HeaderButton: React.FC<Props> = ({ withChevon, name }) => {
   return (
     <button className="flex flex-row gap-x-2">
       <span className="font-sans text-base text-gray-500">{name}</span>
-      {withChevon && <ChevonDownIcon className={'w-4 h-4 text-gray-500 my-auto'} />}
+      {withChevon && <ChevonDownIcon styleName={"w-4 h-4 text-gray-500 my-auto"} />}
     </button>
   );
 };
-
-export default HeaderButton;
--- stories/buttons/GeneralLinkButton.stories.tsx
+++ stories/buttons/GeneralLinkButton.stories.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import GeneralLinkButton from '../../components/buttons/GeneralLinkButton';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { GeneralLinkButton } from "../../components/buttons/GeneralLinkButton";
 
 export default {
-  title: 'Components/Button/GeneralLinkButton',
+  title: "Components/Button/GeneralLinkButton",
   component: GeneralLinkButton,
 } as ComponentMeta<typeof GeneralLinkButton>;
 
-const Template: ComponentStory<typeof GeneralLinkButton> = (args) => <GeneralLinkButton {...args} />;
+const Template: ComponentStory<typeof GeneralLinkButton> = (args) => (
+  <GeneralLinkButton {...args} />
+);
 
 export const light = Template.bind({});
 
 light.args = {
   title: "View roles",
-  href: "/open-roles"
-}
+  href: "/open-roles",
+};
