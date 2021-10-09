cc53c59c26595007720f2ff985e1a614af39aaae
pochun-chiu <pochun.chiu@instill.tech>
2021-10-09 22:13:01 +0800

    fix: refactor components to follow new code style

--- lib/airtable/index.ts
+++ lib/airtable/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { InstillPosition, AirtablePositionRecord } from "../../types/airtable";
+import { AirtablePositionRecord } from "../../types/airtable";
 
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 const AIRTABLE_CAREER_BASE_ID = process.env.AIRTABLE_CAREER_BASE_ID;
@@ -7,14 +7,14 @@ const AIRTABLE_CRREER_TABLE_TITLE = process.env.AIRTABLE_CRREER_TABLE_TITLE;
 export const airtableCareerTableUrl = `https://api.airtable.com/v0/${AIRTABLE_CAREER_BASE_ID}/${AIRTABLE_CRREER_TABLE_TITLE}?api_key=${AIRTABLE_API_KEY}`;
 
 interface AirtableRecords {
-  records: AirtablePositionRecord[]
+  records: AirtablePositionRecord[];
 }
 
 export const getInstillOpenPositions = async (): Promise<AirtablePositionRecord[]> => {
   try {
-    const res = await axios.get<AirtableRecords>(airtableCareerTableUrl)
-    return Promise.resolve(res.data.records)
-  } catch(err){
-    return Promise.reject(err)
+    const res = await axios.get<AirtableRecords>(airtableCareerTableUrl);
+    return Promise.resolve(res.data.records);
+  } catch (err) {
+    return Promise.reject(err);
   }
 };
