516c73a34b0a2651b9f32761c86ae998229aed6b
pochun-chiu <pochun.chiu@instill.tech>
2021-10-09 21:50:36 +0800

    fix: refactor button components to follow new code style

--- components/buttons/CTAPlayDemoButton.tsx
+++ components/buttons/CTAPlayDemoButton.tsx
@@ -1,15 +1,13 @@
 import { FC } from "react";
-import PlayIcon from "../icons/PlayIcon";
+import { PlayIcon } from "../icons/PlayIcon";
 
 interface Props {}
 
-const CTAPlayDemoButton: FC<Props> = () => {
+export const CTAPlayDemoButton: FC<Props> = () => {
   return (
     <button className="flex flex-row gap-x-3 px-7 py-4 rounded-lg border border-[#D0D5DD]">
       <PlayIcon />
       <span className="font-sans text-[#344054] font-medium text-lg">Demo</span>
     </button>
-  )
-}
-
-export default CTAPlayDemoButton
+  );
+};
