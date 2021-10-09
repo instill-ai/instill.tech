a88bc433eca3944b95f9cb01d2230856e29ae069
pochun-chiu <pochun.chiu@instill.tech>
2021-10-09 22:41:55 +0800

    fix: refactor SubHeadline component to follow new code style

--- components/SubHeadline.tsx
+++ components/SubHeadline.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import * as classNames from 'classnames';
+import { FC } from "react";
+import * as classNames from "classnames";
 
 interface Props {
   subHeadlineText: string;
@@ -10,7 +10,7 @@ export const SubHeadline: FC<Props> = ({ subHeadlineText, styleName }) => {
   return (
     <h2
       className={classNames.default(
-        'mx-auto font-sans font-normal text-center text-xl text-[#667085] max-w-3xl',
+        "mx-auto font-sans font-normal text-center text-xl text-[#667085] max-w-3xl",
         styleName
       )}
     >
