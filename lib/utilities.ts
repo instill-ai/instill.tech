e250340bae4ae3684dcd605180ba1b21f3bf41d5
pochun-chiu <pochun.chiu@instill.tech>
2021-10-14 11:42:00 +0800

    feat: make color won't be duplicated in the same generated block

--- lib/utilities.ts
+++ lib/utilities.ts
@@ -2,3 +2,12 @@
 export const getRandomInt = (max: number): number => {
   return Math.floor(Math.random() * max);
 };
+
+export const getNonDuplicatedRandomInt = (max: number, oldValue: number): number => {
+  const newValue = Math.floor(Math.random() * max);
+  if (newValue !== oldValue){
+    return newValue
+  }
+
+  return getNonDuplicatedRandomInt(max, oldValue)
+}
