01afc52972a4b4ae0d61926ed54da459f5db7bcd
pochun-chiu <pochun.chiu@instill.tech>
2021-10-14 10:57:39 +0800

    feat: add rect generator type

---
+++ types/generator.ts
@@ -0,0 +1,10 @@
+export interface RectGeneratorInfo {
+  baseStrokeWidth: number;
+  baseStrokeColor: string;
+  colorArray: string[];
+  blockSize: number;
+  blockTypeCount: number;
+  rowCount: number;
+  columnCount: number;
+  canvasPadding: number;
+}
