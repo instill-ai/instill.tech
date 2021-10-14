ad1af199f838e23c2966fa4de2add2524f4af83e
pochun-chiu <pochun.chiu@instill.tech>
2021-10-14 11:28:45 +0800

    feat: add horizontal block component and its story

---
+++ components/generators/HorizontalRectBlock.tsx
@@ -0,0 +1,23 @@
+import { FC, useEffect } from "react";
+import {
+  constructHorizontalRectBlock,
+  prepareCanvas,
+} from "../../lib/generator/rect-block-generator";
+import { rectGeneratorInfo } from "./RectGenerator";
+
+interface Props {}
+
+export const HorizontalRectBlock: FC<Props> = () => {
+  const id = "generator-canvas-horizontal-rect-block";
+  useEffect(() => {
+    const { ctx } = prepareCanvas(id, rectGeneratorInfo);
+    constructHorizontalRectBlock(
+      ctx,
+      rectGeneratorInfo.canvasPadding,
+      rectGeneratorInfo.canvasPadding,
+      rectGeneratorInfo
+    );
+  }, []);
+
+  return <canvas id={id}></canvas>;
+};
