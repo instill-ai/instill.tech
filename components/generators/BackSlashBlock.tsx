1cbf9b54453be36cb786f3a9cb88d9ebd2d7ff47
pochun-chiu <pochun.chiu@instill.tech>
2021-10-14 11:18:55 +0800

    feat: add back slash block and its story

---
+++ components/generators/BackSlashBlock.tsx
@@ -0,0 +1,24 @@
+import { FC, useEffect } from "react";
+import {
+  constructBackslashTriangleBlock,
+  prepareCanvas,
+} from "../../lib/generator/rect-block-generator";
+import { rectGeneratorInfo } from "./RectGenerator";
+
+interface Props {}
+
+export const BackSlashBlock: FC<Props> = () => {
+  const id = "generator-canvas-back-slash-block";
+
+  useEffect(() => {
+    const { ctx } = prepareCanvas(id, rectGeneratorInfo);
+    constructBackslashTriangleBlock(
+      ctx,
+      rectGeneratorInfo.canvasPadding,
+      rectGeneratorInfo.canvasPadding,
+      rectGeneratorInfo
+    );
+  }, []);
+
+  return <canvas id={id}></canvas>;
+};
