ac793a44f03ab6076edd2ac3ea9b881cb7c7e21e
pochun-chiu <pochun.chiu@instill.tech>
2021-10-14 11:31:09 +0800

    feat: add vertical block component and its story

---
+++ components/generators/VerticalRectBlock.tsx
@@ -0,0 +1,23 @@
+import { FC, useEffect } from "react";
+import {
+  constructVerticalRectBlock,
+  prepareCanvas,
+} from "../../lib/generator/rect-block-generator";
+import { rectGeneratorInfo } from "./RectGenerator";
+
+interface Props {}
+
+export const VerticalRectBlock: FC<Props> = () => {
+  const id = "generator-canvas-vertical-rect-block";
+  useEffect(() => {
+    const { ctx } = prepareCanvas(id, rectGeneratorInfo);
+    constructVerticalRectBlock(
+      ctx,
+      rectGeneratorInfo.canvasPadding,
+      rectGeneratorInfo.canvasPadding,
+      rectGeneratorInfo
+    );
+  }, []);
+
+  return <canvas id={id}></canvas>;
+};
