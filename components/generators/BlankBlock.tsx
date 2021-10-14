a6b9c2d27fc2a993eca215bd494d327f7015763e
pochun-chiu <pochun.chiu@instill.tech>
2021-10-14 11:25:49 +0800

    feat: add blank block component and its story

---
+++ components/generators/BlankBlock.tsx
@@ -0,0 +1,20 @@
+import { FC, useEffect } from "react";
+import { constructBlankBlock, prepareCanvas } from "../../lib/generator/rect-block-generator";
+import { rectGeneratorInfo } from "./RectGenerator";
+
+interface Props {}
+
+export const BlankBlock: FC<Props> = () => {
+  const id = "generator-canvas-blank-block";
+  useEffect(() => {
+    const { ctx } = prepareCanvas(id, rectGeneratorInfo);
+    constructBlankBlock(
+      ctx,
+      rectGeneratorInfo.canvasPadding,
+      rectGeneratorInfo.canvasPadding,
+      rectGeneratorInfo
+    );
+  }, []);
+
+  return <canvas id={id}></canvas>;
+};
---
+++ stories/generators/BlankBlock.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { BlankBlock } from "../../components/generators/BlankBlock";
+
+export default {
+  title: "Components/generator/BlankBlock",
+  component: BlankBlock,
+} as ComponentMeta<typeof BlankBlock>;
+
+const Template: ComponentStory<typeof BlankBlock> = (args) => <BlankBlock {...args} />;
+
+export const Default = Template.bind({});
