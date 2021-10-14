4b5ff7f1c8f88b7926b1821d6d863ef837820954
pochun-chiu <pochun.chiu@instill.tech>
2021-10-14 11:13:20 +0800

    feat: add slash block and its story

---
+++ stories/generators/SlashBlock.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { SlashBlock } from "../../components/generators/SlashBlock";
+
+export default {
+  title: "Components/generator/SlashBlock",
+  component: SlashBlock,
+} as ComponentMeta<typeof SlashBlock>;
+
+const Template: ComponentStory<typeof SlashBlock> = (args) => <SlashBlock {...args} />;
+
+export const Default = Template.bind({});
