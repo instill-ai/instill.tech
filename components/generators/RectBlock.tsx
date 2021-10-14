8acdbbb38f6fc668e08b0ad2e6012729fcdff310
pochun-chiu <pochun.chiu@instill.tech>
2021-10-14 10:57:58 +0800

    fix: refactor function into dedicated file

--- components/generators/RectBlock.tsx
+++ components/generators/RectBlock.tsx
@@ -1,17 +1,26 @@
 import { FC, useEffect } from "react";
+import {
+  generateTargetMetric,
+  getCanvasElementById,
+  getCanvasRendering2DContext,
+  switchBlockConstructor,
+} from "../../lib/generator/rect-block-generator";
+import { RectGeneratorInfo } from "../../types/generator";
 
 interface Props {}
 
-const blockSize = 200;
-const blockTypeAmount = 5;
-const baseStrokeColor = "#000000";
-const baseStrokeWidth = 4;
-const colorNum = 6;
-const rowSize = 3;
-const columnSize = 3;
+const rectGeneratorInfo: RectGeneratorInfo = {
+  blockSize: 200,
+  blockTypeCount: 5,
+  baseStrokeColor: "#000000",
+  baseStrokeWidth: 4,
+  rowCount: 3,
+  columnCount: 3,
+  canvasPadding: 10,
+  colorArray: ["#FFFF1A", "#FCB21B", "#27FC86", "#3EEDFF", "#F75FFF", "#C65AFF"],
+};
 
-
-const color = {
+export const RectGeneratorColor = {
   yellowSun: "#FFFF1A",
   brightYellow: "#FCB21B",
   guppieGreen: "#27FC86",
@@ -20,261 +29,31 @@ const color = {
   heliotrope: "#C65AFF",
 };
 
-const colorArray = ["#FFFF1A", "#FCB21B", "#27FC86", "#3EEDFF", "#F75FFF", "#C65AFF"];
-
 export const RectBlock: FC<Props> = () => {
   useEffect(() => {
     const canvas: HTMLCanvasElement = getCanvasElementById("generator-canvas");
-
-    console.log(generateTargetMetric(rowSize, columnSize))
-
-    canvas.width = 610;
-    canvas.height = 610;
+    const metric = generateTargetMetric(rectGeneratorInfo);
+    canvas.width =
+      rectGeneratorInfo.blockSize * rectGeneratorInfo.columnCount +
+      rectGeneratorInfo.canvasPadding * 2;
+    canvas.height =
+      rectGeneratorInfo.blockSize * rectGeneratorInfo.rowCount +
+      rectGeneratorInfo.canvasPadding * 2;
 
     const ctx: CanvasRenderingContext2D = getCanvasRendering2DContext(canvas);
 
-    let randNum: number = getRandomInt(6);
-    let x;
-    let y;
-
-    const newPoint = constructSlashTriangleBlock(ctx, 5, 5);
-    x = newPoint.x;
-    y = newPoint.y;
-
-    const newpoint2 = constructHorizontalRectBlock(ctx, x, y);
-    x = newpoint2.x;
-    y = newpoint2.y;
-
-    constructBackslashTriangleBlock(ctx, x, y);
-
-    // ctx.fillStyle = colorArray[randNum];
-    // console.log(colorArray[randNum]);
-    // ctx.fillRect(5, 5, 100, 200);
-
-    // ctx.lineWidth = 4;
-    // ctx.strokeStyle = "#000000";
-    // ctx.strokeRect(5, 5, 100, 200);
-
-    // randNum = getRandomInt(6);
-
-    // ctx.fillStyle = colorArray[randNum];
-    // console.log(colorArray[randNum]);
-    // ctx.fillRect(105, 5, 100, 200);
-
-    // ctx.lineWidth = 4;
-    // ctx.strokeStyle = "#000000";
-    // ctx.strokeRect(105, 5, 100, 200);
-
-    // randNum = getRandomInt(6);
-    // ctx.fillStyle = colorArray[randNum];
-    // ctx.beginPath();
-    // ctx.moveTo(205, 5);
-    // ctx.lineTo(205, 205);
-    // ctx.lineTo(405, 205);
-    // ctx.lineTo(205, 5);
-    // ctx.fill();
+    for (let i = 0; i < metric.length; i++) {
+      const row = metric[i];
+      let x = rectGeneratorInfo.canvasPadding;
+      let y = i * rectGeneratorInfo.blockSize + rectGeneratorInfo.canvasPadding;
+      for (let j = 0; j < row.length; j++) {
+        switchBlockConstructor(row[j], ctx, x, y, rectGeneratorInfo);
+        if (j !== row.length - 1) {
+          x = rectGeneratorInfo.blockSize * (j + 1) + rectGeneratorInfo.canvasPadding;
+        }
+      }
+    }
   }, []);
 
   return <canvas id="generator-canvas"></canvas>;
-};
-
-const getCanvasElementById = (id: string): HTMLCanvasElement => {
-  const canvas = document.getElementById(id);
-
-  if (!(canvas instanceof HTMLCanvasElement)) {
-    throw new Error(
-      `The element of id "${id}" is not a HTMLCanvasElement. Make sure a <canvas id="${id}""> element is present in the document.`
-    );
-  }
-
-  return canvas;
-};
-
-const getCanvasRendering2DContext = (canvas: HTMLCanvasElement): CanvasRenderingContext2D => {
-  const context = canvas.getContext("2d");
-
-  if (context === null) {
-    throw new Error("This browser does not support 2-dimensional canvas rendering contexts.");
-  }
-
-  return context;
-};
-
-const getRandomInt = (max: number): number => {
-  // If max = 2, it will return 0 or 1
-  return Math.floor(Math.random() * max);
-};
-
-const constructVerticalRectBlock = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
-  const width = blockSize / 2;
-  const height = blockSize;
-
-  // Build fillRect first to make sure stroke won't cover by fillRect
-  let randNum: number = getRandomInt(colorNum - 1);
-  ctx.fillStyle = colorArray[randNum];
-  ctx.fillRect(x, y, width, height);
-
-  randNum = getRandomInt(colorNum - 1);
-  ctx.fillStyle = colorArray[randNum];
-  ctx.fillRect(x + width, y, width, height);
-
-  // Build strokeRect
-
-  ctx.lineWidth = baseStrokeWidth;
-  ctx.strokeStyle = baseStrokeColor;
-  ctx.strokeRect(x, y, width, height);
-
-  ctx.lineWidth = baseStrokeWidth;
-  ctx.strokeStyle = baseStrokeColor;
-  ctx.strokeRect(x + width, y, width, height);
-
-  return {
-    x: x + blockSize,
-    y: y,
-  };
-};
-
-const constructHorizontalRectBlock = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
-  const width = blockSize;
-  const height = blockSize / 2;
-
-  let randNum: number = getRandomInt(colorNum - 1);
-  ctx.fillStyle = colorArray[randNum];
-  ctx.fillRect(x, y, width, height);
-
-  randNum = getRandomInt(colorNum - 1);
-  ctx.fillStyle = colorArray[randNum];
-  ctx.fillRect(x, y + height, width, height);
-
-  ctx.lineWidth = baseStrokeWidth;
-  ctx.strokeStyle = baseStrokeColor;
-  ctx.strokeRect(x, y, width, height);
-
-  ctx.lineWidth = baseStrokeWidth;
-  ctx.strokeStyle = baseStrokeColor;
-  ctx.strokeRect(x, y + height, width, height);
-
-  return {
-    x: x + blockSize,
-    y: y,
-  };
-};
-
-const constructBackslashTriangleBlock = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
-  const width = blockSize;
-  const height = blockSize;
-
-  // In order to avoid fillrect cover stroke, we draw fillRect then strokeRect
-
-  let randNum: number = getRandomInt(colorNum - 1);
-  ctx.fillStyle = colorArray[randNum];
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(x, y + height);
-  ctx.lineTo(x + width, y + height);
-  ctx.fill();
-
-  randNum = getRandomInt(colorNum - 1);
-  ctx.fillStyle = colorArray[randNum];
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(x + width, y);
-  ctx.lineTo(x + width, y + height);
-  ctx.fill();
-
-  ctx.lineWidth = baseStrokeWidth;
-  ctx.strokeStyle = baseStrokeColor;
-  ctx.beginPath();
-  ctx.lineJoin = "round";
-  ctx.moveTo(x, y);
-  ctx.lineTo(x, y + height);
-  ctx.lineTo(x + width, y + height);
-  ctx.lineTo(x, y);
-  ctx.stroke();
-
-  ctx.lineWidth = baseStrokeWidth;
-  ctx.strokeStyle = baseStrokeColor;
-  ctx.beginPath();
-  ctx.lineJoin = "round";
-  ctx.moveTo(x, y);
-  ctx.lineTo(x + width, y);
-  ctx.lineTo(x + width, y + height);
-  ctx.moveTo(x, y);
-  ctx.stroke();
-};
-
-const constructSlashTriangleBlock = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
-  const width = blockSize;
-  const height = blockSize;
-
-  let randNum: number = getRandomInt(colorNum - 1);
-  ctx.fillStyle = colorArray[randNum];
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(x, y + height);
-  ctx.lineTo(x + width, y);
-  ctx.fill();
-
-  randNum = getRandomInt(colorNum - 1);
-  ctx.fillStyle = colorArray[randNum];
-  ctx.beginPath();
-  ctx.moveTo(x, y + height);
-  ctx.lineTo(x + width, y + height);
-  ctx.lineTo(x + width, y);
-  ctx.fill();
-
-  ctx.lineWidth = baseStrokeWidth;
-  ctx.strokeStyle = baseStrokeColor;
-  ctx.beginPath();
-  ctx.lineJoin = "round";
-  ctx.moveTo(x, y);
-  ctx.lineTo(x, y + height);
-  ctx.lineTo(x + width, y);
-  ctx.lineTo(x, y);
-  ctx.stroke();
-
-  ctx.lineWidth = baseStrokeWidth;
-  ctx.strokeStyle = baseStrokeColor;
-  ctx.beginPath();
-  ctx.lineJoin = "round";
-  ctx.moveTo(x, y + height);
-  ctx.lineTo(x + width, y + height);
-  ctx.lineTo(x + width, y);
-  ctx.lineTo(x, y + height);
-  ctx.stroke();
-
-  return {
-    x: x + blockSize,
-    y: y,
-  };
-};
-
-const constructBlankBLock = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
-  const width = blockSize;
-  const height = blockSize;
-
-  let randNum: number = getRandomInt(colorNum - 1);
-  ctx.fillStyle = colorArray[randNum];
-  ctx.fillRect(x, y, width, height);
-
-  ctx.lineWidth = baseStrokeWidth;
-  ctx.strokeStyle = baseStrokeColor;
-  ctx.strokeRect(x, y, width, height);
-
-  return {
-    x: x + blockSize,
-    y: y,
-  };
-};
-
-const generateTargetMetric = (rowSize: number, columnSize: number) => {
-  let row = [];
-  for (let i = 0; i < rowSize; i++) {
-    let column = [];
-    for (let j = 0; j < columnSize; j++) {
-      column[j] = getRandomInt(blockTypeAmount);
-    }
-    row[i] = column;
-  }
-  return row
 };
