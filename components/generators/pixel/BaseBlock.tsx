58ea2908a0fd17e2499b8b3d365e8eb12b39e287
pochun-chiu <pochun.chiu@instill.tech>
2021-10-16 15:37:07 +0800

    fix: refactor common script into dedicated file

--- components/generators/pixel/BaseBlock.tsx
+++ components/generators/pixel/BaseBlock.tsx
@@ -1,5 +1,5 @@
 import { FC, useEffect } from "react";
-import { prepareCanvas } from "../../../lib/generator/rect-block-generator";
+import { prepareCanvas } from "../../../lib/generator/common";
 import { GeneratorInfo } from "../../../types/generator";
 
 interface Props {}
@@ -13,18 +13,18 @@ const generatorInfo: GeneratorInfo = {
   columnCount: 3,
   canvasPadding: 10,
   colorArray: ["#FFFF1A", "#FCB21B", "#27FC86", "#3EEDFF", "#F75FFF", "#C65AFF"],
-}
+};
 
 export const BaseBlock: FC<Props> = () => {
   const id = "pixel-base-block";
 
   useEffect(() => {
-    const { ctx } = prepareCanvas(id, generatorInfo)
-  }, [])
+    const { ctx } = prepareCanvas(id, generatorInfo);
+  }, []);
 
   return (
     <div>
       <canvas id={id}></canvas>
     </div>
-  )
-}
+  );
+};
