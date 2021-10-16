acd4bfacb0a41db5f208ec081c3683933af605a4
pochun-chiu <pochun.chiu@instill.tech>
2021-10-16 16:39:01 +0800

    refactor: refactor generatorInfo prop into generator file

--- components/generators/pixel/BaseBlock.tsx
+++ components/generators/pixel/BaseBlock.tsx
@@ -1,33 +1,25 @@
 import { FC, useEffect } from "react";
 import { prepareCanvas } from "../../../lib/generator/common";
-import { constructPixelBaseBlockAllRandom } from "../../../lib/generator/pixel-block-generator";
-import { GeneratorInfo } from "../../../types/generator";
+import {
+  constructPixelBlockOutline,
+  constructPixelBaseBlockAllRandom,
+} from "../../../lib/generator/pixel-block-generator";
+import { generatorInfo } from "./DiagramGenerator";
 
 interface Props {}
-
-const generatorInfo: GeneratorInfo = {
-  blockSize: 300,
-  blockTypeCount: 5,
-  baseStrokeColor: "#000000",
-  baseStrokeWidth: 4,
-  rowCount: 3,
-  columnCount: 3,
-  canvasPadding: 10,
-  colorArray: ["#FFFF1A", "#FCB21B", "#27FC86", "#3EEDFF", "#F75FFF", "#C65AFF"],
-};
 
 export const BaseBlock: FC<Props> = () => {
   const id = "pixel-base-block";
 
   useEffect(() => {
     const { ctx } = prepareCanvas(id, generatorInfo);
-
-    constructPixelBaseBlockAllRandom(ctx, generatorInfo);
+    constructPixelBaseBlockAllRandom(ctx, generatorInfo, 10, 10);
+    constructPixelBlockOutline(ctx, generatorInfo, 10, 10);
   }, []);
 
   return (
     <div>
-      <canvas id={id}></canvas>
+      <canvas id={id} />
     </div>
   );
 };
