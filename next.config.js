f215abee1f7dc2f422f2b7ca685ffa2860148539
pochun-chiu <pochun.chiu@instill.tech>
2022-01-13 11:43:59 +0800

    feat: remove webpack bundle analyzer when in dev

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
 module.exports = {
   swcMinify: false, // replacing Terser for minification
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
+  webpack: (config, { isServer, dev }) => {
     // if (isServer) {
     //   require("./lib/generate-sitemap");
     // }
 
-    const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-    config.plugins.push(
-      new BundleAnalyzerPlugin({
-        analyzerMode: "disabled",
-        generateStatsFile: true,
-        reportFilename: isServer ? "../analyze/server.json" : "./analyze/client.json",
-      })
-    );
+    if (!dev) {
+      const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: "disabled",
+          generateStatsFile: true,
+          reportFilename: isServer ? "../analyze/server.json" : "./analyze/client.json",
+        })
+      );
+    }
 
     return config;
   },
