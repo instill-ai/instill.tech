cbd4f9c57674a0e64ab01544fb626bd9f468daa0
pochun-chiu <pochun.chiu@instill.tech>
2022-01-13 14:55:24 +0800

    refactor: deprecated tailwind darkMode config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./stories/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
