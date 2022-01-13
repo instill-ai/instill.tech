ec217636f95725ba918a0ee341834a4e00734368
pochun-chiu <pochun.chiu@instill.tech>
2022-01-13 13:47:58 +0800

    feat: remove all the old files

--- components/ui/buttons/GetAccessButton.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-interface Props {}
-
-export const GetAccessButton: FC<Props> = () => {
-  return <button className="px-6 py-2 border border-gray-700 rounded-md">Get access</button>;
-};
