0a2633564457143e58ac6713c983d2e8948f49f0
pochun-chiu <pochun.chiu@instill.tech>
2022-01-13 11:41:54 +0800

    feat: disable secMinify and setup react strict mode

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
 module.exports = {
+  swcMinify: false, // replacing Terser for minification
+  reactStrictMode: true,
   webpack: (config, { isServer }) => {
     // if (isServer) {
     //   require("./lib/generate-sitemap");
     // }
 
-    const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+    const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
     config.plugins.push(
       new BundleAnalyzerPlugin({
-        analyzerMode: 'disabled',
+        analyzerMode: "disabled",
         generateStatsFile: true,
-        reportFilename: isServer ? '../analyze/server.json' : './analyze/client.json',
+        reportFilename: isServer ? "../analyze/server.json" : "./analyze/client.json",
       })
     );
 
     return config;
   },
-}
+};
