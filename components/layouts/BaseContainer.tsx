35c9b7c6de7914203da25792b4d99b56ce3f1429
pochun-chiu <pochun.chiu@instill.tech>
2022-01-14 09:33:07 +0800

    feat: refactor: refactor container dimension

--- components/layouts/BaseContainer.tsx
+++ components/layouts/BaseContainer.tsx
@@ -3,5 +3,5 @@ import { FC } from "react";
 interface Props {}
 
 export const BaseContainer: FC<Props> = ({ children }) => {
-  return <div className="w-screen min-h-screen">{children}</div>;
+  return <div className="flex flex-col min-h-screen">{children}</div>;
 };
--- components/layouts/SectionContainer.tsx
+++ components/layouts/SectionContainer.tsx
@@ -3,5 +3,5 @@ import { FC } from "react";
 interface Props {}
 
 export const SectionContainer: FC<Props> = ({ children }) => {
-  return <div className="lg:px-28">{children}</div>;
+  return <div className="flex flex-1 max-w-[1440px] md:w-10/12 md:mx-auto">{children}</div>;
 };
