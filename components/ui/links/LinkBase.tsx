c40f853af8d42e7b2fb54ed65f87fbd4b3c2c83f
pochun-chiu <pochun.chiu@instill.tech>
2022-01-15 17:40:56 +0800

    refactor: rename CustomLink to LinkBase

--- components/ui/links/LinkBase.tsx
+++ components/ui/links/LinkBase.tsx
@@ -3,21 +3,33 @@ import { FC } from "react";
 
 interface Props {
   href: string;
+  styleName?: string;
 }
 
-export const CustomLink: FC<Props> = ({ href, children, ...props }) => {
+export const LinkBase: FC<Props> = ({
+  href,
+  children,
+  styleName,
+  ...props
+}) => {
   const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"));
 
   if (isInternalLink) {
     return (
       <Link href={href} {...props}>
-        {children}
+        <div className={styleName}>{children}</div>
       </Link>
     );
   }
 
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" {...props}>
+    <a
+      className={styleName}
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      {...props}
+    >
       {children}
     </a>
   );
