49bff33567a4bbe319c89bb0f91333e93950683e
pochun-chiu <pochun.chiu@instill.tech>
2022-01-15 20:34:41 +0800

    feat: add props comment and refactor styleName structure

--- components/ui/links/DiscordLink.tsx
+++ components/ui/links/DiscordLink.tsx
@@ -1,16 +1,18 @@
 import { FC } from "react";
 import { DiscordIcon } from "../icons/DiscordIcon";
 import { LinkBase } from "./LinkBase";
+import * as classNames from "classnames";
 
 interface Props {
+  /** Color and width - tailwindCSS based, default: w-4 text-instillGray30 */
   styleName?: string;
   href: string;
 }
 
 export const DiscordLink: FC<Props> = ({ href, styleName }) => {
-  const style = styleName ? styleName : "flex w-4 text-instillGray30";
+  const style = styleName ? styleName : "w-4 text-instillGray30";
   return (
-    <LinkBase styleName={style} href={href}>
+    <LinkBase styleName={classNames.default("flex", style)} href={href}>
       <DiscordIcon />
     </LinkBase>
   );
--- components/ui/links/MediumLink.tsx
+++ components/ui/links/MediumLink.tsx
@@ -1,16 +1,18 @@
 import { FC } from "react";
 import { MediumIcon } from "../icons/MediumIcon";
 import { LinkBase } from "./LinkBase";
+import * as classNames from "classnames";
 
 interface Props {
+  /** Color and width - tailwindCSS based, default: w-4 text-instillGray30 */
   styleName?: string;
   href: string;
 }
 
 export const MediumLink: FC<Props> = ({ styleName, href }) => {
-  const style = styleName ? styleName : "flex w-4 text-instillGray30";
+  const style = styleName ? styleName : "w-4 text-instillGray30";
   return (
-    <LinkBase styleName={style} href={href}>
+    <LinkBase styleName={classNames.default("flex", style)} href={href}>
       <MediumIcon />
     </LinkBase>
   );
