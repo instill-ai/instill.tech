0295ff85deaa19b764e52c357996778215b1936b
pochun-chiu <pochun.chiu@instill.tech>
2022-01-15 22:08:24 +0800

    feat: add styleName prop into SocialLinksGroup

--- components/ui/SocialLinksGroup.tsx
+++ components/ui/SocialLinksGroup.tsx
@@ -2,12 +2,15 @@ import { FC } from "react";
 import { DiscordLink } from "./links/DiscordLink";
 import { GithubLink } from "./links/GithubLink";
 import { MediumLink } from "./links/MediumLink";
+import * as classNames from "classnames";
 
-interface Props {}
+interface Props {
+  styleName?: string;
+}
 
-export const SocialLinksGroup: FC<Props> = () => {
+export const SocialLinksGroup: FC<Props> = ({ styleName }) => {
   return (
-    <div className="flex flex-row justify-center gap-x-5">
+    <div className={classNames.default("flex flex-row gap-x-5", styleName)}>
       <GithubLink href="https://github.com/instill-ai" />
       <DiscordLink href="https://discord.gg/Rnn58CebTK" />
       <MediumLink href="" />
