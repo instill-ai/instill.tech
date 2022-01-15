cae9febd5faa9f5d78f2a79cf4fc3a3e3b63a2fc
pochun-chiu <pochun.chiu@instill.tech>
2022-01-15 17:30:04 +0800

    feat: add CustomLink

---
+++ components/CustomLink.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { FC } from "react";
+
+interface Props {
+  href: string;
+}
+
+export const CustomLink: FC<Props> = ({ href, children, ...props }) => {
+  const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"));
+
+  if (isInternalLink) {
+    return (
+      <Link href={href} {...props}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer" {...props}>
+      {children}
+    </a>
+  );
+};
