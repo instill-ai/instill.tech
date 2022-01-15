e1a517af4521a090f869182bee3cafb464dce61e
pochun-chiu <pochun.chiu@instill.tech>
2022-01-15 22:09:08 +0800

    feat: add SubscribeEmailButton

---
+++ components/ui/buttons/SubscribeEmailButton.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import { ButtonBase } from "./ButtonBase";
+
+interface Props {
+  onClick: () => void;
+  styleName?: string;
+}
+
+export const SubscribeEmailButton: FC<Props> = ({ onClick, styleName }) => {
+  return (
+    <ButtonBase
+      onClick={onClick}
+      styleName={styleName}
+      type="submit"
+      variant="contained"
+      color="primary"
+    >
+      Subscribe
+    </ButtonBase>
+  );
+};
