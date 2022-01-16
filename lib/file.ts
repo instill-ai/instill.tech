8fb13462dc1f14468e511598fbb9a5a5e74b5629
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 17:25:55 +0800

    feat: add onClink function into ButtonBase

--- lib/file.ts
+++ lib/file.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import * as path from "path";
 import matter from "gray-matter";
 
-export const getMarkDownFileContent = (filePath: string): string => {
+export const getMDFileContent = (filePath: string): string => {
   const fullPath = path.resolve(process.cwd(), "markdown", filePath);
 
   const fileContents = fs.readFileSync(fullPath, "utf-8");
