4f3e963f734db5813ff0451f1bb1783c98a0c397
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 15:22:46 +0800

    feat: make ReactMDWrapper responsive

--- components/ReactMDWrapper.tsx
+++ components/ReactMDWrapper.tsx
@@ -1,17 +1,19 @@
 import ReactMarkdown from "react-markdown";
+import * as classNames from "classnames";
 
 import { FC } from "react";
 
 interface Props {
+  styleName?: string;
   content: string;
 }
 
-const ReactMarkdownWrapper: FC<Props> = ({ content }) => {
+export const ReactMDWrapper: FC<Props> = ({ content, styleName }) => {
   return (
-    <article className="prose">
+    <article
+      className={classNames.default("prose prose-sm md:prose-lg", styleName)}
+    >
       <ReactMarkdown>{content}</ReactMarkdown>
     </article>
   );
 };
-
-export default ReactMarkdownWrapper;
