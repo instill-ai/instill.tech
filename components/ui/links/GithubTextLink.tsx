4556557eccf1fb531310377872c54ee93e8da675
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 22:34:27 +0800

    feat: separate GithubLink to GithubTextLink and GithubIconLink

--- components/ui/links/GithubTextLink.tsx
+++ components/ui/links/GithubTextLink.tsx
@@ -1,20 +1,21 @@
 import { FC } from "react";
-import { GithubIcon } from "../icons/GithubIcon";
 import { LinkBase } from "./LinkBase";
 import * as classNames from "classnames";
 
 interface Props {
   /** Color and width - tailwindCSS based, default: w-4 text-instillGray30 */
   styleName?: string;
-  href: string;
 }
 
-export const GithubLink: FC<Props> = ({ styleName, href }) => {
+export const GithubTextLink: FC<Props> = ({ styleName }) => {
   const style = styleName ? styleName : "w-4 text-instillGray30";
 
   return (
-    <LinkBase styleName={classNames.default("flex", style)} href={href}>
-      <GithubIcon />
+    <LinkBase
+      styleName={classNames.default("flex", style)}
+      href="https://github.com/instill-ai"
+    >
+      <p className="my-auto">Github</p>
     </LinkBase>
   );
 };
