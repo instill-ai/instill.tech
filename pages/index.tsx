cd2e5688664219153c4281bfc42fa203907fb61a
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 16:45:37 +0800

    feat: add PageHead into index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Headline } from "../components/ui/Headline";
 import { MainCtaGroup } from "../components/ui/MainCtaGroup";
 import { SubHeadline } from "../components/ui/SubHeadline";
 import { PageBase } from "../components/layouts/PageBase";
+import { PageHead } from "../components/layouts/PageHead";
 
 interface Props {}
 
@@ -15,12 +16,15 @@ const Home: FC<Props> & {
   getLayout?: FC<GetLayOutProps>;
 } = () => {
   return (
-    <>
+    <PageHead
+      pageTitle="Where visual data preparation made for all | Instill AI"
+      pageDescription="Empower modern data stack, tapping the value of unstructured visual data with our open source community."
+    >
       <Headline styleName="mb-5 px-[18px] md:px-0 md:max-w-[800px]" />
       <SubHeadline styleName="mb-10 px-[18px] md:px-0 md:max-w-[800px]" />
       <MainCtaGroup styleName="mb-[100px] sm:mb-[240px]" />
       <SubscriptionEmailForm styleName="mb-[60px]" />
-    </>
+    </PageHead>
   );
 };
 
