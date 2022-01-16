bdba5e0ee25b4e6ccc913e7ae399bd731ae80985
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 11:08:38 +0800

    feat: add ga tag manager into _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,6 +39,23 @@ export const Document: FC<Props> = () => {
           sizes="16x16"
           href="/favicon-16x16.png"
         />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async={true}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <body>
         <Main />
