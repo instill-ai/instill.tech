1b6b28ac54534124f87963323e1340d0640905f7
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 21:32:37 +0800

    feat: add medium link

--- components/ui/links/MediumLink.tsx
+++ components/ui/links/MediumLink.tsx
@@ -6,13 +6,15 @@ import * as classNames from "classnames";
 interface Props {
   /** Color and width - tailwindCSS based, default: w-4 text-instillGray30 */
   styleName?: string;
-  href: string;
 }
 
-export const MediumLink: FC<Props> = ({ styleName, href }) => {
+export const MediumLink: FC<Props> = ({ styleName }) => {
   const style = styleName ? styleName : "w-4 text-instillGray30";
   return (
-    <LinkBase styleName={classNames.default("flex", style)} href={href}>
+    <LinkBase
+      styleName={classNames.default("flex", style)}
+      href="https://medium.com/instill-ai"
+    >
       <MediumIcon />
     </LinkBase>
   );
