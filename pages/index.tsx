d7666ea52ef83cc9bec9e25875ab47e849dfab9c
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 14:02:01 +0800

    refactor: refactor landing page with getLayut method

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,31 @@
-import { FC } from "react";
+import { FC, ReactElement } from "react";
+import { SubscriptionEmailForm } from "../components/forms/SubscriptionEmailForm";
+import { Headline } from "../components/ui/Headline";
+import { MainCtaGroup } from "../components/ui/MainCtaGroup";
+import { SubHeadline } from "../components/ui/SubHeadline";
+import { PageBase } from "../components/layouts/PageBase";
+
 interface Props {}
 
-const Home: FC<Props> = () => {
-  return <div className="bg-[#063477]" />;
+interface GetLayOutProps {
+  page: ReactElement;
+}
+
+const Home: FC<Props> & {
+  getLayout?: FC<GetLayOutProps>;
+} = () => {
+  return (
+    <>
+      <Headline styleName="mb-5 px-[18px] md:px-0 md:max-w-[800px]" />
+      <SubHeadline styleName="mb-10 px-[18px] md:px-0 md:max-w-[800px]" />
+      <MainCtaGroup styleName="mb-[100px] sm:mb-[240px]" />
+      <SubscriptionEmailForm styleName="mb-[60px]" />
+    </>
+  );
+};
+
+Home.getLayout = (page) => {
+  return <PageBase>{page}</PageBase>;
 };
 
 export default Home;
