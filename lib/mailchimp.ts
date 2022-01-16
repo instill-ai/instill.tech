faaa15aa17a681f0be2a4eb0d368ac290813f1d7
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 10:26:14 +0800

    refactor: rename subscribe to mailchimp and refactor the env variables

--- lib/mailchimp.ts
+++ lib/mailchimp.ts
@@ -5,20 +5,24 @@ mailchimp.setConfig({
   server: process.env.NEXT_PUBLIC_MAILCHIMP_API_SERVER, // e.g. us1
 });
 
-export const subscribe = async (email: string) => {
+export const addMemberIntoMailchimpList = async (email: string) => {
   if (!email) {
     return Promise.reject("EmailIsRequired");
   }
 
   try {
-    await mailchimp.lists.addListMember(process.env.MAILCHIMP_LIST_ID, {
-      email_address: email,
-    });
+    await mailchimp.lists.addListMember(
+      process.env.NEXT_PUBLIC_MAILCHIMP_LIST_ID,
+      {
+        email_address: email,
+      }
+    );
 
     return Promise.resolve("Success");
   } catch (error) {
-    if (error.response.body.title === "Member Exists") {
-      return Promise.reject("MemberExisted");
+    console.log(error);
+    if (error.response && error.response.body.title === "Member Exists") {
+      return Promise.reject("MemberExists");
     }
     return Promise.reject(error.message || error.toString());
   }
