5ada5cff00156d4736fc22b63f09817894a8705a
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 16:47:59 +0800

    refactor: remove PrivatePolicyLink href prop

--- components/ui/links/PrivacyPolicyLink.tsx
+++ components/ui/links/PrivacyPolicyLink.tsx
@@ -5,13 +5,12 @@ import * as classNames from "classnames";
 interface Props {
   /** textSize and width - tailwindCSS based, default: instill-text-body text-instillGray30 */
   styleName?: string;
-  href: string;
 }
 
-export const PrivacyPolicyLink: FC<Props> = ({ styleName, href }) => {
+export const PrivacyPolicyLink: FC<Props> = ({ styleName }) => {
   const style = styleName ? styleName : "instill-text-body text-instillGray30";
   return (
-    <LinkBase href={href} styleName={classNames.default("flex", style)}>
+    <LinkBase href="/privacy" styleName={classNames.default("flex", style)}>
       Privacy policy
     </LinkBase>
   );
