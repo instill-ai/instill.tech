b5d47414501bc1f78b07fc0e1c774cda86a4ad8a
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 19:21:09 +0800

    feat: add subscribe endpoint and delete hello endpoint

---
+++ pages/api/subscribe.ts
@@ -0,0 +1,30 @@
+import mailchimp from "@mailchimp/mailchimp_marketing";
+import { NextApiRequest, NextApiResponse } from "next";
+
+mailchimp.setConfig({
+  apiKey: process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY,
+  server: process.env.NEXT_PUBLIC_MAILCHIMP_API_SERVER, // e.g. us1
+});
+
+export const subscribe = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(500).json({ error: "Email is required" });
+  }
+
+  try {
+    await mailchimp.lists.addListMember(process.env.MAILCHIMP_LIST_ID, {
+      email_address: email,
+    });
+
+    return res.status(201).json({ error: "" });
+  } catch (error) {
+    if (error.response && error.response.body.title === "Member Exists") {
+      return res.status(500).json({ error: "MemberExists" });
+    }
+    return res.status(500).json({ error: error.message || error.toString() });
+  }
+};
+
+export default subscribe;
