2be24fc407bc431c12ecd9457848907f78a77c9f
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 14:01:10 +0800

    refactor: refactor app base to utilize getLayout method

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
+import { NextPage } from "next";
+import { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
 import "../styles/global.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return <>{getLayout(<Component {...pageProps} />)}</>;
 }
 
 export default MyApp;
