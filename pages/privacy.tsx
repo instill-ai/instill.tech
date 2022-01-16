754bfb4d3fd63168c8f3721e768a1e4aa4209484
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 16:09:38 +0800

    feat: add privacy page

---
+++ pages/privacy.tsx
@@ -0,0 +1,42 @@
+import { GetStaticProps } from "next";
+import { FC, ReactElement } from "react";
+import { PageBase } from "../components/layouts/PageBase";
+import { PageHead } from "../components/layouts/PageHead";
+import { ReactMDWrapper } from "../components/ReactMDWrapper";
+import { getMDFileContent } from "../lib/file";
+
+interface Props {
+  content: string;
+}
+
+interface GetLayOutProps {
+  page: ReactElement;
+}
+
+const CookiePage: FC<Props> & {
+  getLayout?: FC<GetLayOutProps>;
+} = ({ content }) => {
+  return (
+    <PageHead
+      pageTitle="Privacy policy | Instill AI"
+      pageDescription="Instill AI's cookie policy."
+    >
+      <ReactMDWrapper styleName="mx-5 md:mx-0 mb-[60px]" content={content} />
+    </PageHead>
+  );
+};
+
+CookiePage.getLayout = (page) => {
+  return <PageBase>{page}</PageBase>;
+};
+
+export default CookiePage;
+
+export const getStaticProps: GetStaticProps = () => {
+  const content = getMDFileContent("privacy.md");
+  return {
+    props: {
+      content,
+    },
+  };
+};
