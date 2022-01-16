d90c5c93a539f4f0f44f4aad01f53063f6f2dd20
pochun-chiu <pochun.chiu@instill.tech>
2022-01-17 00:39:55 +0800

    fix: fix button icon style issue

--- components/ui/SocialLinksGroup.tsx
+++ components/ui/SocialLinksGroup.tsx
@@ -11,9 +11,9 @@ interface Props {
 export const SocialLinksGroup: FC<Props> = ({ styleName }) => {
   return (
     <div className={classNames.default("flex flex-row gap-x-5", styleName)}>
-      <GithubIconLink styleName="w-6 sm:w-4 text-instillGray30 hover:text-instillGray05" />
-      <DiscordLink styleName="w-6 sm:w-4 text-instillGray30 hover:text-instillGray05" />
-      <MediumLink styleName="w-6 sm:w-4 text-instillGray30 hover:text-instillGray05" />
+      <GithubIconLink iconStyle="w-6 h-6 sm:w-4 sm:h-6 text-instillGray30 hover:text-instillGray05" />
+      <DiscordLink iconStyle="w-6 h-6 sm:w-4 sm:h-6 text-instillGray30 hover:text-instillGray05" />
+      <MediumLink iconStyle="w-6 h-6 sm:w-4 sm:h-6 text-instillGray30 hover:text-instillGray05" />
     </div>
   );
 };
