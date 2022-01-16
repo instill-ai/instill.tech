6a6a0a76828a8beafd81249cba1aac61fe3a0748
pochun-chiu <pochun.chiu@instill.tech>
2022-01-16 11:25:26 +0800

    feat: add ga event

--- components/ui/buttons/JoinDiscordButton.tsx
+++ components/ui/buttons/JoinDiscordButton.tsx
@@ -1,15 +1,22 @@
+import { useRouter } from "next/router";
 import { FC } from "react";
 import { DiscordIcon } from "../icons/DiscordIcon";
 import { ButtonBase } from "./ButtonBase";
+import * as ga from "../../../lib/google-analytic";
 
 interface Props {}
 
 export const JoinDiscordButton: FC<Props> = () => {
+  const router = useRouter();
   return (
     <ButtonBase
       variant="contained"
       color="white"
       startIcon={<DiscordIcon styleName="w-5" />}
+      onClick={() => {
+        ga.eventHelpers.engagement("join_discord");
+        router.push("");
+      }}
     >
       Join our Discord
     </ButtonBase>
