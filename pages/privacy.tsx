3587944e09b2677f4a56440a6dde14e470368971
pochun-chiu <pochun.chiu@instill.tech>
2022-01-17 20:18:43 +0800

    feat: add PolicyPageLayout with mininal sticky get in touch component

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -2,7 +2,7 @@ import { GetStaticProps } from "next";
 import { FC, ReactElement } from "react";
 import { PageBase } from "../components/layouts/PageBase";
 import { PageHead } from "../components/layouts/PageHead";
-import { ReactMDWrapper } from "../components/ReactMDWrapper";
+import { PolicyPageLayout } from "../components/layouts/PolicyPageLayout";
 import { getMDFileContent } from "../lib/file";
 
 interface Props {
@@ -21,10 +21,7 @@ const CookiePage: FC<Props> & {
       pageTitle="Privacy policy | Instill AI"
       pageDescription="Instill AI's cookie policy."
     >
-      <ReactMDWrapper
-        styleName="mx-5 md:mx-0 mt-[180px] mb-[60px]"
-        content={content}
-      />
+      <PolicyPageLayout content={content} />
     </PageHead>
   );
 };
