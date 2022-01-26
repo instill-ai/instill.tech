325d3f6ea356920d649b02ad61e2332bbdb5b5eb
pochun-chiu <pochun.chiu@instill.tech>
2022-01-26 13:58:01 +0800

    feat: add VisualDataPreparationFeatureSvg

---
+++ components/ui/svgs/VisualDataPreparationFeatureSvg.tsx
@@ -0,0 +1,231 @@
+import { FC } from "react";
+import * as classNames from "classnames";
+import { SvgBase } from "./SvgBase";
+
+interface Props {
+  styleName?: string;
+}
+
+export const VisualDataPreparationFeatureSvg: FC<Props> = ({ styleName }) => {
+  return (
+    <SvgBase viewBox="0 0 304 299" styleName={styleName}>
+      <path
+        d="M302.931 74.7637H79.5039V297.932H302.931V74.7637Z"
+        fill="#0066CC"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M296.728 68.5674H73.3008V291.736H296.728V68.5674Z"
+        fill="#0D73D9"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M290.513 62.3594H67.0859V285.528H290.513V62.3594Z"
+        fill="#1A80E6"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M284.31 56.1631H60.8828V279.331H284.31V56.1631Z"
+        fill="#268CF2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M278.095 49.9561H54.668V273.124H278.095V49.9561Z"
+        fill="#3399FF"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M224.498 1.06836H1.07031V224.237H224.498V1.06836Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M90.4454 1.06836H45.7578V45.7042H90.4454V1.06836Z"
+        fill="#FFDF3A"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M135.133 1.06836H90.4453V45.7042H135.133V1.06836Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M179.809 1.06836H135.121V45.7042H179.809V1.06836Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M224.496 1.06836H179.809V45.7042H224.496V1.06836Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M45.7579 1.06836H1.07031V45.7042H45.7579V1.06836Z"
+        fill="#FFDF3A"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M90.4454 45.7041H45.7578V90.3399H90.4454V45.7041Z"
+        fill="#FFDF3A"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M135.133 45.7041H90.4453V90.3399H135.133V45.7041Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M179.809 45.7041H135.121V90.3399H179.809V45.7041Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M224.496 45.7041H179.809V90.3399H224.496V45.7041Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M45.7579 45.7041H1.07031V90.3399H45.7579V45.7041Z"
+        fill="#FFDF3A"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M90.4454 90.3398H45.7578V134.976H90.4454V90.3398Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M135.133 90.3398H90.4453V134.976H135.133V90.3398Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M179.809 90.3398H135.121V134.976H179.809V90.3398Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M224.496 90.3398H179.809V134.976H224.496V90.3398Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M45.7579 90.3398H1.07031V134.976H45.7579V90.3398Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M90.4454 134.965H45.7578V179.601H90.4454V134.965Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M135.133 134.965H90.4453V179.601H135.133V134.965Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M179.809 134.965H135.121V179.601H179.809V134.965Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M224.496 134.965H179.809V179.601H224.496V134.965Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M45.7579 134.965H1.07031V179.601H45.7579V134.965Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M90.4454 179.601H45.7578V224.236H90.4454V179.601Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M135.133 179.601H90.4453V224.236H135.133V179.601Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M179.809 179.601H135.121V224.236H179.809V179.601Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M224.496 179.601H179.809V224.236H224.496V179.601Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M45.7579 179.601H1.07031V224.236H45.7579V179.601Z"
+        fill="#F2F2F2"
+        stroke="black"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+    </SvgBase>
+  );
+};
