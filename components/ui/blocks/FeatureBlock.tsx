60c520bfa08e79e05e7349145c00d0c41706c36a
pochun-chiu <pochun.chiu@instill.tech>
2022-01-26 14:50:45 +0800

    feat: make FeatureBlock responsive

--- components/ui/blocks/FeatureBlock.tsx
+++ components/ui/blocks/FeatureBlock.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from "react";
+import * as classNames from "classnames";
 
 interface Props {
   imagePosition: "right" | "left";
@@ -16,25 +17,42 @@ export const FeatureBlock: FC<Props> = ({
   image,
 }) => {
   const copyBlock = (
-    <div className="flex flex-col w-full h-full">
+    <div className="flex flex-col my-auto w-full h-full">
       <p className="instill-text-body text-instillBlue30 mb-1">{label}</p>
       <h2 className="instill-text-h2 text-instillGray95 mb-[30px]">{title}</h2>
       <p className="instill-text-body text-instillGray95">{description}</p>
     </div>
   );
 
-  const element =
-    imagePosition === "left" ? (
-      <>
-        <div className="col-span-2">{image}</div>
-        <div className="col-span-1">{copyBlock}</div>
-      </>
-    ) : (
-      <>
-        <div className="col-span-1">{copyBlock}</div>
-        <div className="col-span-2">{image}</div>
-      </>
-    );
+  const gridItems = (
+    <>
+      <div
+        className={classNames.default(
+          "flex sm:h-[400px] sm:w-[455px]",
+          imagePosition === "left" ? "" : "sm:col-start-2"
+        )}
+      >
+        {image}
+      </div>
+      <div
+        className={classNames.default(
+          "flex sm:h-[400px] sm:w-[455px]",
+          imagePosition === "left" ? "" : "sm:row-start-1"
+        )}
+      >
+        {copyBlock}
+      </div>
+    </>
+  );
 
-  return <div className="grid grid-cols-1 sm:grid-cols-3">{element}</div>;
+  return (
+    <div
+      className={classNames.default(
+        "flex w-full px-4 py-[30px] sm:h-[560px] sm:p-0",
+        imagePosition === "left" ? "bg-instillGray05" : "bg-white"
+      )}
+    >
+      <div className="grid grid-cols-1 sm:grid-cols-2 m-auto">{gridItems}</div>
+    </div>
+  );
 };
