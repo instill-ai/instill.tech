f045fd4aac43f60d8869b8f4f41d916a4b6f601f
pochun-chiu <pochun.chiu@instill.tech>
2022-01-26 13:44:59 +0800

    feat: add SvgBase component

---
+++ components/ui/svgs/SvgBase.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import * as classNames from "classnames";
+
+interface Props {
+  viewBox: string;
+  styleName?: string;
+}
+
+export const SvgBase: FC<Props> = ({ viewBox, styleName, children }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={viewBox}
+      className={classNames.default("my-auto", styleName)}
+    >
+      {children}
+    </svg>
+  );
+};
