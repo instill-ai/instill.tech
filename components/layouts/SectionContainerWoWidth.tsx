18a756a205e59d5758bbf5075c73791b02da3e74
pochun-chiu <pochun.chiu@instill.tech>
2022-01-26 19:11:59 +0800

    refactor: make PageBase have withMaxWidth props

--- components/layouts/SectionContainerWoWidth.tsx
+++ components/layouts/SectionContainerWoWidth.tsx
@@ -2,6 +2,6 @@ import { FC } from "react";
 
 interface Props {}
 
-export const SectionContainer: FC<Props> = ({ children }) => {
+export const SectionContainerWoWidth: FC<Props> = ({ children }) => {
   return <div className="flex flex-1 flex-col">{children}</div>;
 };
