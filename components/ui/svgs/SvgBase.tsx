dd2335d0ebdf26d90a0999bdaacb89ed290504cb
pochun-chiu <pochun.chiu@instill.tech>
2022-01-27 11:44:58 +0800

    feat: add fill:none in SvgBase

--- components/ui/svgs/SvgBase.tsx
+++ components/ui/svgs/SvgBase.tsx
@@ -11,6 +11,7 @@ export const SvgBase: FC<Props> = ({ viewBox, styleName, children }) => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox={viewBox}
+      fill="none"
       className={classNames.default("my-auto", styleName)}
     >
       {children}
