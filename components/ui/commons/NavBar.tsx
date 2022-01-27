23dec57cba87e00a00403439833ad3697fbff654
pochun-chiu <pochun.chiu@instill.tech>
2022-01-28 00:00:44 +0800

    refactor: refactor NavBar to have maximum 1440px width

--- components/ui/commons/NavBar.tsx
+++ components/ui/commons/NavBar.tsx
@@ -15,42 +15,44 @@ export const NavBar: FC<Props> = () => {
 
   return (
     <div className="flex w-full sm:absolute sm:top-0 sm:z-50">
-      <div className="hidden sm:grid w-full sm:grid-cols-2 p-5 content-center">
-        <LinkBase styleName="my-auto mr-auto" href="/">
-          <InstillAiLogo type="ColourLogoWhiteType" width={159} />
-        </LinkBase>
-
-        <div className="flex flex-row gap-x-[60px] justify-end">
-          <AboutPageLink />
-          <BlogLink />
-          <GithubTextLink />
-          <GetEarlyAccessButton />
-        </div>
-      </div>
-      <div className="flex flex-col w-full p-5 sm:hidden">
-        <div className="flex flex-row w-full mb-[60px]">
-          <LinkBase href="/">
+      <div className="flex w-full max-w-[1440px] mx-auto">
+        <div className="hidden sm:grid w-full sm:grid-cols-2 p-5 content-center">
+          <LinkBase styleName="my-auto mr-auto" href="/">
             <InstillAiLogo type="ColourLogoWhiteType" width={159} />
           </LinkBase>
-          <button
-            className="ml-auto rounded-[3px] hover:bg-instillGray30 hover:bg-opacity-10"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? (
-              <CrossIcon styleName="w-10 text-instillGray05" />
-            ) : (
-              <MenuIcon styleName="w-10 text-instillGray05" />
-            )}
-          </button>
+
+          <div className="flex flex-row gap-x-[60px] justify-end">
+            <AboutPageLink />
+            <BlogLink />
+            <GithubTextLink />
+            <GetEarlyAccessButton />
+          </div>
         </div>
-        <div className="relative">
-          {open && (
-            <div className="absolute top-0 w-full flex flex-col gap-y-10 z-50">
-              <AboutPageLink />
-              <BlogLink />
-              <GithubTextLink />
-            </div>
-          )}
+        <div className="flex flex-col w-full p-5 sm:hidden">
+          <div className="flex flex-row w-full mb-[60px]">
+            <LinkBase href="/">
+              <InstillAiLogo type="ColourLogoWhiteType" width={159} />
+            </LinkBase>
+            <button
+              className="ml-auto rounded-[3px] hover:bg-instillGray30 hover:bg-opacity-10"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? (
+                <CrossIcon styleName="w-10 text-instillGray05" />
+              ) : (
+                <MenuIcon styleName="w-10 text-instillGray05" />
+              )}
+            </button>
+          </div>
+          <div className="relative">
+            {open && (
+              <div className="absolute top-0 w-full flex flex-col gap-y-10 z-50">
+                <AboutPageLink />
+                <BlogLink />
+                <GithubTextLink />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
