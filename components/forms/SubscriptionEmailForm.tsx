ef4a435e4f5695816c8ce4da6403a8c189a126bd
pochun-chiu <pochun.chiu@instill.tech>
2022-01-27 11:51:37 +0800

    feat: add StayInTheLoopBlock

--- components/forms/SubscriptionEmailForm.tsx
+++ components/forms/SubscriptionEmailForm.tsx
@@ -53,19 +53,7 @@ export const SubscriptionEmailForm: FC<Props> = ({ styleName }) => {
   };
 
   return (
-    <form
-      onSubmit={subscribe}
-      className={classNames.default(
-        "grid grid-cols-1 gap-y-[30px] sm:grid-cols-2 sm:gap-y-0 sm:gap-x-8 md:gap-x-16 lg:gap-x-28 px-4",
-        styleName
-      )}
-    >
-      <div className="flex flex-col sm:mr-auto">
-        <h3 className="instill-text-h3 text-instillGray05">Stay in the Loop</h3>
-        <p className="instill-text-body text-instillGray05">
-          Join our mailing list for the latest news and updates.
-        </p>
-      </div>
+    <form onSubmit={subscribe}>
       <div className="flex flex-col w-full gap-y-1 my-auto sm:ml-auto">
         <div className="flex flex-col gap-y-2 sm:gap-y-0">
           <div className="flex flex-row gap-x-2.5 justify-end">
