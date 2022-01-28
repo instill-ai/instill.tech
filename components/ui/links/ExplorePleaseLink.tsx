7222eba8e73c8be4a7834c503f0d9b4d459ee18f
pochun-chiu <pochun.chiu@instill.tech>
2022-01-29 02:54:45 +0800

    refactor: refactor ExplorePleaseLink

---
+++ components/ui/links/ExplorePleaseLink.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import { RightArrowIcon } from "../icons/RightArrowIcon";
+import { ButtonBase } from "../buttons/ButtonBase";
+import { LinkBase } from "./LinkBase";
+import * as classNames from "classnames";
+
+interface Props {
+  styleName?: string;
+}
+
+export const ExplorePleaseLink: FC<Props> = ({ styleName }) => {
+  return (
+    <LinkBase
+      href="#feature-block-group"
+      styleName={classNames.default("flex flex-col group", styleName)}
+    >
+      <p className="mx-auto text-instillBlue30 group-hover:text-[#236698]">
+        Explore
+      </p>
+      <RightArrowIcon styleName="rotate-90 mt-5 mx-auto w-10 h-10 text-instillBlue30 group-hover:text-[#236698]" />
+    </LinkBase>
+  );
+};
