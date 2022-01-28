cbb4bd5de5a94da547f5b37b3914846ed4f6e772
pochun-chiu <pochun.chiu@instill.tech>
2022-01-29 02:14:51 +0800

    feat: rearrange page to make it suite style needs

--- pages/about.tsx
+++ pages/about.tsx
@@ -21,67 +21,65 @@ const AboutPage: FC<Props> & {
       pageTitle="About us | Instill Ai"
       pageDescription="Instill AI, founded in 2020 (June 11th 2020, to be more specific), provides no-/low-code tools to convert unstructured visual data to meaningful structured representations."
     >
-      <div className="flex flex-col max-w-[1440px] md:w-10/12 md:mx-auto">
-        <div className="flex w-full sm:h-[584px]">
-          <h1 className="m-auto instill-text-h1 text-instillGray05 max-w-[934px] text-center">
-            Make Vision AI Accessible to Everyone
-          </h1>
-        </div>
-        <div
-          className={classNames.default(
-            "grid grid-cols-1 gap-y-12 md:grid-rows-2 md:grid-cols-2 md:gap-x-6 md:gap-y-6 mb-[84px]",
-            elementMaxWidth
-          )}
-        >
-          <div className="flex flex-col p-10 gap-y-5 bg-white">
-            <h2 className="instill-text-h2 text-instillGray95 mb-1">
-              Our Company
-            </h2>
-            <p className="instill-text-body text-instillGray95">
-              Instill AI, founded in 2020 (June 11th 2020, to be more specific),
-              provides no-/low-code tools to convert unstructured visual data to
-              meaningful structured representations. Users can integrate our
-              service into their data stack, and start tapping into the wealth
-              of their visual data and benefit from Vision AI in a snap.
-            </p>
-            <p className="instill-text-body text-instillGray95">
-              We are a small and ambitious team of passionate
-              engineers/researchers sharing an unconventional culture fused by
-              DevOps, MLOps and academic research lab. We are hands-on and love
-              to automate everything. We care about our products and deliver to
-              the highest standard.
-            </p>
+      <div className="max-w-[1440px] md:w-10/12 md:mx-auto">
+        <div className="flex flex-col px-4 md:px-0 max:w-10/12 max:mx-auto">
+          <div className="flex w-full pt-[87px] pb-[152px] sm:py-0 sm:h-[584px]">
+            <h1 className="m-auto instill-text-h1 text-instillGray05 max-w-[934px] text-center">
+              Make Vision AI Accessible to Everyone
+            </h1>
           </div>
-          <div className="flex flex-col gap-y-5 bg-white p-10">
-            <h2 className="instill-text-h2 text-instillGray95">Our Value</h2>
-            <div className="flex flex-col">
-              <h3 className="instill-text-h3 text-instillGray95">
-                User obsession
-              </h3>
+          <div className="grid grid-cols-1 px-4 lg:px-0 gap-y-6 lg:grid-rows-2 lg:grid-cols-2 lg:gap-x-6 lg:gap-y-6 mb-[84px]">
+            <div className="flex flex-col p-10 gap-y-5 bg-white rounded-[1px]">
+              <h2 className="instill-text-h2 text-instillGray95 mb-1">
+                Our Company
+              </h2>
               <p className="instill-text-body text-instillGray95">
-                We succeed only when our customers succeed. Learning what can
-                really help our customers and delivering the value they need
-                will always be Instill&#39;s highest interest.
+                Instill AI, founded in 2020 (June 11th 2020, to be more
+                specific), provides no-/low-code tools to convert unstructured
+                visual data to meaningful structured representations. Users can
+                integrate our service into their data stack, and start tapping
+                into the wealth of their visual data and benefit from Vision AI
+                in a snap.
+              </p>
+              <p className="instill-text-body text-instillGray95">
+                We are a small and ambitious team of passionate
+                engineers/researchers sharing an unconventional culture fused by
+                DevOps, MLOps and academic research lab. We are hands-on and
+                love to automate everything. We care about our products and
+                deliver to the highest standard.
               </p>
             </div>
-            <div className="flex flex-col">
-              <h3 className="instill-text-h3 text-instillGray95">
-                High-performing company with shared values
-              </h3>
-              <p className="instill-text-body text-instillGray95">
-                We always deliver more than expected. To achieve that, we are
-                the optimisers obsessively chasing intrinsic efficiency and
-                effectiveness.
-              </p>
+            <div className="flex flex-col gap-y-5 bg-white p-10 rounded-[1px]">
+              <h2 className="instill-text-h2 text-instillGray95">Our Value</h2>
+              <div className="flex flex-col">
+                <h3 className="instill-text-h3 text-instillGray95">
+                  User obsession
+                </h3>
+                <p className="instill-text-body text-instillGray95">
+                  We succeed only when our customers succeed. Learning what can
+                  really help our customers and delivering the value they need
+                  will always be Instill&#39;s highest interest.
+                </p>
+              </div>
+              <div className="flex flex-col">
+                <h3 className="instill-text-h3 text-instillGray95">
+                  High-performing company with shared values
+                </h3>
+                <p className="instill-text-body text-instillGray95">
+                  We always deliver more than expected. To achieve that, we are
+                  the optimisers obsessively chasing intrinsic efficiency and
+                  effectiveness.
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div className="md:col-span-2">
-            <SecureYourSpotBlock />
+            <div className="lg:col-span-2">
+              <SecureYourSpotBlock />
+            </div>
           </div>
-        </div>
-        <div className={elementMaxWidth}>
-          <StayInTheLoopBlock />
+          <div className={elementMaxWidth}>
+            <StayInTheLoopBlock />
+          </div>
         </div>
       </div>
     </PageHead>
