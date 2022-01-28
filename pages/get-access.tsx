d33e75db2244b4697eeb6aedd3631543c25c0178
pochun-chiu <pochun.chiu@instill.tech>
2022-01-29 02:14:51 +0800

    feat: rearrange page to make it suite style needs

--- pages/get-access.tsx
+++ pages/get-access.tsx
@@ -18,28 +18,25 @@ const GetEarlyAccessPage: FC<Props> & {
 
   return (
     <PageHead pageTitle="Get early access | Instill Ai" pageDescription="">
-      <div
-        className={classNames.default(
-          "grid grid-cols-1 lg:grid-cols-2 py-[60px] lg:pt-[244px] lg:pb-[60px] px-4 md:px-0",
-          elementMaxWidth
-        )}
-      >
-        <div className="flex flex-col max-w-[450px] mb-20 lg:mb-0">
-          <div className="flex flex-col gap-y-5 mb-[60px]">
-            <h2 className="instill-text-h2 text-instillGray05">
-              Get Early Access
-            </h2>
-            <p className="instill-text-body text-instillGray05">
-              We&#39;re now in private alpha. Join and see first-hand how
-              Instill AI can help adopt Vision AI in your company.
-            </p>
+      <div className={elementMaxWidth}>
+        <div className="grid grid-cols-1 lg:grid-cols-2 py-[60px] lg:pt-[244px] lg:pb-[60px] px-4 md:px-0 max:mx-auto max:w-10/12">
+          <div className="flex flex-col max-w-[450px] mb-20 lg:mb-0">
+            <div className="flex flex-col gap-y-5 mb-[60px]">
+              <h2 className="instill-text-h2 text-instillGray05">
+                Get Early Access
+              </h2>
+              <p className="instill-text-body text-instillGray05">
+                We&#39;re now in private alpha. Join and see first-hand how
+                Instill AI can help adopt Vision AI in your company.
+              </p>
+            </div>
+            <div className="flex w-full">
+              <AlphaBadgeSvg styleName="max-w-[355px]" />
+            </div>
           </div>
-          <div className="flex w-full">
-            <AlphaBadgeSvg styleName="max-w-[355px]" />
+          <div>
+            <MailchimpSignupForm />
           </div>
-        </div>
-        <div>
-          <MailchimpSignupForm />
         </div>
       </div>
     </PageHead>
