718b7936b94c1959868b43cba3743ab5d75f6948
pochun-chiu <pochun.chiu@instill.tech>
2022-02-22 14:55:26 +0800

    refactor: refactor SocialLinksGroup, add comment

--- components/ui/SocialLinksGroup.tsx
+++ components/ui/SocialLinksGroup.tsx
@@ -8,12 +8,23 @@ interface Props {
   styleName?: string;
 }
 
+/**
+ *
+ * @param styleName - tailwind format style
+ * @returns
+ * - Use styleName to control this component's position and width
+ * - All link elements have fixed style
+ * - Currently links: Github | Discord | Medium
+ */
+
 export const SocialLinksGroup: FC<Props> = ({ styleName }) => {
+  const style =
+    "w-6 h-6 sm:w-4 sm:h-6 text-instillGray30 hover:text-instillGray05";
   return (
     <div className={classNames.default("flex flex-row gap-x-5", styleName)}>
-      <GithubIconLink iconStyle="w-6 h-6 sm:w-4 sm:h-6 text-instillGray30 hover:text-instillGray05" />
-      <DiscordLink iconStyle="w-6 h-6 sm:w-4 sm:h-6 text-instillGray30 hover:text-instillGray05" />
-      <MediumLink iconStyle="w-6 h-6 sm:w-4 sm:h-6 text-instillGray30 hover:text-instillGray05" />
+      <GithubIconLink iconStyle={style} />
+      <DiscordLink iconStyle={style} />
+      <MediumLink iconStyle={style} />
     </div>
   );
 };
