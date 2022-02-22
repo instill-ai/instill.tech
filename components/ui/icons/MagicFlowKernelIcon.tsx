287df2abec5d19d0390d43c6992af7a56265a374
pochun-chiu <pochun.chiu@instill.tech>
2022-02-22 10:36:33 +0800

    feat: add MagicFlowKernelIcon

---
+++ components/ui/icons/MagicFlowKernelIcon.tsx
@@ -0,0 +1,115 @@
+import { FC } from "react";
+import { IconBase } from "./IconBase";
+
+interface Props {
+  styleName?: string;
+}
+
+export const MagicFlowKernelIcon: FC<Props> = ({ styleName }) => {
+  return (
+    <IconBase viewBox="0 0 67 44" styleName={styleName}>
+      <path
+        d="m33.625 38.037 32.363-18.685c.11-.063.2-.012.2.116v4.36L33.825 42.514c-.11.064-.2.012-.2-.115v-4.36zM33.615 38.03 1.2 19.313c-.11-.064-.2-.012-.2.116v4.362l32.416 18.715c.11.064.2.012.2-.116V38.03z"
+        fill="#E8E8E8"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeLinejoin="round"
+      />
+      <path
+        d="m17.296 9.922-8.142 4.676 8.142 4.677 8.145-4.72-8.145-4.633z"
+        fill="#28F77E"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="m33.58 9.922-8.14 4.676 8.14 4.677 8.146-4.72-8.145-4.633zM49.866 9.922l-8.141 4.676 8.14 4.677 8.146-4.72-8.145-4.633z"
+        fill="#F6F6F6"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="m25.438 14.6-8.145 4.676 8.145 4.677 8.141-4.72-8.14-4.633z"
+        fill="#28F77E"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="m41.727 14.6-8.145 4.676 8.145 4.677 8.141-4.72-8.14-4.633z"
+        fill="#F6F6F6"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="m58.012 14.6-8.145 4.676 8.145 4.677 8.142-4.72-8.142-4.633z"
+        fill="#F6F6F6"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+        strokeLinejoin="round"
+      />
+      <path
+        d="m33.58 19.277-8.14 4.677 8.14 4.673 8.146-4.716-8.145-4.634zM49.866 19.277l-8.141 4.677 8.14 4.673 8.146-4.716-8.145-4.634z"
+        fill="#F6F6F6"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="m41.727 23.953-8.145 4.673 8.145 4.676 8.141-4.715-8.14-4.634z"
+        fill="#F6F6F6"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="m9.153 14.6-8.145 4.676 8.145 4.677 8.141-4.72L9.153 14.6z"
+        fill="#28F77E"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+        strokeLinejoin="round"
+      />
+      <path
+        d="m17.296 19.277-8.142 4.677 8.142 4.673 8.145-4.716-8.145-4.634z"
+        fill="#28F77E"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="m25.438 23.953-8.145 4.673 8.145 4.676 8.141-4.715-8.14-4.634z"
+        fill="#F6F6F6"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="m33.58 28.625-8.14 4.677 8.14 4.676 8.146-4.715-8.145-4.638z"
+        fill="#F6F6F6"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+        strokeLinejoin="round"
+      />
+      <path
+        d="m41.727 14.6 8.141-4.677-8.14-4.677-8.146 4.715 8.145 4.638zM25.438 14.6l8.141-4.677-8.14-4.677-8.146 4.715 8.145 4.638z"
+        fill="#F6F6F6"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="m33.58 9.923 8.146-4.676L33.58.57l-8.142 4.716 8.142 4.637z"
+        fill="#F6F6F6"
+        stroke="#1A1A1A"
+        strokeWidth=".976"
+        strokeMiterlimit="10"
+        strokeLinejoin="round"
+      />
+    </IconBase>
+  );
+};
