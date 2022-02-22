324eca9713096b37606047dbe2ba91a6c80b8e1c
pochun-chiu <pochun.chiu@instill.tech>
2022-02-22 15:44:07 +0800

    feat: send amplitude event when user navigate to github

--- components/ui/buttons/GitHubCtaButton.tsx
+++ components/ui/buttons/GitHubCtaButton.tsx
@@ -1,13 +1,23 @@
 import { useRouter } from "next/router";
 import { FC, useCallback } from "react";
+import { sendAmplitudeData } from "../../../lib/amplitude";
 import { RightArrowIcon } from "../icons/RightArrowIcon";
 import { ButtonBase } from "./ButtonBase";
 
 interface Props {}
 
+/**
+ *
+ * @param styleName - tailwind format style
+ * @returns react function component
+ * - The style of button and text are fixed.
+ * - Use styleName to change position only.
+ */
+
 export const GithubCtaButton: FC<Props> = () => {
   const router = useRouter();
   const handleClick = useCallback(() => {
+    sendAmplitudeData("to_github", { type: "navigation" });
     router.push("https://github.com/instill-ai");
   }, [router]);
 
