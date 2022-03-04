b5211909d45b7db99c81f0e1ffedbe682d44c81a
pochun-chiu <pochun.chiu@instill.tech>
2022-03-04 11:28:23 +0800

    feat: add CareerHero

---
+++ components/ui/CareerHero.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import { ViewJobsButton } from "./buttons/ViewJobsButton";
+import * as classNames from "classnames";
+
+interface Props {
+  /** <Tailwind format> - position and width */
+  styleName?: string;
+}
+
+export const CareerHero: FC<Props> = ({ styleName }) => {
+  return (
+    <div
+      className={classNames.default(
+        "flex flex-col w-full gap-y-10 pt-[100px] lg:pt-[180px] pb-10",
+        styleName
+      )}
+    >
+      <h1 className="w-full instill-text-h1 text-instillGray05 text-center">
+        Come together
+      </h1>
+      <div className="flex flex-col w-full">
+        <p className="text-instillGray05 mx-auto font-sans font-light text-2xl leading-10">
+          We can&#39;t build our vision alone.
+        </p>
+        <p className="text-instillGray05 mx-auto font-sans font-light text-2xl leading-10">
+          Take a look below for your new favorite job.
+        </p>
+      </div>
+      <ViewJobsButton styleName="mx-auto" />
+    </div>
+  );
+};
