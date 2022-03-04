8900d94f95097af8baf1cceeb988882ed632cd71
pochun-chiu <pochun.chiu@instill.tech>
2022-03-04 15:50:00 +0800

    feat: add CareerGeneralInfo

---
+++ components/ui/CareerGeneralIntro.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react";
+import { InstillAiBadge } from "./svgs/images/InstillAiBadge";
+import * as classNames from "classnames";
+
+interface Props {
+  /** <Tailwind format> - position and width */
+  styleName?: string;
+}
+
+export const CareerGeneralIntro: FC<Props> = ({ styleName }) => {
+  return (
+    <div
+      className={classNames.default(
+        "flex flex-col md:flex-row w-full gap-x-20 py-[100px]",
+        styleName
+      )}
+    >
+      <InstillAiBadge styleName="w-full md:w-[402px] md:h-[177px] flex-shrink-0" />
+      <div className="flex flex-col gap-y-5">
+        <p className="instill-text-body text-instillGray05">
+          We&#39;re looking for passionate and dedicated early members to build
+          open-source and highest-quality products for solving a challenging
+          issue in the modern data stack. You&#39;ll be joining the founding
+          team to establish its strong foundation from the very beginning.
+        </p>
+        <p className="instill-text-body text-instillGray05">
+          This is a great opportunity for those who want to build their career
+          with a fast-growing startup at zero to one stage. It is also a
+          position for those who value learning for the long term above earning
+          in the short term. Your colleagues are a bunch of super self-motivated
+          Vision AI missionaries and data tooling practitioners. You will impact
+          the company direction and help Instill AI build its strong foundation
+          and shape its open-source community.
+        </p>
+      </div>
+    </div>
+  );
+};
