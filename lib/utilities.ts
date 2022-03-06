cbeaa9952c569a4de1c05c4fc15d5b44208037ee
pochun-chiu <pochun.chiu@instill.tech>
2022-03-06 18:56:22 +0800

    feat: add handle utils

--- lib/utilities.ts
+++ lib/utilities.ts
@@ -3,11 +3,23 @@ export const getRandomInt = (max: number): number => {
   return Math.floor(Math.random() * max);
 };
 
-export const getNonDuplicatedRandomInt = (max: number, oldValue: number): number => {
+export const getNonDuplicatedRandomInt = (
+  max: number,
+  oldValue: number
+): number => {
   const newValue = Math.floor(Math.random() * max);
-  if (newValue !== oldValue){
-    return newValue
+  if (newValue !== oldValue) {
+    return newValue;
   }
 
-  return getNonDuplicatedRandomInt(max, oldValue)
-}
+  return getNonDuplicatedRandomInt(max, oldValue);
+};
+
+export const handle = async <T>(
+  promise: Promise<T>,
+  defaultError: any = "rejected"
+): Promise<T[] | [any, T]> => {
+  return promise
+    .then((data) => [undefined, data])
+    .catch((error) => Promise.resolve([error || defaultError, undefined]));
+};
