69e3fcaefc1984a2bd01c3b4ea06b708b9d42319
pochun-chiu <pochun.chiu@instill.tech>
2022-03-07 15:24:45 +0800

    feat: add BackToPreviousPageLink

---
+++ components/ui/links/BackToPreviousPageLink.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import { LinkBase } from "./LinkBase";
+import * as classNames from "classnames";
+import { LeftArrowIcon } from "../icons/LeftArrowIcon";
+
+interface Props {
+  styleName?: string;
+
+  /** Previous page's link */
+  url: string;
+}
+
+export const BackToPreviousPageLink: FC<Props> = ({ styleName, url }) => {
+  return (
+    <LinkBase
+      href={url}
+      styleName={classNames.default("flex flex-row gap-x-5", styleName)}
+    >
+      <LeftArrowIcon styleName="my-auto w-[15px] h-[15px] text-instillGray30" />
+      <p className="instill-text-body text-instillGray30">Back</p>
+    </LinkBase>
+  );
+};
