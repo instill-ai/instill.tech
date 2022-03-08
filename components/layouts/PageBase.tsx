242a108aee0310bc6a27ab5d7e5faa9b03e77ab3
pochun-chiu <pochun.chiu@instill.tech>
2022-03-08 15:10:02 +0800

    refactor: refactor PageBase to lazy load Footer

--- components/layouts/PageBase.tsx
+++ components/layouts/PageBase.tsx
@@ -1,21 +1,24 @@
-import { FC, ReactNode } from "react";
-import { Footer } from "../ui/commons/Footer";
+import dynamic from "next/dynamic";
+import { FC, ReactNode, useRef } from "react";
+import { useOnScreen } from "../../hooks/useOnScreen";
 import { NavBar } from "../ui/commons/NavBar";
 import { BaseContainer } from "./BaseContainer";
 import { SectionContainer } from "./SectionContainer";
 import { SectionContainerFull } from "./SectionContainerFull";
 
+const Footer = dynamic(() =>
+  import("../ui/commons/Footer").then((mod) => mod.Footer)
+);
+
 interface Props {
-  currentPage?: string;
-  children: ReactNode;
+  /** with max width = max-w-[1440px] md:w-10/12 md:mx-auto */
   withMaxWidth: boolean;
 }
 
-export const PageBase: FC<Props> = ({
-  currentPage,
-  children,
-  withMaxWidth,
-}) => {
+export const PageBase: FC<Props> = ({ withMaxWidth, children }) => {
+  const footerRef = useRef<HTMLDivElement>();
+  const footerIsOnScreen = useOnScreen(footerRef);
+
   return (
     <BaseContainer>
       <NavBar />
@@ -24,7 +27,9 @@ export const PageBase: FC<Props> = ({
       ) : (
         <SectionContainerFull>{children}</SectionContainerFull>
       )}
-      <Footer />
+      <div className="flex" ref={footerRef}>
+        {footerIsOnScreen && <Footer />}
+      </div>
     </BaseContainer>
   );
 };
