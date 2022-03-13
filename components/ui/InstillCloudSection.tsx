09d691bbf3bbf97bd234fbb4f3522fb58283c2ad
pochun-chiu <pochun.chiu@instill.tech>
2022-03-13 20:21:55 +0800

    refactor: refactor InstillCloudSection to use new StickyScrollLayout

--- components/ui/InstillCloudSection.tsx
+++ components/ui/InstillCloudSection.tsx
@@ -1,37 +1,34 @@
-import { useRouter } from "next/router";
-import { FC, useEffect, useRef, useState } from "react";
-import { useScrollPositionWhenObserve } from "../../hooks/useScrollPositionWhenObserve";
-import { ElementPosition, getElementPosition } from "../../lib/utilities";
+import { FC } from "react";
+import { StickyScrollLayout } from "../layouts/StickyScrollLayout";
 import { InstillCloudBanner } from "./svgs/images/InstillCloudBanner";
 
 interface Props {}
 
 export const InstillCloudSection: FC<Props> = () => {
-  const sectionRef = useRef<HTMLDivElement>();
-  const [targetPosition, setTargetPosition] = useState<ElementPosition>();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!router.isReady) {
-      return;
-    }
-
-    setTargetPosition(getElementPosition(sectionRef.current));
-  }, [router.isReady]);
-
-  const { scrollY, isObserving } = useScrollPositionWhenObserve(
-    sectionRef,
-    200
-  );
-
   return (
-    <div ref={sectionRef} className="flex w-full bg-white">
-      <InstillCloudBanner
-        scrollY={scrollY}
-        isObserving={isObserving}
-        observeTargetPosition={targetPosition}
-        styleName="mx-auto max:w-[1440px] h-[997px]"
-      />
+    <div className="flex flex-col">
+      <StickyScrollLayout
+        targetChildren={
+          <InstillCloudBanner styleName="w-full mx-auto max:w-[1440px] max:h-[997px]" />
+        }
+        spanRatio={0.5}
+        height={3000}
+      >
+        <div className="flex flex-col max-w-[1440px] md:w-10/12 md:mx-auto bg-instillGray95 p-10 z-50">
+          <p className="text-center instill-text-body text-instillBlue30 mb-5">
+            INSTILL CLOUD
+          </p>
+          <h2 className="text-center instill-text-h2 text-instillGray05 mb-10">
+            Visual Data Preparation without Managing Infrastructure
+          </h2>
+          <p className="text-center instill-text-body text-instillGray05 md:px-36">
+            Instill Cloud provides production-ready visual data preparation
+            services — hustle-free setup and Vision AI model serving with
+            ready-to-use and custom models. We help you save your time and you
+            focus on the core business.
+          </p>
+        </div>
+      </StickyScrollLayout>
     </div>
   );
 };
