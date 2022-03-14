a53c7c52f56eab28e1a6326d17bf70f68ca78261
pochun-chiu <pochun.chiu@instill.tech>
2022-03-14 20:02:13 +0800

    feat: add lazy load at CareerDetailPage

--- pages/career/[slug].tsx
+++ pages/career/[slug].tsx
@@ -1,11 +1,12 @@
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useEffect, useRef, useState } from "react";
+import dynamic from "next/dynamic";
+
 import { PageBase } from "../../components/layouts/PageBase";
 import { PageHead } from "../../components/layouts/PageHead";
 import { CareerPositionDescriptionBlock } from "../../components/ui/blocks/CareerPositionDescriptionBlock";
 import { CareerPostionDetailsBlock } from "../../components/ui/blocks/CareerPostionDetailsBlock";
-import { StayInTheLoopBlock } from "../../components/ui/blocks/StayInTheLoopBlock";
 import { BackToPreviousPageLink } from "../../components/ui/links/BackToPreviousPageLink";
 import {
   getClickUpTaskQuery,
@@ -14,6 +15,13 @@ import {
 import { handle } from "../../lib/utilities";
 import { IClickUpTask } from "../../types/clickUp";
 import { TPositionDetails } from "../../types/instill";
+import { useOnScreen } from "../../hooks/useOnScreen";
+
+const StayInTheLoopBlock = dynamic(() =>
+  import("../../components/ui/blocks/StayInTheLoopBlock").then(
+    (mod) => mod.StayInTheLoopBlock
+  )
+);
 
 interface Props {
   position: TPositionDetails;
@@ -32,34 +40,51 @@ const CareerPositionPage: FC<Props> & {
     router.push("/404");
   }
 
+  // lazy load stayInTheLoop
+  const stayInTheLoopRef = useRef<HTMLDivElement>();
+  const stayInTheLoopIsOnScreen = useOnScreen(stayInTheLoopRef);
+  const [loadStayInTheLoop, setLoadStayInTheLoop] = useState(false);
+
+  useEffect(() => {
+    if (stayInTheLoopIsOnScreen && !loadStayInTheLoop) {
+      setLoadStayInTheLoop(true);
+    }
+  }, [stayInTheLoopIsOnScreen]);
+
   return (
     <PageHead
       pageTitle={`${position.name} | Instill AI`}
       pageDescription="We're on a mission to make Vision Al highly accessbile to everyone. Join us and make a dent in the universe!"
     >
-      <div className="flex flex-col pt-[100px] lg:pt-[180px] pb-10">
-        <div className="flex mb-10 px-4 md:px-0">
-          <BackToPreviousPageLink url="/career" />
-        </div>
+      <div className="flex bg-instillGray95">
+        <div className="flex flex-col pt-[100px] lg:pt-[180px] pb-10 max-w-[1440px] md:mx-auto md:w-10/12 ">
+          <div className="flex mb-10 px-4 md:px-0">
+            <BackToPreviousPageLink url="/career" />
+          </div>
 
-        <div className="flex flex-col gap-y-20 md:gap-y-0 md:flex-row md:gap-x-6 mb-[100px]">
-          <CareerPostionDetailsBlock
-            styleName="px-4 md:px-0 md:w-4/12"
-            position={position}
-          />
-          <CareerPositionDescriptionBlock
-            styleName="px-4 md:px-10 md:w-8/12"
-            description={position.description}
-          />
+          <div className="flex flex-col gap-y-20 md:gap-y-0 md:flex-row md:gap-x-6 mb-[100px]">
+            <CareerPostionDetailsBlock
+              styleName="px-4 md:px-0 md:w-4/12"
+              position={position}
+            />
+            <CareerPositionDescriptionBlock
+              styleName="px-4 md:px-10 md:w-8/12"
+              description={position.description}
+            />
+          </div>
+          <div className="flex" ref={stayInTheLoopRef}>
+            {loadStayInTheLoop && (
+              <StayInTheLoopBlock styleName="px-4 md:px-0" />
+            )}
+          </div>
         </div>
-        <StayInTheLoopBlock styleName="px-4 md:px-0" />
       </div>
     </PageHead>
   );
 };
 
 CareerPositionPage.getLayout = (page) => {
-  return <PageBase withMaxWidth={true}>{page}</PageBase>;
+  return <PageBase withMaxWidth={false}>{page}</PageBase>;
 };
 
 export default CareerPositionPage;
