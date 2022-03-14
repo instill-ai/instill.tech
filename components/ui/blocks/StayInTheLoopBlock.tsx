94340c7d30b185c4ce84e22f80fde41f11db9335
pochun-chiu <pochun.chiu@instill.tech>
2022-03-14 15:54:41 +0800

    fix: fix StayInTheLoop style

--- components/ui/blocks/StayInTheLoopBlock.tsx
+++ components/ui/blocks/StayInTheLoopBlock.tsx
@@ -10,7 +10,7 @@ export const StayInTheLoopBlock: FC<Props> = ({ styleName }) => {
   return (
     <div
       className={classNames.default(
-        "grid grid-cols-1 gap-y-[30px] py-6 sm:py-[60px] sm:grid-cols-2 sm:gap-y-0 sm:gap-x-8 md:gap-x-16 lg:gap-x-28",
+        "w-full grid grid-cols-1 gap-y-[30px] py-6 sm:py-[60px] sm:grid-cols-2 sm:gap-y-0 sm:gap-x-8 md:gap-x-16 lg:gap-x-28",
         styleName
       )}
     >
