77192937f4b6c04c4066c1894a20f718422bc462
pochun-chiu <pochun.chiu@instill.tech>
2022-03-15 16:41:21 +0800

    feat: add default height at section container

--- components/layouts/SectionContainerFull.tsx
+++ components/layouts/SectionContainerFull.tsx
@@ -3,5 +3,5 @@ import { FC } from "react";
 interface Props {}
 
 export const SectionContainerFull: FC<Props> = ({ children }) => {
-  return <div className="flex flex-1 flex-col">{children}</div>;
+  return <div className="flex h-full flex-1 flex-col">{children}</div>;
 };
