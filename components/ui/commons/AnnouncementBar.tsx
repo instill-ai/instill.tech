69c771a0e177721b5b125da51effe0f4a3e1d4d7
pochun-chiu <pochun.chiu@instill.tech>
2022-03-17 14:58:07 +0800

    refactor: use flexbox instead of absolute position

--- components/ui/commons/AnnouncementBar.tsx
+++ components/ui/commons/AnnouncementBar.tsx
@@ -12,15 +12,13 @@ export const AnnouncementBar: FC<Props> = ({ children }) => {
   }, []);
 
   return (
-    <div className="relative flex w-full bg-instillYellow py-2.5">
+    <div className="flex w-full gap-x-2 bg-instillYellow py-2.5 px-4">
       <div className="mx-auto flex">{children}</div>
-      <div className="absolute right-4 top-0 flex h-full">
-        <div
-          onClick={handleClick}
-          className="my-auto flex hover:cursor-pointer hover:bg-instillGray15"
-        >
-          <CrossIcon styleName="my-auto w-5 h-5 text-instillGray95" />
-        </div>
+      <div
+        onClick={handleClick}
+        className="my-auto flex hover:cursor-pointer hover:bg-instillGray15"
+      >
+        <CrossIcon styleName="my-auto w-5 h-5 text-instillGray95" />
       </div>
     </div>
   );
