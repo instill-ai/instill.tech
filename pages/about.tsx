e190925d0852901cf182bbcbdb5f97bb731ca780
pochun-chiu <pochun.chiu@instill.tech>
2022-03-19 20:02:16 +0800

    feat: add temp about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,6 +7,14 @@ import { PageHead } from "../components/layouts/PageHead";
 import { sendAmplitudeData } from "../lib/amplitude";
 import { useOnScreen } from "../hooks/useOnScreen";
 import { useAmplitudeCtx } from "../context/AmplitudeContext";
+import { GetStaticProps } from "next";
+import { IClickUpTask } from "../types/clickUp";
+import {
+  listClickUpTasksInListQuery,
+  transformClickUpTaskToMemberDetails,
+} from "../lib/clickUp";
+import { TMemberDetails } from "../types/instill";
+import { OurMembersSection } from "../components/ui/OurMembersSection";
 
 const SecureYourSpotBlock = dynamic(() =>
   import("../components/ui/blocks/SecureYourSpotBlock").then(
@@ -24,13 +32,16 @@ interface GetLayOutProps {
   page: ReactElement;
 }
 
-interface Props {}
+interface Props {
+  members: TMemberDetails[];
+}
 
 const AboutPage: FC<Props> & {
   getLayout?: FC<GetLayOutProps>;
-} = () => {
+} = ({ members }) => {
   const router = useRouter();
   const { amplitudeIsInit } = useAmplitudeCtx();
+  console.log(members);
 
   useEffect(() => {
     if (router.isReady && amplitudeIsInit) {
@@ -71,68 +82,78 @@ const AboutPage: FC<Props> & {
       pageTitle="About us | Instill AI"
       pageDescription="Instill AI, founded in 2020 (June 11th 2020, to be more specific), provides no-/low-code tools to convert unstructured visual data to meaningful structured representations."
     >
-      <div className="flex bg-instillGray95">
-        <div className="max-w-[1440px] md:mx-auto md:w-10/12">
-          <div className="flex flex-col px-4 md:px-0 max:mx-auto max:w-10/12">
-            <div className="flex w-full pt-[87px] pb-[152px] sm:h-[584px] sm:py-0">
-              <h1 className="instill-text-h1 m-auto max-w-[934px] text-center text-instillGray05">
-                Make Vision AI Accessible to Everyone
-              </h1>
+      <div className="flex flex-col bg-instillGray95">
+        <div className="mb-5 flex flex-col">
+          <div className="flex w-full pt-[87px] pb-[152px] sm:h-[584px] sm:py-0">
+            <h1 className="instill-text-h1 m-auto max-w-[934px] text-center text-instillGray05">
+              Make Vision AI Accessible to Everyone
+            </h1>
+          </div>
+          <div className="grid grid-cols-1 gap-y-6 lg:grid-cols-2 lg:gap-x-6 lg:gap-y-6">
+            <div className="flex flex-col gap-y-5 rounded-[1px] bg-white p-10">
+              <h2 className="instill-text-h2 mb-1 text-instillGray95">
+                Our Company
+              </h2>
+              <p className="instill-text-body text-instillGray95">
+                Instill AI, founded in 2020 (June 11th 2020, to be more
+                specific), provides no-/low-code tools to convert unstructured
+                visual data to meaningful structured representations. Users can
+                integrate our service into their data stack, and start tapping
+                into the wealth of their visual data and benefit from Vision AI
+                in a snap.
+              </p>
+              <p className="instill-text-body text-instillGray95">
+                We are a small and ambitious team of passionate
+                engineers/researchers sharing an unconventional culture fused by
+                DevOps, MLOps and academic research lab. We are hands-on and
+                love to automate everything. We care about our products and
+                deliver to the highest standard.
+              </p>
             </div>
-            <div className="grid grid-cols-1 gap-y-6 lg:grid-cols-2 lg:gap-x-6 lg:gap-y-6">
-              <div className="flex flex-col gap-y-5 rounded-[1px] bg-white p-10">
-                <h2 className="instill-text-h2 mb-1 text-instillGray95">
-                  Our Company
-                </h2>
+            <div className="flex flex-col gap-y-5 rounded-[1px] bg-white p-10">
+              <h2 className="instill-text-h2 text-instillGray95">Our Value</h2>
+              <div className="flex flex-col">
+                <h3 className="instill-text-h3 text-instillGray95">
+                  User obsession
+                </h3>
                 <p className="instill-text-body text-instillGray95">
-                  Instill AI, founded in 2020 (June 11th 2020, to be more
-                  specific), provides no-/low-code tools to convert unstructured
-                  visual data to meaningful structured representations. Users
-                  can integrate our service into their data stack, and start
-                  tapping into the wealth of their visual data and benefit from
-                  Vision AI in a snap.
-                </p>
-                <p className="instill-text-body text-instillGray95">
-                  We are a small and ambitious team of passionate
-                  engineers/researchers sharing an unconventional culture fused
-                  by DevOps, MLOps and academic research lab. We are hands-on
-                  and love to automate everything. We care about our products
-                  and deliver to the highest standard.
+                  We succeed only when our users succeed. Learning what can
+                  really help our users and delivering the value they need will
+                  always be Instill&#39;s highest interest.
                 </p>
               </div>
-              <div className="flex flex-col gap-y-5 rounded-[1px] bg-white p-10">
-                <h2 className="instill-text-h2 text-instillGray95">
-                  Our Value
-                </h2>
-                <div className="flex flex-col">
-                  <h3 className="instill-text-h3 text-instillGray95">
-                    User obsession
-                  </h3>
-                  <p className="instill-text-body text-instillGray95">
-                    We succeed only when our users succeed. Learning what can
-                    really help our users and delivering the value they need
-                    will always be Instill&#39;s highest interest.
-                  </p>
-                </div>
-                <div className="flex flex-col">
-                  <h3 className="instill-text-h3 text-instillGray95">
-                    High-performing company with shared values
-                  </h3>
-                  <p className="instill-text-body text-instillGray95">
-                    We always deliver more than expected. To achieve that, we
-                    are the optimisers obsessively chasing intrinsic efficiency
-                    and effectiveness.
-                  </p>
-                </div>
+              <div className="flex flex-col">
+                <h3 className="instill-text-h3 text-instillGray95">
+                  High-performing company with shared values
+                </h3>
+                <p className="instill-text-body text-instillGray95">
+                  We always deliver more than expected. To achieve that, we are
+                  the optimisers obsessively chasing intrinsic efficiency and
+                  effectiveness.
+                </p>
               </div>
-            </div>
-            <div className="flex" ref={secureYourSpotBlockRef}>
-              {loadSecureYourSpotBlock && <SecureYourSpotBlock />}
-            </div>
-            <div className="mb-20 flex" ref={stayInTheLoopBlockRef}>
-              {loadStayInTheLoopBlock && <StayInTheLoopBlock />}
             </div>
           </div>
+        </div>
+        <div className="bg-white">
+          <OurMembersSection
+            styleName="md:mx-auto md:w-10/12 max-w-[1440px]"
+            members={members}
+          />
+        </div>
+        <div
+          className="flex max-w-[1440px] md:mx-auto md:w-10/12"
+          ref={secureYourSpotBlockRef}
+        >
+          {loadSecureYourSpotBlock && (
+            <SecureYourSpotBlock bgColor="white" layout="compact" />
+          )}
+        </div>
+        <div
+          className="mb-20 flex max-w-[1440px] md:mx-auto md:w-10/12"
+          ref={stayInTheLoopBlockRef}
+        >
+          {loadStayInTheLoopBlock && <StayInTheLoopBlock />}
         </div>
       </div>
     </PageHead>
@@ -144,3 +165,27 @@ AboutPage.getLayout = (page) => {
 };
 
 export default AboutPage;
+
+export const getStaticProps: GetStaticProps = async () => {
+  let tasks: IClickUpTask[];
+  let members: TMemberDetails[] = [];
+
+  try {
+    tasks = await listClickUpTasksInListQuery("181513244");
+    tasks.forEach((task) => {
+      members.push(transformClickUpTaskToMemberDetails(task));
+    });
+    members = members.sort((a, b) => a.order - b.order);
+  } catch (err) {
+    console.error("Something went wrong when retrieve member on Clickup", err);
+  }
+
+  return {
+    props: {
+      members,
+    },
+
+    // This page is using ISR
+    revalidate: 10,
+  };
+};
