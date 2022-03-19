189c288e9a5b51cbc9c3369cd77b89e20f188164
pochun-chiu <pochun.chiu@instill.tech>
2022-03-19 19:59:59 +0800

    fix: fix MemberAvatarKernelBlock type

--- components/ui/blocks/MemberAvatarKernelBlock.tsx
+++ components/ui/blocks/MemberAvatarKernelBlock.tsx
@@ -3,26 +3,28 @@ import { FC } from "react";
 import { MemberKernelSvg } from "../svgs/images/MemberKernelSvg";
 
 interface Props {
-  kernelPosition: string;
+  kernelColorCubeLocation: string[];
   kernelColor: string;
   avatarAlt: string;
-  avatarSrc: string;
+  avatarWithFrameDesktop: string;
+  avatarWithFrameMobile: string;
 }
 
 export const MemberAvatarKernelBlock: FC<Props> = ({
   avatarAlt,
-  avatarSrc,
+  avatarWithFrameDesktop,
+  avatarWithFrameMobile,
 }) => {
   return (
     <div className="relative flex w-full">
-      <MemberKernelSvg styleName="w-full h-full hover:opacity-0" />
-      <div className="absolute top-0 bottom-0 right-0 left-0 -z-20 block">
+      <MemberKernelSvg styleName="w-full h-full hover:opacity-0 z-20" />
+      <div className="absolute top-0 bottom-0 right-0 left-0 block">
         <Image
           alt={avatarAlt}
           width={360}
           height={360}
           layout="responsive"
-          src={avatarSrc}
+          src={avatarWithFrameDesktop}
         />
       </div>
     </div>
