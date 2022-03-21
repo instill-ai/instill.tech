b9912a164fdc227fa53e577e57bb43593eb6cc5e
pochun-chiu <pochun.chiu@instill.tech>
2022-03-21 16:22:48 +0800

    feat: add position method for mobile view

--- components/ui/OurMembersSection.tsx
+++ components/ui/OurMembersSection.tsx
@@ -1,8 +1,20 @@
-import { FC, useCallback, useState } from "react";
+import {
+  createRef,
+  FC,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import * as classNames from "classnames";
+
 import { TMemberDetails } from "../../types/instill";
 import { MemberAvatarKernelBlock } from "./blocks/MemberAvatarKernelBlock";
-import * as classNames from "classnames";
 import { MemberIntroBlock } from "./blocks/MemberIntroBlock";
+import { useWindowDimension } from "../../useWindowDimension";
+import { ElementPosition, getElementPosition } from "../../lib/utilities";
+import { useRouter } from "next/router";
 
 interface Props {
   /** <Tailwind config> */
@@ -12,32 +24,96 @@ interface Props {
 }
 
 export const OurMembersSection: FC<Props> = ({ members, styleName }) => {
-  const [targerMember, setTargetMember] = useState<TMemberDetails>(null);
+  // If openAllKernel === true, every member's avatar will be revealed.
   const [openAllKernel, setOpenAllKernel] = useState(false);
-
   const handleKernel = useCallback(() => {
-    console.log(openAllKernel);
     setOpenAllKernel(!openAllKernel);
   }, [openAllKernel]);
 
-  const onMouseEnterHandler = useCallback((memberId: string) => {
-    const index = members.findIndex((e) => e.id === memberId);
-    console.log(memberId);
+  // TargetMember's intro will be displayed at MemberIntroBlock, we will setTargetMember
+  // once user hover on member's avatar. If it's on mobile view(width < 768px), the hover
+  // behavior will be changed to click behavior to enhance user experience.
 
-    if (index === -1) {
+  const [targerMember, setTargetMember] = useState<TMemberDetails>(null);
+  const [targetPosition, setTagetPosition] = useState<ElementPosition>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [containerPosition, setContainerPosition] =
+    useState<ElementPosition>(null);
+  const windowDimenstion = useWindowDimension();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!router.isReady) {
       return;
     }
+    setContainerPosition(getElementPosition(containerRef.current));
+  }, [router]);
 
-    setTargetMember(members[index]);
-  }, []);
+  const onMouseEnterHandler = useCallback(
+    (memberId: string) => {
+      if (windowDimenstion && windowDimenstion.width < 768) {
+        return;
+      }
+
+      const index = members.findIndex((e) => e.id === memberId);
+
+      if (index === -1) {
+        console.error("Target member not found");
+        return;
+      }
+
+      setTargetMember(members[index]);
+    },
+    [windowDimenstion, members]
+  );
 
   const onMouseLeaveHandler = useCallback(() => {
-    console.log("remove");
+    if (windowDimenstion && windowDimenstion.width < 768) {
+      return;
+    }
+    setTargetMember(null);
+  }, [windowDimenstion]);
+
+  const membersRef = useMemo(
+    () =>
+      members.map((m) => {
+        return { id: m.id, ref: createRef<HTMLDivElement>() };
+      }),
+    [members]
+  );
+
+  const onClickHandler = useCallback(
+    (memberId: string) => {
+      if (windowDimenstion && windowDimenstion.width >= 768) {
+        return;
+      }
+      const index = members.findIndex((e) => e.id === memberId);
+
+      if (index === -1) {
+        console.error("Target member not found");
+        return;
+      }
+
+      setTargetMember(members[index]);
+
+      const refIndex = membersRef.findIndex((e) => e.id === memberId);
+
+      setTagetPosition(getElementPosition(membersRef[refIndex].ref.current));
+    },
+    [windowDimenstion, members, membersRef]
+  );
+
+  const onCancelHandler = useCallback(() => {
     setTargetMember(null);
   }, []);
 
   return (
-    <div className={classNames.default("flex flex-col py-10", styleName)}>
+    <div
+      className={classNames.default(
+        "flex flex-col py-10 px-4 md:px-0",
+        styleName
+      )}
+    >
       <div className="mb-10 flex flex-col">
         <h2 className="instill-text-h2 mb-5">Our Member</h2>
         <h3 className="instill-text-body">
@@ -52,27 +128,56 @@ export const OurMembersSection: FC<Props> = ({ members, styleName }) => {
           to {openAllKernel ? "hide" : "expand"} them all.
         </h3>
       </div>
-      <div className="relative grid-cols-3 gap-x-10 md:grid">
-        <div className="col-span-2 grid-cols-2 gap-x-10 gap-y-10 md:grid">
-          {members.map((member) => (
-            <MemberAvatarKernelBlock
-              key={member.id}
-              id={member.id}
-              kernelColor={member.kernelColor}
-              kernelColorRectLocation={member.kernelColorRectLocation}
-              avatarAlt={`Instill member - ${member.name}'s avatar with detection frame`}
-              avatarWithFrameDesktop={member.avatarWithFrameDesktop}
-              avatarWithFrameMobile={member.avatarWithFrameMobile}
-              onMouseEnterHandler={onMouseEnterHandler}
-              onMouseLeaveHandler={onMouseLeaveHandler}
-              openKernel={openAllKernel}
-            />
-          ))}
+      <div
+        ref={containerRef}
+        className="relative flex flex-col md:grid md:grid-cols-5 md:gap-x-6 lg:grid-cols-3 lg:gap-x-10"
+      >
+        <div className="flex w-full md:col-span-3 lg:col-span-2">
+          <div className="grid w-full grid-cols-2 gap-x-2.5 gap-y-2.5 md:gap-x-6 md:gap-y-6 lg:gap-x-10 lg:gap-y-10">
+            {members.map((m) => {
+              const index = membersRef.findIndex((e) => e.id === m.id);
+              return (
+                <MemberAvatarKernelBlock
+                  ref={membersRef[index].ref}
+                  key={m.id}
+                  id={m.id}
+                  kernelColor={m.kernelColor}
+                  kernelColorRectLocation={m.kernelColorRectLocation}
+                  avatarAlt={`Instill member - ${m.name}'s avatar with detection frame`}
+                  avatarWithFrameDesktop={m.avatarWithFrameDesktop}
+                  avatarWithFrameMobile={m.avatarWithFrameMobile}
+                  onMouseEnterHandler={onMouseEnterHandler}
+                  onMouseLeaveHandler={onMouseLeaveHandler}
+                  onClickHandler={onClickHandler}
+                  openKernel={openAllKernel}
+                />
+              );
+            })}
+          </div>
         </div>
-        <div className="col-span-1">
+        <div
+          className={classNames.default(
+            "absolute w-full md:static md:col-span-2 md:flex lg:col-span-1",
+            {
+              hidden: !targerMember,
+              "z-20": targerMember,
+            }
+          )}
+          style={{
+            top:
+              targetPosition &&
+              `${
+                targetPosition.y -
+                containerPosition.y +
+                targetPosition.height +
+                10
+              }px`,
+          }}
+        >
           <MemberIntroBlock
-            styleName="sticky top-[164px]"
-            indent="pl-12"
+            styleName="md:sticky md:top-[164px]"
+            indent="pl-4 md:pl-6 xl:pl-8"
+            onCancelHandler={onCancelHandler}
             member={targerMember}
           />
         </div>
