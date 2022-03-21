250097d555a62874c81b631cd1cd90d49c44afc6
pochun-chiu <pochun.chiu@instill.tech>
2022-03-21 08:53:52 +0800

    feat: add debounce function

--- lib/utilities.ts
+++ lib/utilities.ts
@@ -56,3 +56,25 @@ export const getElementPosition = (element: HTMLElement): ElementPosition => {
     height: box.height,
   };
 };
+
+// Debounce function
+// ref: https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_debounce
+
+export const debounce = (func: any, wait: number, immediate?: boolean) => {
+  let timeout: NodeJS.Timeout;
+
+  return function () {
+    let context = this;
+    let args = arguments;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(function () {
+      timeout = null;
+      console.log("hi timeout");
+      if (!immediate) func.apply(context, args);
+    }, wait);
+
+    if (immediate && !timeout) func.apply(context, args);
+  };
+};
