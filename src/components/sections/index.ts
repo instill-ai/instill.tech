859e6d7b5461b7b97b053ef5f0650b04eceb87a4
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-20 15:21:42 +0800

    refactor: refactor InstillCloudSection

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -2,6 +2,7 @@ import SecureYourSpotSection from "./SecureYourSpotSection";
 import type { SecureYourSpotSectionProps } from "./SecureYourSpotSection";
 import OurMembersSection from "./OurMembersSection";
 import type { OurMembersSectionProps } from "./OurMembersSection";
+import InstillCloudSection from "./InstillCloudSection";
 
-export { SecureYourSpotSection, OurMembersSection };
+export { SecureYourSpotSection, OurMembersSection, InstillCloudSection };
 export type { SecureYourSpotSectionProps, OurMembersSectionProps };
