c14e6f9394cacc8ea7edcda589b353f6550db1d2
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-20 15:51:07 +0800

    feat: add HeroSection

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -3,6 +3,17 @@ import type { SecureYourSpotSectionProps } from "./SecureYourSpotSection";
 import OurMembersSection from "./OurMembersSection";
 import type { OurMembersSectionProps } from "./OurMembersSection";
 import InstillCloudSection from "./InstillCloudSection";
+import HeroSection from "./HeroSection";
+import type { HeroSectionProps } from "./HeroSection";
 
-export { SecureYourSpotSection, OurMembersSection, InstillCloudSection };
-export type { SecureYourSpotSectionProps, OurMembersSectionProps };
+export {
+  SecureYourSpotSection,
+  OurMembersSection,
+  InstillCloudSection,
+  HeroSection,
+};
+export type {
+  SecureYourSpotSectionProps,
+  OurMembersSectionProps,
+  HeroSectionProps,
+};
