51d863d531fae396692d1bbf3f8943c210ff018d
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-21 19:09:37 +0800

    refactor: refactor GithubCtaButton

--- src/components/ui/buttons/index.ts
+++ src/components/ui/buttons/index.ts
@@ -3,6 +3,12 @@ import type { ExploreMoreScrollButtonProps } from "./ExploreMoreScrollButton";
 import GetEarlyAccessButton from "./GetEarlyAccessButton";
 import type { GetEarlyAccessButtonProps } from "./GetEarlyAccessButton";
 import JoinDiscordButton from "./JoinDiscordButton";
+import GithubCtaButton from "./GitHubCtaButton";
 
-export { ExploreMoreScrollButton, GetEarlyAccessButton, JoinDiscordButton };
+export {
+  ExploreMoreScrollButton,
+  GetEarlyAccessButton,
+  JoinDiscordButton,
+  GithubCtaButton,
+};
 export type { ExploreMoreScrollButtonProps, GetEarlyAccessButtonProps };
