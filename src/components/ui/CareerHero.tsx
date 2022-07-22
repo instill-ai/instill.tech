ac90637ede655f559067de98198215ca5b868edd
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-22 15:53:29 +0800

    fix: fix ViewJobsScrollButton position

--- src/components/ui/CareerHero.tsx
+++ src/components/ui/CareerHero.tsx
@@ -30,7 +30,10 @@ export const CareerHero: FC<Props> = ({
     "text-lg leading-[27px] text-instillGrey05 mx-auto font-sans md:font-light md:text-2xl md:leading-10 text-center";
   return (
     <div
-      className={cn("flex w-full flex-col gap-y-10 px-4 md:px-0", marginBottom)}
+      className={cn(
+        "flex w-full flex-col gap-y-10 px-4 pt-10 pb-10 md:px-0 md:pt-[100px]",
+        marginBottom
+      )}
     >
       <h1 className="instill-text-h1 w-full text-center text-instillGrey05">
         Come together
@@ -47,7 +50,10 @@ export const CareerHero: FC<Props> = ({
           Take a look below for your new favorite job.
         </p>
       </div>
-      <ViewJobsScrollButton scrollHandler={viewJobsScrollHandler} />
+      <ViewJobsScrollButton
+        scrollHandler={viewJobsScrollHandler}
+        position="mx-auto"
+      />
     </div>
   );
 };
