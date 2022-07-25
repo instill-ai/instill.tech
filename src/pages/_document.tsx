0a4027c6712a1172b5aec9bd76d53fc98f22a0b0
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-25 20:20:38 +0800

    feat: add plausible (#130)
    Because
    
    - We want to use Plausible as ga alternative
    
    This commit
    
    - Add Plausible tracking

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -65,16 +65,9 @@ export const Document: FC<Props> = () => {
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
         />
         <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
+          defer={true}
+          data-domain="instill.tech"
+          src="https://plausible.io/js/plausible.js"
         />
       </Head>
       <body>
