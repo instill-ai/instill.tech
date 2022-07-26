80022ce5795d8ea6a2435a28df3401beff498b51
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-26 15:49:28 +0800

    feat: add docs style

---
+++ src/styles/docsBaseStyle.ts
@@ -0,0 +1,11 @@
+export const docsBaseStyles = {
+  // Remember to change scroll-margin-top at index.css
+  headerHeight: "h-20",
+
+  sidebar: {
+    topOffset: "top-20",
+    hoveredItemBgColor: "bg-blue-200",
+    currentItemBgColor: "bg-blue-500",
+    currentItemTextColor: "text-black",
+  },
+};
