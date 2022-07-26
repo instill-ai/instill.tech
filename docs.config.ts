8cc411887c9d08633e1d17eaf29608d0c746b5aa
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-26 15:48:27 +0800

    feat: add docs config

---
+++ docs.config.ts
@@ -0,0 +1,65 @@
+import { NavBar, Sidebar } from "@/types/docs";
+
+export const SITE = {
+  title: "Documentation",
+  description: "Your website description.",
+  defaultLanguage: "en_US",
+};
+
+export const OPEN_GRAPH = {
+  image: {
+    src: "https://github.com/withastro/astro/blob/main/assets/social/banner.jpg?raw=true",
+    alt:
+      "astro logo on a starry expanse of space," +
+      " with a purple saturn-like planet floating in the right foreground",
+  },
+  twitter: "astrodotbuild",
+};
+
+export const SIDEBAR: Sidebar = {
+  leftSidebar: {
+    items: [
+      { header: true, text: "Intro", collapsible: true },
+      { text: "Astro", link: "astro" },
+      { text: "React", link: "react" },
+      { text: "Svelte", link: "svelte" },
+      { text: "Vue", link: "vue" },
+      { text: "Page 5", link: "page-5" },
+      { text: "Page 6", link: "page-6" },
+      { header: true, text: "Quick start", collapsible: false },
+      { text: "Page 7", link: "page-7" },
+      { text: "Page 8", link: "page-8" },
+    ],
+  },
+  rightSidebar: {
+    tableOfContentHeaders: ["h1", "h2", "h3"],
+  },
+};
+
+export const NAVBAR: NavBar = {
+  title: "Nitro",
+  logo: {
+    src: "/logo.svg",
+    width: 40,
+    height: 30,
+    alt: "Nitro logo",
+  },
+  items: [
+    {
+      href: "https://github.com/EiffelFly/nitro",
+      className: "github-social-link",
+      icon: {
+        src: "/github.svg",
+        width: 24,
+        height: 24,
+        alt: "github icon",
+      },
+      position: "right",
+    },
+    {
+      position: "left",
+      to: "/blog",
+      label: "Blog",
+    },
+  ],
+};
