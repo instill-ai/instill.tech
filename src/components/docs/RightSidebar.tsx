f257fcb7921636d990a7fe1691465a9a90a53bda
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-27 15:30:04 +0800

    refactor: refactor RightSidebar style

--- src/components/docs/RightSidebar.tsx
+++ src/components/docs/RightSidebar.tsx
@@ -1,26 +1,20 @@
 import { docsBaseStyles } from "@/style/docsBaseStyle";
-import { Sidebar } from "@/types/docs";
 import cn from "clsx";
 
-import TableOfContent from "./TableOfContent";
+import TableOfContent, { TableOfContentProps } from "./TableOfContent";
 
 export type RightSidebarProps = {
-  headers: any[];
-  rightSidebar: Sidebar["rightSidebar"];
+  headers: TableOfContentProps["headers"];
 };
 
-const RightSidebar = ({ headers, rightSidebar }: RightSidebarProps) => {
+const RightSidebar = ({ headers }: RightSidebarProps) => {
   return (
     <nav
       className={cn("sticky w-full", docsBaseStyles.sidebar.topOffset)}
       aria-labelledby="grid-right"
     >
       <div className="h-full overflow-auto">
-        <TableOfContent
-          headers={headers}
-          headerMinDepth={rightSidebar.headerMinDepth}
-          headerMaxDepth={rightSidebar.headerMaxDepth}
-        />
+        <TableOfContent headers={headers} />
       </div>
     </nav>
   );
--- src/components/docs/TableOfContent.tsx
+++ src/components/docs/TableOfContent.tsx
@@ -1,35 +1,28 @@
 export type TableOfContentProps = {
-  headers: any[];
-  headerMinDepth: number;
-  headerMaxDepth: number;
+  headers: {
+    slug: string;
+    text: string;
+  }[];
 };
 
-const TableOfContent = ({
-  headers,
-  headerMinDepth,
-  headerMaxDepth,
-}: TableOfContentProps) => {
+const TableOfContent = ({ headers }: TableOfContentProps) => {
   return (
     <>
       <h2 className="mb-4">On this page</h2>
       <ul>
-        {headers
-          .filter(
-            ({ depth }) => depth >= headerMinDepth && depth <= headerMaxDepth
-          )
-          .map((header) => (
-            <li
-              key={header.slug}
-              className="group my-2.5 border-l-2 border-slate-300 py-0.5 pl-4 text-sm hover:border-slate-700"
+        {headers.map((header) => (
+          <li
+            key={header.slug}
+            className="group my-2.5 border-l-2 border-slate-300 py-0.5 pl-4 text-sm hover:border-slate-700"
+          >
+            <a
+              className="block truncate text-slate-600 group-hover:text-slate-900"
+              href={`#${header.slug}`}
             >
-              <a
-                className="block truncate text-slate-600 group-hover:text-slate-900"
-                href={`#${header.slug}`}
-              >
-                {header.text}
-              </a>
-            </li>
-          ))}
+              {header.text}
+            </a>
+          </li>
+        ))}
       </ul>
     </>
   );
