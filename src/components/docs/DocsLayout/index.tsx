11929988345b90d2774a7caac6f98b782fc1d3c2
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-27 15:30:24 +0800

    feat: implement getHeader function at DocsLayout

--- src/components/docs/DocsLayout/index.tsx
+++ src/components/docs/DocsLayout/index.tsx
@@ -1,11 +1,11 @@
-import { Children, FC, useMemo } from "react";
+import { FC, useState, useEffect } from "react";
 import LeftSidebar from "../LeftSidebar";
 import { SIDEBAR, NAVBAR } from "../../../../docs.config";
 import { useRouter } from "next/router";
 import RightSidebar from "../RightSidebar";
 import Navbar from "../Navbar";
 import { Frontmatter } from "@/types/docs";
-import { renderToString } from "react-dom/server";
+import { docsBaseStyles } from "@/style/docsBaseStyle";
 
 export type DocsLayoutProps = {
   meta: Frontmatter;
@@ -13,11 +13,34 @@ export type DocsLayoutProps = {
 
 const DocsLayout: FC<DocsLayoutProps> = ({ children, meta }) => {
   const router = useRouter();
+  const [headers, setHeaders] = useState<{ slug: string; text: string }[]>([]);
+
+  useEffect(() => {
+    let newHeaders = [];
+    const headersEl = document.querySelectorAll(
+      SIDEBAR.rightSidebar.tableOfContentHeaders
+        .map((e) => `#grid-main ${e}`)
+        .join(",")
+    );
+
+    headersEl.forEach((el) => {
+      newHeaders.push({
+        slug: `#${el.id}`,
+        text: el.textContent,
+      });
+    });
+
+    setHeaders(newHeaders);
+  }, []);
 
   return (
     <>
-      <Navbar navbar={NAVBAR} />
-      <main className="mx-auto grid grid-flow-col grid-cols-12 gap-x-5">
+      <Navbar
+        navbar={NAVBAR}
+        maxWidth="max-w-[1440px]"
+        marginBottom={docsBaseStyles.navbarMarginBottom}
+      />
+      <main className="mx-auto grid max-w-[1440px] grid-flow-col grid-cols-12 gap-x-10">
         <aside
           id="grid-left"
           className="col-span-2 ml-10"
@@ -28,7 +51,7 @@ const DocsLayout: FC<DocsLayoutProps> = ({ children, meta }) => {
             currentPagePath={router.pathname}
           />
         </aside>
-        <div id="grid-main" className="col-span-8 mx-auto max-w-4xl">
+        <div id="grid-main" className="prose col-span-8 mx-auto max-w-4xl">
           {children}
         </div>
         <aside
@@ -36,7 +59,7 @@ const DocsLayout: FC<DocsLayoutProps> = ({ children, meta }) => {
           className="grid-sidebar col-span-2 mr-10"
           title="Table of Contents"
         >
-          <RightSidebar headers={[]} rightSidebar={SIDEBAR.rightSidebar} />
+          <RightSidebar headers={headers} />
         </aside>
       </main>
     </>
