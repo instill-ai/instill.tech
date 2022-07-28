888eb07642bc5379433a818c5406ae7492903ce0
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-28 16:43:49 +0800

    refactor: refactor how career folder export its components

--- src/components/career/index.ts
+++ src/components/career/index.ts
@@ -1,6 +1,18 @@
 import CareerGeneralIntro from "./CareerGeneralIntro";
 import CareerHero from "./CareerHero";
+import { CareerPositionDetailsSection } from "./CareerPositionDetailsSection";
+import type { CareerPositionDetailsSectionProps } from "./CareerPositionDetailsSection";
+import { CareerPositionListSection } from "./CareerPositionListSection";
+import type { CareerPositionListSectionProps } from "./CareerPositionListSection";
 
-export * from "./CareerPositionDetailsSection";
-export * from "./CareerPositionListSection";
-export { CareerGeneralIntro, CareerHero };
+export {
+  CareerGeneralIntro,
+  CareerHero,
+  CareerPositionDetailsSection,
+  CareerPositionListSection,
+};
+
+export type {
+  CareerPositionDetailsSectionProps,
+  CareerPositionListSectionProps,
+};
