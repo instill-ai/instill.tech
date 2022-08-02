1926ffb34efd8a5392ad445c12702aec4f2341ae
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-02 19:27:05 +0800

    fix: fix navbar top style not correct issue

--- src/styles/docsBaseStyle.ts
+++ src/styles/docsBaseStyle.ts
@@ -1,10 +1,10 @@
 export const docsBaseStyles = {
   // Remember to change scroll-margin-top at index.css
   navbarHeight: "h-20",
-  navbarMarginBottom: "mb-10",
+  navbarMarginBottom: "mb-12",
 
   sidebar: {
-    topOffset: "top-30",
+    topOffset: "top-32",
     hoveredItemBgColor: "bg-blue-200",
     currentItemBgColor: "bg-blue-500",
     currentItemTextColor: "text-black",
