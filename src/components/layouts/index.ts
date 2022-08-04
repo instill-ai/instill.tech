89bfc7aeff4033569481b4c31a85a8d0cdcacf13
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-04 16:17:39 +0800

    feat: remove layout folder

--- src/components/layouts/index.ts
+++
@@ -1,5 +0,0 @@
-import PolicyPageLayout from "./PolicyPageLayout";
-import type { PolicyPageLayoutProps } from "./PolicyPageLayout";
-
-export { PolicyPageLayout };
-export type { PolicyPageLayoutProps };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,9 +17,6 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
-
-  console.log(Component.getLayout);
-
   return (
     <AnnouncementBarCtxProvider>
       {getLayout(<Component {...pageProps} />)}
