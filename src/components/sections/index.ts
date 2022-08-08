071468650500b5e8a8581deea0a711245ffb1d10
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-08 21:02:43 +0800

    chore: remove NewsletterArchiveSection

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,8 +1,5 @@
 import OurMembersSection from "./OurMembersSection";
 import type { OurMembersSectionProps } from "./OurMembersSection";
 
-import NewsletterArchiveSection from "./NewsletterArchiveSection";
-import type { NewsletterArchiveSectionProps } from "./NewsletterArchiveSection";
-
-export { OurMembersSection, NewsletterArchiveSection };
-export type { OurMembersSectionProps, NewsletterArchiveSectionProps };
+export { OurMembersSection };
+export type { OurMembersSectionProps };
