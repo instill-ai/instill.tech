e0862342b48de89585c73875fa202f400525d16f
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-08 15:18:42 +0800

    feat: center doc content (#169)
    * feat: center the main content when the screen exceed 1440px
    
    * refactor: refactor LeftSidebar folder structure

--- src/components/docs/DocsLayout.tsx
+++ src/components/docs/DocsLayout.tsx
@@ -12,6 +12,11 @@ export type DocsLayoutProps = {
   meta: Frontmatter;
 };
 
+// When screen exceed min-width:1440px we want to center the docs-content,  make left sidebar
+// expand to the left edge of the docs-content. In order to accomplish this task, left sidebar's
+// position will be fixed at the leftside and have its width determined by the max-width of docs
+// container
+
 const DocsLayout: FC<DocsLayoutProps> = ({ children, meta }) => {
   const router = useRouter();
   const [headers, setHeaders] = useState<{ slug: string; text: string }[]>([]);
@@ -37,15 +42,23 @@ const DocsLayout: FC<DocsLayoutProps> = ({ children, meta }) => {
 
   return (
     <>
-      <style jsx>{``}</style>
+      <style jsx>
+        {`
+          @media screen and (min-width: 1440px) {
+            .docs-left-sidebar {
+              width: calc((100vw - 1140px) / 2);
+            }
+          }
+        `}
+      </style>
       <PageHead
         pageTitle={`${meta.title} | Documentation`}
         pageDescription={meta.description}
       />
-      <main className="mx-auto grid grid-flow-col grid-cols-12 gap-x-10">
+      <main className="mx-auto grid grid-flow-col grid-cols-12 gap-x-10 max:flex">
         <aside
           className={cn(
-            "fixed md:sticky h-full md:flex top-0 md:col-span-4 z-30 bg-instillGrey05 transform md:transform-none transition-transform",
+            "docs-left-sidebar fixed md:sticky h-full md:flex md:col-span-4 max:fixed top-0 z-30 bg-instillGrey05 transform md:transform-none transition-transform",
             leftSidebarIsOpen ? "translate-x-0" : "-translate-x-full"
           )}
           title="Site Navigation"
@@ -64,14 +77,14 @@ const DocsLayout: FC<DocsLayoutProps> = ({ children, meta }) => {
           />
         ) : null}
 
-        <div className="flex flex-col col-span-12 md:col-span-8 mr-auto pb-40 w-full max-w-[1140px]">
+        <div className="docs-content flex flex-col col-span-12 md:grid-col-8 max:mx-auto pb-40 w-full max-w-[1140px]">
           <Nav
             setLeftSidebarIsOpen={setLeftSidebarIsOpen}
             navbar={NAVBAR}
             marginBottom="md:mb-4"
           />
           <div className="grid grid-cols-8">
-            <div className="col-span-8 xl:col-span-6 px-8">
+            <div className="col-span-8 xl:col-span-6 pl-8 max:pl-16 pr-8">
               <h1 className="font-sans font-semibold text-3xl mb-10">
                 {meta.title}
               </h1>
