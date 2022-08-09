3e5785cd39ad7787034705d0d9febfc34e845041
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-09 16:46:57 +0800

    refactor: refactor lib/mailchimp

---
+++ src/lib/mailchimp/index.ts
@@ -0,0 +1,27 @@
+import {
+  addMemberIntoMailchimpList,
+  removePlaceholderAndFooterWords,
+  removeMailchimpStyleAndMeta,
+} from "./mailchimp";
+
+import type {
+  Recipient,
+  Campaign,
+  ListCampaignsResponse,
+  GetCampaignContentResponse,
+  NewsletterPublicCampaign,
+} from "./types";
+
+export {
+  addMemberIntoMailchimpList,
+  removePlaceholderAndFooterWords,
+  removeMailchimpStyleAndMeta,
+};
+
+export type {
+  Recipient,
+  Campaign,
+  ListCampaignsResponse,
+  GetCampaignContentResponse,
+  NewsletterPublicCampaign,
+};
