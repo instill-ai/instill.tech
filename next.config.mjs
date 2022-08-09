08c02a6119f4f23fa35dfd36f7b929b5d7265c70
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-09 20:52:08 +0800

    feat: update code snippet color schema

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { h } from "hastscript";
 
 const theme = JSON.parse(
-  await readFile(new URL("./src/styles/slack-dark.json", import.meta.url))
+  await readFile(new URL("./src/styles/rose-pine-moon.json", import.meta.url))
 );
 
 const withMDX = mdx({
