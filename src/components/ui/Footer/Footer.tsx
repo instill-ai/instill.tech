a000c8dbb3fcb2784dce08925ea7aaf1e7150418
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-13 22:47:32 +0800

    feat: update info block style (#227)
    Because
    
    - We have a new, shiny, cool info block style!
    
    This commit
    
    - Update info block style
    - Fix footer's flex box issue

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -13,25 +13,25 @@ const Footer = () => {
           marginBottom="mb-5"
           borderColor="border-instillGrey70"
         />
-        <div className="flex flex-col gap-y-[60px] sm:gap-y-10">
+        <div className="flex flex-col space-y-[60px] sm:space-y-10">
           <div className="flex flex-col">
             <h3 className="instill-text-h3 mb-5 max-w-[256px] text-instillGrey05">
               Visual Data Preparation Made for All
             </h3>
             <Logo type="whiteLogomark" width={30} />
           </div>
-          <div className="flex flex-col-reverse gap-y-[30px] sm:grid sm:grid-cols-3 sm:gap-y-0">
+          <div className="flex flex-col-reverse space-y-[30px] sm:grid sm:grid-cols-3 sm:space-y-0">
             <CopyRightText />
-            <div className="flex flex-col gap-y-[60px] sm:hidden">
+            <div className="flex flex-col space-y-[60px] sm:hidden">
               <SocialLinks styleName="sm:justify-center" />
-              <div className="flex flex-row gap-x-5 sm:justify-end">
+              <div className="flex flex-row space-x-5 sm:justify-end">
                 <CookiePolicyLink />
                 <div className="my-1.5 border-r border-instillGrey15" />
                 <PrivacyPolicyLink />
               </div>
             </div>
             <SocialLinks styleName="hidden sm:flex sm:justify-center" />
-            <div className="hidden gap-x-2 sm:flex sm:flex-row sm:justify-end md:gap-x-5">
+            <div className="hidden space-x-2 sm:flex sm:flex-row sm:justify-end md:space-x-5">
               <CookiePolicyLink />
               <div className="my-1.5 max-h-[16px] border-r border-instillGrey15" />
               <PrivacyPolicyLink />
