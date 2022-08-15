0b24f2edaea9da874d1add6c2982d89c19b292aa
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-08-15 20:30:38 +0800

    feat: highlight current hash at table of content

--- src/components/docs/RightSidebar/TableOfContent.tsx
+++ src/components/docs/RightSidebar/TableOfContent.tsx
@@ -1,4 +1,7 @@
-import { MouseEvent, useCallback } from "react";
+import cn from "clsx";
+import { useEffect, useState } from "react";
+import { Nullable } from "@/types/instill";
+import { useRouter } from "next/router";
 
 export type TableOfContentProps = {
   headers: {
@@ -9,15 +12,25 @@ export type TableOfContentProps = {
 };
 
 const TableOfContent = ({ headers }: TableOfContentProps) => {
-  // const handleClick = useCallback(
-  //   (e: MouseEvent<HTMLAnchorElement>, slug: string) => {
-  //     //e.preventDefault();
-  //     document.querySelector(`#${slug}`).scrollIntoView({
-  //       behavior: "smooth",
-  //     });
-  //   },
-  //   []
-  // );
+  const router = useRouter();
+  const [currentHash, setCurrentHash] = useState<Nullable<string>>(null);
+
+  useEffect(() => {
+    const handleHashChange = (event: HashChangeEvent) => {
+      setCurrentHash(event.newURL.split("#")[1]);
+    };
+
+    window.addEventListener("hashchange", handleHashChange);
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!router.isReady) return;
+
+    setCurrentHash(router.asPath.split("#")[1]);
+  }, [router]);
 
   return (
     <div className="flex flex-col mb-10">
@@ -26,12 +39,23 @@ const TableOfContent = ({ headers }: TableOfContentProps) => {
         {headers.map((header) => (
           <li
             key={header.slug}
-            className="group my-2.5 border-l-2 border-slate-300 py-0.5 pl-4 text-sm hover:border-instillBlue50"
+            className={cn(
+              "group border-l-2 my-1.5 py-0.5 pl-[15px] text-sm hover:border-instillBlue50",
+              currentHash === header.slug
+                ? "border-instillBlue50"
+                : "border-instillGrey50"
+            )}
           >
             <a
-              className="block truncate text-instillGrey50 group-hover:text-instillBlue50"
+              className={cn(
+                "block truncate group-hover:text-instillBlue50",
+                currentHash === header.slug
+                  ? "text-instillBlue50"
+                  : "text-instillGrey50"
+              )}
               href={`#${header.slug}`}
-              style={{ paddingLeft: `${(header.depth - 1) * 10}px` }}
+              style={{ paddingLeft: `${(header.depth - 2) * 12}px` }}
+              onClick={() => setCurrentHash(header.slug)}
             >
               {header.text}
             </a>
