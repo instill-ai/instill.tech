2e6a68476520901b407907243a56c650edaf0682
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-09-23 14:06:46 +0800

    feat: Implement new footer design (edit) (#272)
    Because
    
    - We have a new Footer design!
    
    This commit
    
    - Implement new footer design

--- src/components/ui/CopyRightText.tsx
+++ src/components/ui/CopyRightText.tsx
@@ -1,9 +1,7 @@
-const CopyRightText = () => {
+export const CopyRightText = () => {
   return (
     <div className="instill-text-body text-instillGrey30">
       © 2022 Instill AI Ltd.
     </div>
   );
 };
-
-export default CopyRightText;
