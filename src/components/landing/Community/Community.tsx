07e5b3be8fba7b00a7590e8912b3c83ac79a6e3a
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-10-03 16:56:00 +0800

    feat: update community section

--- src/components/landing/Community/Community.tsx
+++ src/components/landing/Community/Community.tsx
@@ -1,4 +1,5 @@
 import cn from "clsx";
+import SectionLabel from "../SectionLabel";
 import CommunityBlock from "./CommunityBlock";
 
 export type CommunityProps = {
@@ -7,17 +8,19 @@ export type CommunityProps = {
 
 const Community = ({ marginBottom }: CommunityProps) => {
   return (
-    <div className={cn("flex flex-col", marginBottom)}>
-      <div className="mb-10 flex flex-col">
-        <div className="font-sans text-lg font-normal uppercase text-instillBlue50">
-          Follow us
-        </div>
-        <h2 className="mb-5 font-mono text-4xl font-medium text-instillGrey90">
-          Built by the community
+    <div className={cn("flex flex-col bg-instillGrey90 p-10", marginBottom)}>
+      <div className="mb-[200px] flex  flex-col">
+        <SectionLabel
+          text="follow us"
+          position="mr-auto"
+          marginBottom="mb-2.5"
+        />
+        <h2 className="mb-5 font-mono text-4xl font-medium text-white">
+          Built by and for the community
         </h2>
-        <p className="text-lg font-normal text-instillGrey90">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo amet
-          purus mattis ac, tempus tellus.
+        <p className="w-7/12 text-lg font-normal text-white">
+          Join 100+ Data/AI practitioners discussing VDP in action and help
+          building the modern ETL infrastructure for unstructured visual data.
         </p>
       </div>
       <div className="grid grid-cols-3 gap-x-6">
@@ -29,12 +32,12 @@ const Community = ({ marginBottom }: CommunityProps) => {
         />
         <CommunityBlock
           name="Discord"
-          title="Join our Dicord"
+          title="Join the discussion on Dicord"
           linkText="Join the community"
           link=""
         />
         <CommunityBlock
-          name="GitHub"
+          name="Twitter"
           title="Keep updated on Twitter"
           linkText="Follow us"
           link=""
