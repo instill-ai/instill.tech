813099f99c5e9ee11289915b2693313f8ccb3dd9
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-10-21 17:06:21 +0800

    refactor: rename SelectWrapper to ControlSelectWrapper

--- src/components/landing/CaseStudy/ControlSelectWrapper.tsx
+++ src/components/landing/CaseStudy/ControlSelectWrapper.tsx
@@ -9,13 +9,17 @@ export type ControlSelectWrapperProps = {
   label: string;
   isActive: boolean;
   options: SingleSelectOption[];
+  disabled: boolean;
+  onFocus?: () => void;
 };
 
 const ControlSelectWrapper = ({
   id,
   label,
   isActive,
+  disabled,
   options,
+  onFocus,
 }: ControlSelectWrapperProps) => {
   return (
     <div className="relative">
@@ -26,15 +30,18 @@ const ControlSelectWrapper = ({
         label={label}
         options={options}
         value={options[0]}
+        onFocus={onFocus}
         required={true}
-        disabled={true}
+        disabled={disabled}
       />
-      <div
-        className={cn(
-          "absolute top-0 bottom-0 right-0 left-0 border-2",
-          isActive ? "border-instillLightGreen" : "border-instillGrey20"
-        )}
-      />
+      {disabled ? (
+        <div
+          className={cn(
+            "absolute top-0 bottom-0 right-0 left-0 border-2",
+            isActive ? "border-instillLightGreen" : "border-instillGrey20"
+          )}
+        />
+      ) : null}
     </div>
   );
 };
