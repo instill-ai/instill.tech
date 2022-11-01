00950b2cc77e8e0d4a010024ab93bff1b1d4abce
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-01 14:03:12 +0800

    feat: cleanup VDP mobile view

--- src/components/landing/Vdp/Vdp.tsx
+++ src/components/landing/Vdp/Vdp.tsx
@@ -1,7 +1,9 @@
+import { useRef } from "react";
 import { VdpLogo } from "@instill-ai/design-system";
 import cn from "clsx";
+
+import { useRefPosition } from "@/hooks/useRefPosition";
 import Image from "next/future/image";
-import { useEffect, useRef, useState } from "react";
 import SectionLabel from "../SectionLabel";
 import VdpFlow from "./VdpFlow";
 
@@ -11,47 +13,45 @@ export type VdpProps = {
 
 const Vdp = ({ marginBottom }: VdpProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const [containerWidth, setContainerWidth] = useState(0);
-
-  useEffect(() => {
-    const updateContainerWidth = () => {
-      setContainerWidth(containerRef.current.offsetWidth);
-    };
-
-    updateContainerWidth();
-    window.addEventListener("resize", updateContainerWidth);
-    return () => {
-      window.removeEventListener("resize", updateContainerWidth);
-    };
-  }, []);
+  const containerDimension = useRefPosition(containerRef, {
+    listenWindowResize: true,
+  });
 
   return (
     <div className={cn("flex flex-col py-10", marginBottom)} ref={containerRef}>
+      <div className="mb-10 flex flex-col">
+        <SectionLabel
+          text="Visual data preperation"
+          position="mr-auto"
+          marginBottom="mb-2.5"
+        />
+        <div className="flex flex-row">
+          <h2 className="w-full font-mono text-4xl text-instillGrey90 md:w-7/12 xl:text-5xl">
+            Fastest way to build end-to-end visual data ETL pipelines
+          </h2>
+          <div className="hidden w-5/12 xl:flex">
+            <VdpLogo type="expand" width={340} position="ml-auto my-auto" />
+          </div>
+        </div>
+      </div>
       <div className="mb-10 flex">
         <VdpLogo
           type="expand"
-          width={containerWidth > 360 ? 320 : containerWidth * 0.8}
+          width={
+            containerDimension
+              ? containerDimension.width > 360
+                ? 320
+                : containerDimension.width * 0.8
+              : 0
+          }
           position="mr-auto my-auto xl:hidden"
         />
-      </div>
-      <SectionLabel
-        text="Visual data preperation"
-        position="mr-auto"
-        marginBottom="mb-2.5"
-      />
-      <div className="mb-[30px] flex flex-row">
-        <h2 className="w-full font-mono text-4xl text-instillGrey90 md:w-7/12 xl:text-5xl">
-          Fastest way to build end-to-end visual data ETL pipelines
-        </h2>
-        <div className="hidden w-5/12 xl:flex">
-          <VdpLogo type="expand" width={340} position="ml-auto my-auto" />
-        </div>
       </div>
       <VdpFlow marginBottom="mb-5" />
       <Image
         src="/images/vdp-flow.png"
-        width={containerWidth}
-        height={containerWidth * 0.54}
+        width={containerDimension ? containerDimension.width : 0}
+        height={containerDimension ? containerDimension.width * 0.54 : 0}
         sizes="100vw"
         alt="The flow of VDP"
       />
