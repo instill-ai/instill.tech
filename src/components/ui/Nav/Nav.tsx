c11f7d74b5e6514e1f537b99112ca7f8c9e5bd83
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-01 15:25:48 +0800

    feat: clean up Nav mobile view

--- src/components/ui/Nav/Nav.tsx
+++ src/components/ui/Nav/Nav.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import cn from "clsx";
-import Image from "next/future/image";
-import { CrossIcon, Logo } from "@instill-ai/design-system";
+import { CrossIcon, Logo, MenuIcon } from "@instill-ai/design-system";
 import Link from "next/link";
 
 import { AnnouncementBar } from "@/components/ui";
@@ -31,11 +30,39 @@ const Nav = () => {
     </>
   );
 
-  return (
-    <div
-      id="navbar-test"
-      className="flex w-full flex-col bg-white lg:sticky lg:top-0 lg:z-50"
-    >
+  const desktopView = (
+    <>
+      {enableAnnouncementBar && (
+        <AnnouncementBar>
+          <p className="hidden text-instillGrey95 ">
+            Check out our new article &nbsp;
+            <a
+              className="mr-1.5 italic underline"
+              target="_blank"
+              rel="noreferrer noopener"
+              href="https://blog.instill.tech/introducing-vdp/?utm_source=product&utm_medium=banner"
+            >
+              Introducing VDP: open-source visual data ETL
+            </a>
+          </p>
+        </AnnouncementBar>
+      )}
+      <div className="mx-auto flex max-w-[1440px] flex-row py-5 px-[30px] ">
+        <Link href="/">
+          <a className="my-auto mr-auto flex">
+            <Logo type="ColourLogomarkBlackType" width={158} />
+          </a>
+        </Link>
+
+        <div className="flex flex-row justify-end gap-x-[60px] lg:ml-auto">
+          {navbarLinkGroup}
+        </div>
+      </div>
+    </>
+  );
+
+  const mobileView = (
+    <>
       {enableAnnouncementBar && (
         <AnnouncementBar>
           <p className="text-instillGrey95">
@@ -51,59 +78,57 @@ const Nav = () => {
           </p>
         </AnnouncementBar>
       )}
-      <div className="mx-auto flex w-full max-w-[1440px]">
-        <div className="hidden w-full content-center py-5 px-[30px] lg:flex lg:flex-row">
-          <Link href="/">
-            <a className="my-auto mr-auto flex">
-              <Logo type="ColourLogomarkBlackType" width={158} />
-            </a>
-          </Link>
-
-          <div className="flex flex-row justify-end gap-x-[60px] lg:ml-auto">
+      <div className="flex w-full flex-row p-4 lg:mb-[60px]">
+        <Link href="/">
+          <a className="flex">
+            <Logo type="ColourLogomarkBlackType" width={159} />
+          </a>
+        </Link>
+        <button
+          className="ml-auto flex h-[30px] w-[30px] rounded-[3px] hover:bg-instillGrey30 hover:bg-opacity-10"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? (
+            <CrossIcon
+              width="w-6"
+              height="h-6"
+              color="fill-instillGrey90"
+              position="m-auto"
+            />
+          ) : (
+            <MenuIcon
+              width="w-6"
+              height="h-6"
+              color="fill-instillGrey90"
+              position="m-auto"
+            />
+          )}
+        </button>
+      </div>
+      <div className="relative w-full">
+        {open && (
+          <div className="absolute top-0 left-0 flex w-full flex-col gap-y-[50px] bg-white py-[50px] px-4">
             {navbarLinkGroup}
           </div>
-        </div>
+        )}
+      </div>
+    </>
+  );
+
+  return (
+    <div
+      id="navbar-test"
+      className="flex w-full flex-col bg-white lg:sticky lg:top-0 lg:z-50"
+    >
+      <div className="flex w-full flex-col">
+        <div className="hidden w-full lg:flex lg:flex-row">{desktopView}</div>
         <div
           className={cn(
             "flex h-full w-full flex-col lg:hidden",
-            open ? "fixed top-0 left-0 z-50 bg-instillGrey95" : ""
+            open ? "fixed top-0 left-0 z-50 bg-white" : ""
           )}
         >
-          <div className="flex w-full flex-row p-4 lg:mb-[60px]">
-            <Link href="/">
-              <a className="flex">
-                <Logo type="ColourLogomarkWhiteType" width={159} />
-              </a>
-            </Link>
-            <button
-              className="ml-auto flex h-[30px] w-[30px] rounded-[3px] hover:bg-instillGrey30 hover:bg-opacity-10"
-              onClick={() => setOpen(!open)}
-            >
-              {open ? (
-                <CrossIcon
-                  width="w-6"
-                  height="h-6"
-                  color="fill-instillGrey05"
-                  position="m-auto"
-                />
-              ) : (
-                <Image
-                  src="/images/menu.svg"
-                  alt="Navbar menu icon"
-                  width={30}
-                  height={30}
-                  sizes="30px"
-                />
-              )}
-            </button>
-          </div>
-          <div className="relative w-full">
-            {open && (
-              <div className="absolute top-0 left-0 flex w-full flex-col gap-y-[50px] bg-instillGrey95 py-[50px] px-4">
-                {navbarLinkGroup}
-              </div>
-            )}
-          </div>
+          {mobileView}
         </div>
       </div>
     </div>
