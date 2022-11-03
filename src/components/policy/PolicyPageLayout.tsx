c05bdad6d967c877332effaff345556272feff44
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-03 14:48:28 +0800

    feat: update policy page (#317)
    This commit updates policy page layout to follow the new design. (#273 ) It also fixes a display bug of announcement bar.

--- src/components/policy/PolicyPageLayout.tsx
+++ src/components/policy/PolicyPageLayout.tsx
@@ -17,18 +17,18 @@ const PolicyPageLayout = ({ meta, children }: PolicyPageLayoutProps) => {
         pageDescription={meta.description}
         pageType="main"
       />
-      <div className="flex w-full bg-instillGrey95">
+      <div className="flex w-full bg-white">
         <div className="max-w-[1440px] md:mx-auto md:grid md:w-10/12 md:grid-cols-3">
-          <article className="prose prose-white max-w-none mx-5 md:mx-0 mt-[180px] mb-[60px] md:col-span-2">
+          <article className="prose mx-5 mt-[180px] mb-[60px] max-w-none md:col-span-2 md:mx-0">
             {children}
           </article>
           <div className="relative md:col-span-1 md:mt-[180px]">
             <div className="mb-40 flex flex-col gap-y-2 px-4 pt-40 md:sticky md:top-0 md:mb-0 md:px-16">
-              <p className="text-instillGrey05">
+              <p className="text-instillGrey90">
                 We&apos;re happy to hear from you. Get in touch at
               </p>
               <a
-                className="flex text-instillBlue50"
+                className="flex text-instillSkyBlue"
                 href="mailto:hello@instill.tech"
               >
                 hello@instill.tech
