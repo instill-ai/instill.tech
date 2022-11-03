c65f16966f4d9a53a6e685088d55be8324be38ac
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-03 14:30:26 +0800

    refactor: refactor the export of component/landing

--- src/components/landing/index.ts
+++ src/components/landing/index.ts
@@ -1,42 +1,18 @@
-import Hero from "./Hero";
-import { HeroProps } from "./Hero";
-import InstillCloud from "./InstillCloud";
-import Community from "./Community";
-import Faq from "./Faq/Faq";
-import { FaqProps } from "./Faq/Faq";
-import CodeShowcase from "./CodeShowcase";
-import type { CodeShowcaseProps } from "./CodeShowcase";
-import Vdp from "./Vdp";
-import type { VdpProps } from "./Vdp";
-import HowItWorks from "./HowItWorks";
-import type { HowItWorksProps } from "./HowItWorks";
-import NoCodeInterface from "./NoCodeInterface";
-import type { NoCodeInterfaceProps } from "./NoCodeInterface";
-import CaseStudy from "./CaseStudy";
-import type { CaseStudyProps } from "./CaseStudy";
-import FaqHeader from "./FaqHeader";
-import { FaqHeaderProps } from "./FaqHeader";
-
-export {
-  Features,
-  Hero,
-  InstillCloud,
-  Community,
-  FaqHeader,
-  Faq,
-  CodeShowcase,
-  Vdp,
-  HowItWorks,
-  NoCodeInterface,
-  CaseStudy,
-};
-export type {
-  HeroProps,
-  FaqHeaderProps,
-  FaqProps,
-  CodeShowcaseProps,
-  VdpProps,
-  HowItWorksProps,
-  NoCodeInterfaceProps,
-  CaseStudyProps,
-};
+export { Hero } from "./Hero";
+export type { HeroProps } from "./Hero";
+export { InstillCloud } from "./InstillCloud";
+export { Community } from "./Community";
+export { Faq } from "./Faq/Faq";
+export type { FaqProps } from "./Faq/Faq";
+export { CodeShowcase } from "./CodeShowcase";
+export type { CodeShowcaseProps } from "./CodeShowcase";
+export { Vdp } from "./Vdp";
+export type { VdpProps } from "./Vdp";
+export { HowItWorks } from "./HowItWorks";
+export type { HowItWorksProps } from "./HowItWorks";
+export { NoCodeInterface } from "./NoCodeInterface";
+export type { NoCodeInterfaceProps } from "./NoCodeInterface";
+export { CaseStudy } from "./CaseStudy";
+export type { CaseStudyProps } from "./CaseStudy";
+export { FaqHeader } from "./FaqHeader";
+export type { FaqHeaderProps } from "./FaqHeader";
