5ab8b6cf7488c7ca2f3dea691961443a9c176fa9
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-07 15:27:11 +0800

    refactor: refactor Faq related componenet breakpoint

--- src/components/landing/FaqHeader/FaqHeader.tsx
+++ src/components/landing/FaqHeader/FaqHeader.tsx
@@ -8,7 +8,7 @@ export type FaqHeaderProps = {
 export const FaqHeader = ({ marginBottom }: FaqHeaderProps) => {
   return (
     <div className={cn("relative w-full", marginBottom)}>
-      <FaqText width="sm:w-10/12 md:w-8/12 xl:w-6/12" />
+      <FaqText width="w-10/12 xl:w-6/12" />
       <div className="absolute top-0 left-0 right-0 h-12 bg-instillGrey90 xs:h-16" />
     </div>
   );
