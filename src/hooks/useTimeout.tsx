02c4ac303903a6866a2eace53804bd858bc1b45a
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-08 15:38:03 +0800

    feat: add useTimeout hook

---
+++ src/hooks/useTimeout.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from "react";
+
+// Referece: https://usehooks-ts.com/react-hook/use-timeout
+
+export const useTimeout = (callback: () => void, delay: number) => {
+  const savedCallback = useRef(callback);
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    const tick = () => {
+      savedCallback.current();
+    };
+
+    if (delay !== null) {
+      let timeout = setTimeout(tick, delay);
+      return () => clearTimeout(timeout);
+    }
+  }, [delay]);
+};
