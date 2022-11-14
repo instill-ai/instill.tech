1a1cb71d9d70cb43c29a3a804ce4ad37f98c24bc
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-14 21:20:04 +0800

    feat: add ImgWithFallback

---
+++ src/components/ui/ImgWithFallback.tsx
@@ -0,0 +1,31 @@
+import Image from "next/future/image";
+import { ReactElement, useState } from "react";
+
+export type ImgWithFallbackProps = {
+  src: string;
+  fallbackImg: ReactElement;
+  alt: string;
+  width: number;
+  height: number;
+};
+
+export const ImgWithFallback = ({
+  src,
+  fallbackImg,
+  alt,
+  width,
+  height,
+}: ImgWithFallbackProps) => {
+  const [error, setError] = useState(false);
+  return error ? (
+    fallbackImg
+  ) : (
+    <Image
+      src={src}
+      width={width}
+      height={height}
+      alt={alt}
+      onError={() => setError(true)}
+    />
+  );
+};
