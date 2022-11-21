39f18b6ed2826f3eca593432295d88f0df22dbaa
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-21 19:56:27 +0800

    feat: lazy load footer

--- src/components/ui/PageBase.tsx
+++ src/components/ui/PageBase.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import dynamic from "next/dynamic";
 
 import { Nav } from "@/components/ui";
+import { useInView } from "react-intersection-observer";
 
 const Footer = dynamic(() =>
   import("@/components/ui").then((mod) => mod.Footer)
@@ -12,11 +13,15 @@ export type PageBaseProps = {
 };
 
 export const PageBase = ({ children }: PageBaseProps) => {
+  const [footerRef, footerIsInView] = useInView({
+    triggerOnce: true,
+  });
+
   return (
     <div className="flex min-h-screen flex-col bg-white">
       <Nav />
       <div className="flex flex-1 flex-col">{children}</div>
-      <Footer />
+      <div ref={footerRef}>{footerIsInView ? <Footer /> : null}</div>
     </div>
   );
 };
