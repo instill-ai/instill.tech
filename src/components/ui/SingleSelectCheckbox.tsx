674a709ae9d416f0d67e974fcef1ccdad4f75f4f
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-11-30 15:35:23 +0800

    feat: impl SingleSelectCheckbox style

--- src/components/ui/SingleSelectCheckbox.tsx
+++ src/components/ui/SingleSelectCheckbox.tsx
@@ -1,26 +1,34 @@
-import { Nullable } from "@/types/instill";
-import { Dispatch, SetStateAction } from "react";
+import cn from "clsx";
 
 export type SingleSelectCheckboxProps = {
   options: { label: string; value: string }[];
-  setValue: Dispatch<SetStateAction<Nullable<string>>>;
-  value: Nullable<string>;
+  id: string;
+  setValue: (value: string) => void;
+  value: string;
 };
 
 export const SingleSelectCheckbox = ({
+  id,
   options,
   value,
   setValue,
 }: SingleSelectCheckboxProps) => {
   return (
-    <div>
+    <div className="flex flex-col gap-y-5">
       {options.map((e) => (
-        <div key={e.label}>
-          <input
-            type="checkbox"
-            checked={e.value === value}
-            onChange={() => setValue(e.value)}
-          />
+        <div key={e.label} className="flex flex-row gap-x-5">
+          <div
+            className="my-auto flex min-h-[24px] min-w-[24px] bg-instillGrey05"
+            onClick={() => setValue(e.value)}
+          >
+            <input
+              type="checkbox"
+              checked={e.value === value}
+              onChange={() => setValue(e.value)}
+              className="m-auto h-4 w-4 appearance-none rounded-none bg-instillGrey05 outline-none checked:bg-instillSkyBlue"
+            />
+          </div>
+          <p>{e.label}</p>
         </div>
       ))}
     </div>
