56874a32b89bac094c5f08ddcaa31c278f22f811
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-12-01 19:53:56 +0800

    feat: impl mobile view in TutorialFilter

--- src/components/tutorial/TutorialFilters/TutorialFilters.tsx
+++ src/components/tutorial/TutorialFilters/TutorialFilters.tsx
@@ -1,20 +1,30 @@
+import cn from "clsx";
+import {
+  Dispatch,
+  ReactElement,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { FilterIcon, XIcon } from "@instill-ai/design-system";
+
 import { SingleSelectCheckboxProps } from "@/components/ui";
-import { TutorialMeta } from "@/types/instill";
-import { Dispatch, SetStateAction, useMemo } from "react";
-import { TutorialFilter, TutorialFilterProps } from "./TutorialFilter";
+import { getCvTaskIconAndLabel } from "@/lib/instill";
+import { CvTask, Nullable, TutorialMeta } from "@/types/instill";
+import { TutorialLabel } from "../TutorialLabel";
+import { TutorialFilter } from "./TutorialFilter";
+import { useWindowSize } from "@/hooks/useWindowSize";
+
+type Filters = {
+  cvTask: CvTask | "All";
+  connector: string;
+};
 
 export type TutorialFiltersProps = {
   tutorials: TutorialMeta[];
-  filters: {
-    cvTask: string;
-    connector: string;
-  };
-  setFilters: Dispatch<
-    SetStateAction<{
-      cvTask: string;
-      connector: string;
-    }>
-  >;
+  filters: Filters;
+  setFilters: Dispatch<SetStateAction<Filters>>;
 };
 
 export const TutorialFilters = ({
@@ -22,9 +32,11 @@ export const TutorialFilters = ({
   filters,
   setFilters,
 }: TutorialFiltersProps) => {
+  const windowSize = useWindowSize();
+
   const connectorOptions = useMemo<SingleSelectCheckboxProps["options"]>(() => {
     let options: SingleSelectCheckboxProps["options"] = [
-      { label: "all", value: "all" },
+      { label: "All", value: "All" },
     ];
 
     for (const tutorial of tutorials) {
@@ -54,44 +66,157 @@ export const TutorialFilters = ({
     return options;
   }, [tutorials]);
 
+  const cvTaskFilterLabel = useMemo<{
+    icon: Nullable<ReactElement>;
+    label: string;
+  }>(() => {
+    if (filters.cvTask === "All") {
+      return {
+        icon: null,
+        label: "All",
+      };
+    } else {
+      return getCvTaskIconAndLabel({
+        cvTask: filters.cvTask,
+        iconStyle: {
+          color: "fill-instillGrey80",
+          width: "w-5",
+          height: "h-5",
+          position: "my-auto",
+        },
+      });
+    }
+  }, [filters]);
+
+  const [filterIsOpen, setFilterIsOpen] = useState(false);
+
+  // When window width is bigger than our max width(1127px) we keep
+  // the filter open and close the mobile filter every time the width
+  // changed below 1127px
+
+  useEffect(() => {
+    if (windowSize?.width) {
+      if (windowSize.width > 1127) {
+        setFilterIsOpen(true);
+      } else {
+        setFilterIsOpen(false);
+      }
+    }
+  }, [windowSize?.width]);
+
   return (
-    <div className="flex flex-col gap-y-10">
-      <TutorialFilter
-        id="cvTask"
-        title="CV Task"
-        value={filters.cvTask || ""}
-        setValue={(value) => setFilters((prev) => ({ ...prev, cvTask: value }))}
-        options={[
-          { label: "all", value: "all" },
-          { label: "Image Classification", value: "imageClassification" },
-          { label: "Instance Segmentation", value: "instanceSegmentation" },
-          {
-            label: "Keypoint Detection (Pose Estimation)",
-            value: "keypointDetection",
-          },
-          {
-            label: "Object Detection",
-            value: "objectDetection",
-          },
-          {
-            label: "OCR (optical character recognition)",
-            value: "ocr",
-          },
-          {
-            label: "Semantic Segmentation",
-            value: "semanticSegmentation",
-          },
-        ]}
-      />
-      <TutorialFilter
-        id="connector"
-        title="Connector"
-        value={filters.connector || ""}
-        setValue={(value) =>
-          setFilters((prev) => ({ ...prev, connector: value }))
-        }
-        options={connectorOptions}
-      />
-    </div>
+    <>
+      {/* 
+        This component is complicated. It serves two things, the first one
+        is the normal filter view when the window size is bigger than 1127.
+        The second one is when at the mobile view (width < 1127px) It need 
+        to act like a full screen popup. You could separate these two by 
+        observing the breakpoint xl.
+      */}
+      <div
+        className={cn(
+          "fixed top-0 left-0 z-[60] flex h-screen w-full flex-col gap-y-10 bg-white py-5 px-4 xl:static xl:z-auto xl:h-full xl:px-0 xl:py-0",
+          filterIsOpen ? "" : "hidden"
+        )}
+      >
+        <div
+          className="flex w-full cursor-pointer xl:hidden"
+          onClick={() => setFilterIsOpen(false)}
+        >
+          <XIcon
+            width="w-[30px]"
+            height="h-[30px]"
+            color="fill-instillGrey90"
+            position="ml-auto"
+          />
+        </div>
+        <TutorialFilter
+          id="cvTask"
+          title="CV Task"
+          value={filters.cvTask || ""}
+          setValue={(value) =>
+            setFilters((prev) => ({
+              ...prev,
+              cvTask: value as Filters["cvTask"],
+            }))
+          }
+          options={[
+            { label: "All", value: "All" },
+            { label: "Image Classification", value: "imageClassification" },
+            { label: "Instance Segmentation", value: "instanceSegmentation" },
+            {
+              label: "Keypoint Detection (Pose Estimation)",
+              value: "keypointDetection",
+            },
+            {
+              label: "Object Detection",
+              value: "objectDetection",
+            },
+            {
+              label: "OCR (optical character recognition)",
+              value: "ocr",
+            },
+            {
+              label: "Semantic Segmentation",
+              value: "semanticSegmentation",
+            },
+          ]}
+        />
+        <TutorialFilter
+          id="connector"
+          title="Connector"
+          value={filters.connector || ""}
+          setValue={(value) =>
+            setFilters((prev) => ({ ...prev, connector: value }))
+          }
+          options={connectorOptions}
+        />
+      </div>
+
+      {/* 
+        This is the button that can open filter popup at mobile view
+        and the list of the current filters.
+      */}
+
+      <div className="mb-20 flex flex-col xl:hidden">
+        <div
+          className="mb-2.5 flex flex-row gap-x-2"
+          onClick={() => setFilterIsOpen(true)}
+        >
+          <div className="my-auto flex">
+            <FilterIcon
+              width="w-[30px]"
+              height="h-[30px]"
+              color="fill-instillGrey90"
+            />
+          </div>
+          <h3 className="inline-flex items-center text-instillGrey90 text-instill-h3-medium">
+            Filter
+          </h3>
+        </div>
+        <div className="flex flex-col gap-y-2.5">
+          <div className="flex flex-row gap-x-5">
+            <p className="font-sans text-base font-medium">CV task:</p>
+            <TutorialLabel
+              icon={cvTaskFilterLabel.icon || undefined}
+              label={cvTaskFilterLabel.label}
+              labelTextStyle="font-mono text-xs font-normal text-instillGrey80"
+              labelBgColor="bg-instillGrey05"
+              labelPadding="py-1 px-2"
+            />
+          </div>
+          <div className="flex flex-row gap-x-5">
+            <p className="font-sans text-base font-medium">Connector:</p>
+            <TutorialLabel
+              icon={undefined}
+              label={filters.connector}
+              labelTextStyle="font-mono text-xs font-normal text-instillGrey80"
+              labelBgColor="bg-instillGrey05"
+              labelPadding="py-1 px-2"
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
