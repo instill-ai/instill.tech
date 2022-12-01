4cac7f74872c95214687a77af3cfc12c14041faa
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-12-01 19:55:20 +0800

    feat: make PageHero more flexible

--- src/components/ui/PageHero.tsx
+++ src/components/ui/PageHero.tsx
@@ -6,6 +6,9 @@ export type PageHeroProps = {
   marginBottom?: string;
   subHeadline?: ReactNode;
   ctaButton?: ReactNode;
+  width?: string;
+  position?: string;
+  headerColor?: string;
 };
 
 export const PageHero = ({
@@ -13,15 +16,25 @@ export const PageHero = ({
   headline,
   subHeadline,
   ctaButton,
+  width,
+  position,
+  headerColor,
 }: PageHeroProps) => {
   return (
     <div
       className={cn(
-        "mx-auto flex w-full flex-col gap-y-10 xl:w-8/12",
-        marginBottom
+        "flex flex-col gap-y-10",
+        marginBottom,
+        width ? width : "w-full xl:w-8/12",
+        position ? position : "mx-auto"
       )}
     >
-      <h1 className="w-full text-left font-mono text-5xl font-semibold uppercase text-instillSkyBlue xl:text-instill-h1">
+      <h1
+        className={cn(
+          "w-full text-left font-mono text-5xl font-semibold uppercase xl:text-instill-h1",
+          headerColor ? headerColor : "text-instillSkyBlue"
+        )}
+      >
         {headline}
       </h1>
       <div className="font-sans text-lg font-light text-instillGrey70 xl:text-2xl">
