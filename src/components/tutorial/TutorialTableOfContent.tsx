35598ee10a992af485f185298106dba9edcc3f62
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-12-06 14:49:23 +0800

    feat: add maxWidth prop into TutorialTableOfContent

--- src/components/tutorial/TutorialTableOfContent.tsx
+++ src/components/tutorial/TutorialTableOfContent.tsx
@@ -1,16 +1,19 @@
+import cn from "clsx";
 import { useRouter } from "next/router";
 import { ContributeLinks, TableOfContent, TableOfContentProps } from "../ui";
 
 export type TutorialTableOfContentProps = {
   headers: TableOfContentProps["headers"];
+  maxWidth?: string;
 };
 
 export const TutorialTableOfContent = ({
   headers,
+  maxWidth,
 }: TutorialTableOfContentProps) => {
   const router = useRouter();
   return (
-    <div className="sticky top-[160px] pr-4">
+    <div className={cn("sticky top-[160px] pr-4", maxWidth)}>
       <div className="flex h-full flex-col overflow-auto">
         <TableOfContent headers={headers} />
         <ContributeLinks
