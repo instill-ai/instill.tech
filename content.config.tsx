2b3840418e9c883f9da6b95ff172601bd9e154f8
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-12-22 19:50:13 +0800

    feat: add blog and tutorial link in docs

--- content.config.tsx
+++ content.config.tsx
@@ -153,6 +153,20 @@ const NAV: NavConfig = {
   },
   items: [
     {
+      key: "docs-nav-blog",
+      to: "/blog",
+      className: "",
+      position: "right",
+      label: "Blog",
+    },
+    {
+      key: "docs-nav-tutorials",
+      to: "/tutorials",
+      className: "",
+      position: "right",
+      label: "Tutorial",
+    },
+    {
       key: "docs-nav-product-webiste",
       to: "/",
       className: "",
