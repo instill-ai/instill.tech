93eb4af4ee72cbedd3f171c14b2086a22d009946
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-12-22 19:50:47 +0800

    feat: adjust Hero style

--- src/components/landing/Hero/Hero.tsx
+++ src/components/landing/Hero/Hero.tsx
@@ -16,7 +16,7 @@ export const Hero = ({ scrollHandler }: HeroProps) => {
   return (
     <div className="flex min-h-[90vh] flex-col justify-center px-4 xl:px-0">
       <div className="flex flex-col-reverse xl:mb-[120px] xl:flex-row xl:gap-y-0 xl:gap-x-12">
-        <div className="flex flex-col xl:my-auto xl:w-[57%]">
+        <div className="flex flex-col xl:my-auto xl:w-[64%]">
           <Headline marginBottom="mb-5" />
           <SubHeadline marginBottom="mb-10" />
           <div className="flex flex-col gap-y-5 xl:flex-row xl:gap-x-5 xl:gap-y-0">
@@ -24,7 +24,7 @@ export const Hero = ({ scrollHandler }: HeroProps) => {
             <DiscordCtaButton />
           </div>
         </div>
-        <div className="mb-20 xl:my-auto xl:mb-0 xl:w-[43%]">
+        <div className="mb-20 xl:my-auto xl:w-[36%]">
           <HeroAnimation />
         </div>
       </div>
