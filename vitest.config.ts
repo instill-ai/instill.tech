6ae2db009d07f03796b3f30200b312b8b8f5ec3f
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-16 14:29:59 +0800

    feat: update test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitest/config";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -6,6 +7,11 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    setupFiles: ["./setupTests.ts"],
+    setupFiles: "./setupTests.ts",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
