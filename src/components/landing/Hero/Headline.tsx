99ce28949e9c2ed092c4e4263bf92cdc36427f71
Xiaofei Du <xiaofei.du@instill.tech>
2023-05-02 00:13:45 +0100

    chore: update headline

--- src/components/landing/Hero/Headline.tsx
+++ src/components/landing/Hero/Headline.tsx
@@ -12,7 +12,7 @@ export const Headline = ({ marginBottom }: HeadlineProps) => {
         marginBottom
       )}
     >
-      Open-source Unstructured Data Infrastructure
+      Build Unstructured Data Pipelines 10x Faster
     </h1>
   );
 };
