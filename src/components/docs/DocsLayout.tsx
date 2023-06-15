fd43f1ed267886e7ade04fa89b49a6f88d616dcb
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-15 19:52:05 +0800

    fix: fix DocsLayout leftSidebar and Footer style

--- src/components/docs/DocsLayout.tsx
+++ src/components/docs/DocsLayout.tsx
@@ -52,40 +52,49 @@ export const DocsLayout = ({ children }: DocsLayoutProps) => {
         min-h-screen and min-w-screen are essential to make the page had 
         correct height and width
       */}
-      <main className="min-w-screen grid min-h-screen grid-flow-col grid-cols-12 bg-white dark:bg-instillGrey90 max:block">
-        <aside
-          className={cn(
-            "docs-left-sidebar fixed top-0 z-30 transform bg-instillGrey05 transition-transform dark:bg-instillGrey95 md:sticky md:col-span-3 md:flex md:transform-none",
-            leftSidebarIsOpen ? "translate-x-0" : "-translate-x-full"
-          )}
-        >
-          <LeftSidebar leftSidebar={docsConfigration.sidebar.leftSidebar} />
-        </aside>
+      <main className="min-w-screen relative min-h-screen bg-white dark:bg-instillGrey90 max:block">
+        <div className="grid h-full w-full grid-flow-col grid-cols-12">
+          <aside
+            className={cn(
+              "docs-left-sidebar fixed top-0 z-30 transform bg-instillGrey05 transition-transform dark:bg-instillGrey95 md:sticky md:col-span-3 md:flex md:transform-none max:fixed",
+              leftSidebarIsOpen ? "translate-x-0" : "-translate-x-full"
+            )}
+          >
+            <LeftSidebar leftSidebar={docsConfigration.sidebar.leftSidebar} />
+          </aside>
 
-        {/* 
+          {/* 
           Filler area outside of LeftSidebar at mobile view.
         */}
 
-        {leftSidebarIsOpen ? (
-          <div
-            onClick={() => setLeftSidebarIsOpen((prev) => !prev)}
-            className="fixed top-0 bottom-0 left-0 right-0 z-20 bg-instillGrey70 opacity-80"
-          />
-        ) : null}
+          {leftSidebarIsOpen ? (
+            <div
+              onClick={() => setLeftSidebarIsOpen((prev) => !prev)}
+              className="fixed top-0 bottom-0 left-0 right-0 z-20 bg-instillGrey70 opacity-80"
+            />
+          ) : null}
 
-        {/* 
+          {/* 
           The main content of the documentation.
         */}
 
-        <div className="docs-content col-span-12 flex flex-col pb-40 md:col-span-9 max:col-span-12">
-          <Nav
-            setLeftSidebarIsOpen={setLeftSidebarIsOpen}
-            nav={docsConfigration.nav}
-          />
-          {children}
+          <div className="docs-content col-span-12 flex flex-col pb-40 md:col-span-9 max:col-span-12">
+            <Nav
+              setLeftSidebarIsOpen={setLeftSidebarIsOpen}
+              nav={docsConfigration.nav}
+            />
+            {children}
+          </div>
+        </div>
+
+        {/* 
+          In order to have correct stacking context, we wrap the footer in a relative position 
+          div element.
+        */}
+        <div className="relative z-50 w-full">
+          <Footer />
         </div>
       </main>
-      <Footer />
     </>
   );
 };
