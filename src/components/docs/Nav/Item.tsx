6bd6ac076dad66be4e137eba751d460baf122044
Naman Anand <iamnamananand996@gmail.com>
2023-06-19 13:47:24 +0530

    feat: active routes highlight INS-880

--- src/components/docs/Nav/Item.tsx
+++ src/components/docs/Nav/Item.tsx
@@ -6,12 +6,16 @@ import {
   isInteriorLink,
   NavbarItem,
 } from "@/types/docs";
+import { useRouter } from "next/router";
+import cn from "clsx";
+import { getApplicationType, isRouterActive } from "@/lib/instill";
 
 export type ItemProps = {
   item: NavbarItem;
 };
 
 export const Item = ({ item }: ItemProps) => {
+  const router = useRouter();
   if (isBorder(item)) {
     return (
       <div className="grid grid-cols-1 py-2 md:grid-cols-2">
@@ -23,7 +27,12 @@ export const Item = ({ item }: ItemProps) => {
   if (isInteriorLink(item)) {
     return (
       <a
-        className="my-auto text-sm font-normal text-black hover:text-instillBlue50 dark:text-instillGrey15 dark:hover:text-instillBlue50"
+        className={cn(
+          "my-auto text-sm font-normal  hover:text-instillBlue50 dark:text-instillGrey15 dark:hover:text-instillBlue50",
+          isRouterActive(router.asPath, item.to)
+            ? "text-instillBlue50"
+            : "text-black"
+        )}
         href={item.to}
       >
         {item.label}
--- src/lib/instill/getApplicationType.ts
+++
@@ -1,9 +0,0 @@
-export function getApplicationType(router: string | Array<string>): string {
-  if (router.includes("vdp")) {
-    return "vdp";
-  }
-  if (router.includes("instill-cloud")) {
-    return "instill-cloud";
-  }
-  return "vdp";
-}
