9b89a5fce11cfd7e0fb56b870e41bf3baec1db51
iamnamananand996 <iamnamananand996@gmail.com>
2023-09-01 16:01:32 +0530

    chore: update

--- src/components/ui/ContributeLinks.tsx
+++ src/components/ui/ContributeLinks.tsx
@@ -1,16 +1,12 @@
-import * as React from "react";
-import { useTranslation } from "next-i18next";
-
 export type ContributeLinksProps = {
   githubEditUrl: string;
 };
 
 export const ContributeLinks = ({ githubEditUrl }: ContributeLinksProps) => {
-  const { t } = useTranslation();
   return (
     <div className="flex flex-col">
       <h2 className="mb-4 font-semibold text-instillGrey95 dark:text-instillGrey15">
-        {t("common:contribution")}
+        Contribution
       </h2>
       <a
         href={githubEditUrl}
@@ -26,7 +22,7 @@ export const ContributeLinks = ({ githubEditUrl }: ContributeLinksProps) => {
           <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
         </svg>
         <p className="my-auto text-sm text-instillGrey50 group-hover:text-instillBlue50">
-          {t("common:editThisPage")}
+          Edit this page
         </p>
       </a>
     </div>
