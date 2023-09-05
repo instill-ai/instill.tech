17c57f46a54bae638a2c46790cf5ebf2c100444a
iamnamananand996 <iamnamananand996@gmail.com>
2023-09-05 18:13:58 +0530

    chore: update code-hike version

--- next.config.mjs
+++ next.config.mjs
@@ -67,6 +67,8 @@ const nextConfig = {
     //   require("./lib/generate-sitemap");
     // }
 
+    config.resolve.fallback = { fs: false };
+
     if (!dev) {
       config.plugins.push(
         new BundleAnalyzerPlugin({
@@ -77,18 +79,6 @@ const nextConfig = {
     }
 
     return config;
-  },
-  output: "standalone",
-
-  // We need to ignore the babelrc.json at the root, that is for storybook
-  experimental: {
-    forceSwcTransforms: true,
-
-    // We need this line to make our code-hike snippet work under Next.js standalone server
-    // https://github.com/code-hike/codehike/issues/283
-    outputFileTracingIncludes: {
-      "/docs/[..path]": ["node_modules/.pnpm/**/shiki/**/*.json"],
-    },
   },
   async redirects() {
     return [
