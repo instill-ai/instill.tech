df48aa6262c8e800bd85fb1b9dd0437ace3b56e3
iamnamananand996 <iamnamananand996@gmail.com>
2023-09-26 00:02:08 +0530

    chore: update commit metadata response

--- src/pages/docs/[...path].tsx
+++ src/pages/docs/[...path].tsx
@@ -157,7 +157,7 @@ export const getStaticProps: GetStaticProps<DocsPageProps> = async ({
     commitMeta = await getCommitMeta({
       org: "instill-ai",
       repo: "instill.tech",
-      path: "docs/" + relativePath + ".mdx",
+      path: "docs/" + relativePath + "." + locale + ".mdx",
     });
   } catch (err) {
     console.log(err);
