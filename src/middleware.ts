c2a2317b61b7156cdb72a2bd65639cfbd0103eb7
iamnamananand996 <iamnamananand996@gmail.com>
2023-10-16 23:22:51 +0530

    chore: update middleware as async operation

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,26 +3,26 @@ import type { NextRequest } from "next/server";
 import { VERSIONS } from "../version.mjs";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/docs/vdp/latest")) {
     const redirectUrl = request.url.replace("latest", VERSIONS["vdp"]);
-    return NextResponse.redirect(new URL(redirectUrl, request.url));
+    return await NextResponse.redirect(new URL(redirectUrl, request.url));
   }
   if (request.nextUrl.pathname.startsWith("/docs/base/latest")) {
     const redirectUrl = request.url.replace("latest", VERSIONS["base"]);
-    return NextResponse.redirect(new URL(redirectUrl, request.url));
+    return await NextResponse.redirect(new URL(redirectUrl, request.url));
   }
   if (request.nextUrl.pathname.startsWith("/docs/core/latest")) {
     const redirectUrl = request.url.replace("latest", VERSIONS["core"]);
-    return NextResponse.redirect(new URL(redirectUrl, request.url));
+    return await NextResponse.redirect(new URL(redirectUrl, request.url));
   }
   if (request.nextUrl.pathname.startsWith("/docs/cloud/latest")) {
     const redirectUrl = request.url.replace("latest", VERSIONS["cloud"]);
-    return NextResponse.redirect(new URL(redirectUrl, request.url));
+    return await NextResponse.redirect(new URL(redirectUrl, request.url));
   }
   if (request.nextUrl.pathname.startsWith("/docs/model/latest")) {
     const redirectUrl = request.url.replace("latest", VERSIONS["model"]);
-    return NextResponse.redirect(new URL(redirectUrl, request.url));
+    return await NextResponse.redirect(new URL(redirectUrl, request.url));
   }
 }
 
