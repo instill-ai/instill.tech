ede920878c84f28d0c859159055a3f6e95a6ed01
Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
2023-10-23 20:25:09 +0530

    Update TutorialSearch.tsx

--- src/components/tutorial/TutorialSearch.tsx
+++ src/components/tutorial/TutorialSearch.tsx
@@ -49,7 +49,7 @@ export const TutorialSearch = ({
       />
       <div className="flex w-full flex-row">
         <a
-          href="https://github.com/instill-ai/instill.tech/issues/new?assignees=xiaofei-du,EiffelFly&labels=request+a+tutorial"
+          href="https://github.com/instill-ai/community/issues/new?assignees=xiaofei-du,EiffelFly&labels=request+a+tutorial"
           className="px-2.5 py-[5px] font-sans text-lg font-normal text-instillGrey70 hover:text-instillSkyBlue"
         >
           Request a tutorial
@@ -59,7 +59,7 @@ export const TutorialSearch = ({
           <div className="block w-full" />
         </div>
         <a
-          href="https://github.com/instill-ai/instill.tech/issues/new?assignees=xiaofei-du,EiffelFly&labels=submit+a+tutorial"
+          href="https://github.com/instill-ai/community/issues/new?assignees=xiaofei-du,EiffelFly&labels=submit+a+tutorial"
           className="px-2.5 py-[5px] font-sans text-lg font-normal text-instillGrey70 hover:text-instillSkyBlue"
         >
           Submit a tutorial
