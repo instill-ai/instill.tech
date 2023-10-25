8645c801cdcd94c40cf7b1a8e454ee4def61620d
Utsav Paul <91927689+Smartmind12@users.noreply.github.com>
2023-10-25 23:06:16 +0530

    Update ArticleRightSidebar.tsx

--- src/components/ui/ArticleRightSidebar.tsx
+++ src/components/ui/ArticleRightSidebar.tsx
@@ -25,6 +25,7 @@ export const ArticleRightSidebar = ({
             "https://github.com/instill-ai/instill.tech/edit/main" +
             router.asPath +
             ".mdx"
+          }
           feedbackUrl={
             "https://github.com/instill-ai/community/issues/new/choose"
           }
