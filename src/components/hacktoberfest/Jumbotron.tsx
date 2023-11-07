f3ff8b248a2a66f13c35aee52c3fa15d6f30d24f
iamnamananand996 <iamnamananand996@gmail.com>
2023-11-07 15:07:32 +0530

    chore: jumbotron form initial changes

--- src/components/hacktoberfest/Jumbotron.tsx
+++ src/components/hacktoberfest/Jumbotron.tsx
@@ -18,7 +18,7 @@ import {
 } from "swiper/modules";
 import SwiperButtonNext from "@/components/hacktoberfest/SwiperButtonNext";
 import SwiperButtonPrev from "@/components/hacktoberfest/SwiperButtonPrev";
-import { Button, Icons } from "@instill-ai/design-system";
+import { Button, Icons, Input } from "@instill-ai/design-system";
 
 export default function Jumbotron() {
   const swiper = useSwiper();
@@ -64,30 +64,140 @@ export default function Jumbotron() {
           </Button>
         </SwiperButtonPrev>
         <SwiperSlide>
-          <div className="h-[700px] w-[500px] rounded-sm bg-white p-6">
-            <h3 className="font-semibold leading-none">Llava-13B</h3>
+          <div className="h-[660px] w-[500px] rounded-sm border-md border-semantic-accent-default bg-white p-6 ">
+            <h3 className="mb-3 font-semibold leading-none">Llava-13B</h3>
+            <p className="text-sm text-zinc-500 dark:text-zinc-600">
+              Create and inspire using the worlds fastest growing open source AI
+              platform
+            </p>
+
+            <div className="my-6 flex gap-x-4">
+              <Input.Root className="w-full">
+                <Input.Core
+                  disabled={false}
+                  type="text"
+                  placeholder="Send a message"
+                  value="Tell me a short story"
+                />
+              </Input.Root>
+              <Button variant="primary" size="lg">
+                Genrate
+              </Button>
+            </div>
+
+            <div>
+              <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-[700px] w-[500px] rounded-sm bg-white p-6">
+          <div className="h-[660px] w-[500px] rounded-sm border-md border-semantic-accent-default bg-white p-6">
             <h3 className="font-semibold leading-none">Llama2-7B-chat</h3>
+            <p className="text-sm text-zinc-500 dark:text-zinc-600">
+              Create and inspire using the worlds fastest growing open source AI
+              platform
+            </p>
+
+            <div className="my-6 flex gap-x-4">
+              <Input.Root className="w-full">
+                <Input.Core
+                  disabled={false}
+                  type="text"
+                  placeholder="Send a message"
+                  value="Tell me a short story"
+                />
+              </Input.Root>
+              <Button variant="primary" size="lg">
+                Genrate
+              </Button>
+            </div>
+
+            <div>
+              <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-[700px] w-[500px] rounded-sm bg-white p-6">
+          <div className="h-[660px] w-[500px] rounded-sm border-md border-semantic-accent-default bg-white p-6">
             <h3 className="font-semibold leading-none">
               Stability AI + Open AI for Sticker
             </h3>
+            <p className="text-sm text-zinc-500 dark:text-zinc-600">
+              Create and inspire using the worlds fastest growing open source AI
+              platform
+            </p>
+
+            <div className="my-6 flex gap-x-4">
+              <Input.Root className="w-full">
+                <Input.Core
+                  disabled={false}
+                  type="text"
+                  placeholder="Send a message"
+                  value="Tell me a short story"
+                />
+              </Input.Root>
+              <Button variant="primary" size="lg">
+                Genrate
+              </Button>
+            </div>
+
+            <div>
+              <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-[700px] w-[500px] rounded-sm bg-white p-6">
+          <div className="h-[660px] w-[500px] rounded-sm border-md border-semantic-accent-default bg-white p-6">
             <h3 className="font-semibold leading-none">YOLOv7</h3>
+            <p className="text-sm text-zinc-500 dark:text-zinc-600">
+              Create and inspire using the worlds fastest growing open source AI
+              platform
+            </p>
+
+            <div className="my-6 flex gap-x-4">
+              <Input.Root className="w-full">
+                <Input.Core
+                  disabled={false}
+                  type="text"
+                  placeholder="Send a message"
+                  value="Tell me a short story"
+                />
+              </Input.Root>
+              <Button variant="primary" size="lg">
+                Genrate
+              </Button>
+            </div>
+
+            <div>
+              <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-[700px] w-[500px] rounded-sm bg-white p-6">
+          <div className="h-[660px] w-[500px] rounded-sm border-md border-semantic-accent-default bg-white p-6">
             <h3 className="font-semibold leading-none">SEO article writer</h3>
+            <p className="text-sm text-zinc-500 dark:text-zinc-600">
+              Create and inspire using the worlds fastest growing open source AI
+              platform
+            </p>
+
+            <div className="my-6 flex gap-x-4">
+              <Input.Root className="w-full">
+                <Input.Core
+                  disabled={false}
+                  type="text"
+                  placeholder="Send a message"
+                  value="Tell me a short story"
+                />
+              </Input.Root>
+              <Button variant="primary" size="lg">
+                Genrate
+              </Button>
+            </div>
+
+            <div>
+              <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperButtonNext>
