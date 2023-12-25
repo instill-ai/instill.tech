1f9461b5c326a62d06c724bb349b3027f05b3310
iamnamananand996 <iamnamananand996@gmail.com>
2023-12-26 01:33:26 +0530

    chore: partners images update

--- src/components/about/OurPartner.tsx
+++ src/components/about/OurPartner.tsx
@@ -7,15 +7,36 @@ export const OurPartner = () => {
         <p className="font-sans text-[24px] font-bold leading-9 text-gray-950">
           Partnerships
         </p>
-        <div>
-          <img src={"images/social/"} alt="" className="h-16" />
+        <div className="flex flex-row">
+          <img
+            src={"images/social/number-protocol.svg"}
+            alt=""
+            className="h-16"
+          />
+          <img src={"images/social/huggingface.svg"} alt="" className="h-16" />
+          <img src={"images/social/google.svg"} alt="" className="h-16" />
         </div>
       </div>
       <div className="w-1/2 py-16 pr-16">
         <p className="font-sans text-[24px] font-bold leading-9 text-gray-950">
           Backed by
         </p>
-        <div></div>
+        <div className="flex flex-row gap-x-5">
+          <img
+            src={"images/social/hive-vertures.svg"}
+            alt=""
+            className="h-16"
+          />
+          <img src={"images/social/rtp-global.svg"} alt="" className="h-16" />
+        </div>
+        <div className="flex flex-row gap-x-5">
+          <img
+            src={"images/social/lunar-ventures.svg"}
+            alt=""
+            className="h-16"
+          />
+          <img src={"images/social/venture.svg"} alt="" className="h-16" />
+        </div>
       </div>
     </div>
   );
