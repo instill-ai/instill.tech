acbcf3890e7a8e579444fabd9e2e057f6d878151
iamnamananand996 <iamnamananand996@gmail.com>
2024-01-08 16:31:50 +0530

    chore: responderWithSpeech

--- src/lib/jumbotron-sdk/index.ts
+++ src/lib/jumbotron-sdk/index.ts
@@ -2,10 +2,12 @@ import { llava7b } from "./llava7b";
 import { seoArticle } from "./seoArticle";
 import { stabilityAIOpenAISticker } from "./stabilityAIOpenAISticker";
 import { yolov7 } from "./yolov7";
+import { responderWithSpeech } from "./responderWithSpeech";
 
 export const JumbotronSDK = {
   llava7b,
   seoArticle,
   stabilityAIOpenAISticker,
   yolov7,
+  responderWithSpeech,
 };
