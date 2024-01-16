82b8b41dcffa8b9e236bde7c1c8f8461594bccc1
iamnamananand996 <iamnamananand996@gmail.com>
2024-01-16 19:40:32 +0530

    chore: add producthunt link

--- src/components/landing/Hero/Hero.tsx
+++ src/components/landing/Hero/Hero.tsx
@@ -30,6 +30,21 @@ export const Hero = ({ scrollHandler }: HeroProps) => {
               Free, until you’re ready to upgrade
             </p>
           </div>
+          <div className="my-5">
+            <a
+              href="https://www.producthunt.com/posts/instill-vdp?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-instill-vdp"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=434600&theme=light"
+                alt="Instill VDP - Open-Source Unstructured Data ETL for AI-first applications | Product Hunt"
+                style={{ width: "250px", height: "54px" }}
+                width="250"
+                height="54"
+              />
+            </a>
+          </div>
         </div>
         <div className="mb-20 mt-14 xl:my-auto xl:mt-0 xl:w-[50%]">
           <Jumbotron />
