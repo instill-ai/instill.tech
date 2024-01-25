da8bafefd219378dc62cc4bcc794cdb6984a44dd
iamnamananand996 <iamnamananand996@gmail.com>
2024-01-26 01:35:22 +0530

    chore: update css for blog page

--- src/components/landing/jumbotron/AskOnPage.tsx
+++ src/components/landing/jumbotron/AskOnPage.tsx
@@ -150,7 +150,7 @@ export const AskOnPage = () => {
           <a
             href="https://console.instill.tech/instill-wombat/pipelines/jumbotron-ask-on-page"
             target="_blank"
-            className="absolute bottom-3 right-6 z-30 inline-flex items-center gap-x-2 divide-x divide-zinc-100/10 rounded bg-zinc-800/80 p-0 px-2 text-sm text-white drop-shadow-2xl backdrop-blur hover:text-blue-500"
+            className="absolute bottom-3 right-6 z-30 inline-flex items-center gap-x-2 divide-x divide-zinc-100/10 rounded bg-zinc-800/80 p-0 px-2 text-sm font-normal text-white no-underline drop-shadow-2xl backdrop-blur hover:text-blue-500"
           >
             <svg
               focusable="false"
