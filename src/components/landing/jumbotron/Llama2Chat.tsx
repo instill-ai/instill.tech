5b7a0324ff1727ca8548504b8f0295c942df59da
Xiaofei Du <xiaofei.du@instill.tech>
2024-03-13 15:44:42 +0000

    fix: fix jumbotron pipeline links

--- src/components/landing/jumbotron/Llama2Chat.tsx
+++ src/components/landing/jumbotron/Llama2Chat.tsx
@@ -370,7 +370,7 @@ export const Llama2Chat = () => {
         </div>
         <div className="mt-5 flex justify-end">
           <a
-            href="https://www.instill.tech/instill-wombat/pipelines/jumbotron-visual-understanding"
+            href="https://instill.tech/instill-wombat/pipelines/jumbotron-visual-understanding"
             target="_blank"
             className="absolute bottom-3 right-6 z-30 inline-flex items-center gap-x-2 divide-x divide-zinc-100/10 rounded bg-zinc-800/80 p-0 px-2 text-sm font-normal text-white no-underline drop-shadow-2xl backdrop-blur hover:text-blue-500 "
           >
