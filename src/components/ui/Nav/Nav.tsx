cb5c6de95729fd797e2f15c5530c07213279fb3f
iamnamananand996 <iamnamananand996@gmail.com>
2024-04-30 16:02:04 +0530

    feat: web navbar

--- src/components/ui/Nav/Nav.tsx
+++ src/components/ui/Nav/Nav.tsx
@@ -3,9 +3,13 @@ import { useRouter } from "next/router";
 import cn from "clsx";
 import Link from "next/link";
 import {
+  Button,
   CrossIcon,
+  Icons,
   Logo,
   MenuIcon,
+  Menubar,
+  Separator,
   SolidButton,
 } from "@instill-ai/design-system";
 import { AnnouncementBar, CommonCtaButton } from "@/components/ui";
@@ -50,28 +54,61 @@ export const Nav = () => {
 
   const navbarLinkGroup = (
     <React.Fragment>
-      <HubPageLink />
-      <AboutPageLink />
-      <BlogPageLink />
-      <ChangelogLink />
-      <DocsPageLink />
-      <GithubTextLink />
-      <LoginPageLink />
-
       <div className="relative my-auto">
-        <SolidButton
-          type="button"
-          color={"primary"}
-          padding="px-[17px] py-[7px] transition duration-700 ease-in-out xl:hover-animation"
-          itemGapX="gap-x-5"
-          onClickHandler={() =>
+        <Link
+          href="/pricing"
+          className="flex gap-x-2 text-[16px] font-semibold text-semantic-fg-secondary"
+        >
+          <p className="my-auto">Products</p>
+          <Icons.ChevronDown className="my-auto h-4 w-4 stroke-semantic-fg-secondary" />
+        </Link>
+      </div>
+      <div className="relative my-auto">
+        <Link
+          href="/pricing"
+          className="flex gap-x-2 text-[16px] font-semibold text-semantic-fg-secondary"
+        >
+          <p className="my-auto">For Business</p>
+          <Icons.ChevronDown className="my-auto h-4 w-4 stroke-semantic-fg-secondary" />
+        </Link>
+      </div>
+      <div className="relative my-auto">
+        <Link
+          href="/pricing"
+          className="flex gap-x-2 text-[16px] font-semibold text-semantic-fg-secondary"
+        >
+          <p className="my-auto">For Developers</p>
+          <Icons.ChevronDown className="my-auto h-4 w-4 stroke-semantic-fg-secondary" />
+        </Link>
+      </div>
+      <div className="relative my-auto">
+        <Link
+          href="/pricing"
+          className="flex gap-x-2 text-[16px] font-semibold text-semantic-fg-secondary"
+        >
+          <p className="my-auto">Company</p>
+          <Icons.ChevronDown className="my-auto h-4 w-4 stroke-semantic-fg-secondary" />
+        </Link>
+      </div>
+      <div className="relative my-auto">
+        <Link
+          href="/pricing"
+          className="flex gap-x-2 text-[16px] font-semibold text-semantic-fg-secondary"
+        >
+          <p className="my-auto">Pricing</p>
+        </Link>
+      </div>
+
+      <div className="relative my-auto ml-3">
+        <Button
+          variant="primary"
+          size="lg"
+          onClick={() =>
             router.push("https://instill.tech", undefined, { scroll: false })
           }
         >
-          <p className={cn("z-10 text-[16px] font-normal leading-7")}>
-            Sign Up
-          </p>
-        </SolidButton>
+          Sign Up
+        </Button>
       </div>
     </React.Fragment>
   );
@@ -79,14 +116,214 @@ export const Nav = () => {
   const desktopView = (
     <React.Fragment>
       {enableAnnouncementBar && announcementBar}
-      <div className="mx-auto flex w-full flex-row px-[30px] py-2.5">
-        <Link href="/" className="my-auto mr-auto flex">
-          <Logo variant="ColourLogomarkBlackType" width={158} />
-        </Link>
-
-        <div className="flex flex-row justify-end gap-x-[60px] lg:ml-auto">
-          {navbarLinkGroup}
-        </div>
+      <div className="mx-auto flex w-full flex-row px-[30px]">
+        <Menubar.Root className="w-full !rounded-none border-0 bg-transparent bg-opacity-80 !shadow-none backdrop-blur-md">
+          <Menubar.Menu>
+            <Link href="/" className="my-auto mr-auto flex">
+              <Logo variant="ColourLogomarkBlackType" width={158} />
+            </Link>
+          </Menubar.Menu>
+          <div className="flex w-full flex-row justify-end gap-x-4">
+            <Menubar.Menu>
+              <Menubar.Trigger>
+                <div className="relative my-auto">
+                  <div className="flex gap-x-2 text-[16px] font-semibold text-semantic-fg-secondary">
+                    <p className="my-auto">Products</p>
+                    <Icons.ChevronDown className="my-auto h-4 w-4 stroke-semantic-fg-secondary" />
+                  </div>
+                </div>
+              </Menubar.Trigger>
+              <Menubar.Content className="!rounded-sm" align="center">
+                <div className="flex h-[346px] w-[608px] flex-row">
+                  <div className="w-1/2 py-8 pl-8 pr-4">
+                    <p className="mb-4 font-sans text-[12px] font-semibold uppercase text-[#97A3B7]">
+                      Meet Instill
+                    </p>
+                    <p className="font-sans text-[16px] font-semibold text-[#111B29]">
+                      How it works
+                    </p>
+                  </div>
+                  <div className="py-3">
+                    <Separator orientation="vertical" className="mx-1" />
+                  </div>
+                  <div className="w-1/2 py-8 pl-4 pr-8">
+                    <p className="mb-4 font-sans text-[12px] font-semibold uppercase text-[#97A3B7]">
+                      Explore
+                    </p>
+                    <p className="mb-5 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Instill Hub
+                    </p>
+                    <p className="mb-0 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Pipeline Components
+                    </p>
+                    <p className="font-sans text-[14px] font-medium text-[#00000080]">
+                      Check the latest pipeline components
+                    </p>
+                  </div>
+                </div>
+              </Menubar.Content>
+            </Menubar.Menu>
+            <Menubar.Menu>
+              <Menubar.Trigger>
+                <div className="relative my-auto">
+                  <div className="flex gap-x-2 text-[16px] font-semibold text-semantic-fg-secondary">
+                    <p className="my-auto">For Business</p>
+                    <Icons.ChevronDown className="my-auto h-4 w-4 stroke-semantic-fg-secondary" />
+                  </div>
+                </div>
+              </Menubar.Trigger>
+              <Menubar.Content className="!rounded-sm" align="center">
+                <div className="flex h-[346px] w-[608px] flex-row">
+                  <div className="w-1/2 py-8 pl-8 pr-4">
+                    <p className="mb-4 font-sans text-[12px] font-semibold uppercase text-[#97A3B7]">
+                      Resources
+                    </p>
+                    <p className="mb-4 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Why Instill?
+                    </p>
+                    <p className="font-sans text-[16px] font-semibold text-[#111B29]">
+                      Deployment Options
+                    </p>
+                  </div>
+                  <div className="py-3">
+                    <Separator orientation="vertical" className="mx-1" />
+                  </div>
+                  <div className="w-1/2 py-8 pl-4 pr-8">
+                    <p className="mb-4 font-sans text-[12px] font-semibold uppercase text-[#97A3B7]">
+                      Explore
+                    </p>
+                    <p className="mb-5 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Instill Hub
+                    </p>
+                    <p className="mb-0 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Pipeline Components
+                    </p>
+                    <p className="font-sans text-[14px] font-medium text-[#00000080]">
+                      Check the latest pipeline components
+                    </p>
+                  </div>
+                </div>
+              </Menubar.Content>
+            </Menubar.Menu>
+            <Menubar.Menu>
+              <Menubar.Trigger>
+                <div className="relative my-auto">
+                  <div className="flex gap-x-2 text-[16px] font-semibold text-semantic-fg-secondary">
+                    <p className="my-auto">For Developers</p>
+                    <Icons.ChevronDown className="my-auto h-4 w-4 stroke-semantic-fg-secondary" />
+                  </div>
+                </div>
+              </Menubar.Trigger>
+              <Menubar.Content className="!rounded-sm" align="center">
+                <div className="flex h-[346px] w-[608px] flex-row">
+                  <div className="w-1/2 py-8 pl-8 pr-4">
+                    <p className="mb-4 font-sans text-[12px] font-semibold uppercase text-[#97A3B7]">
+                      Resources
+                    </p>
+                    <p className="mb-4 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Documentation
+                    </p>
+                    <p className="mb-4 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Changelog
+                    </p>
+                    <p className="mb-4 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Tutorials
+                    </p>
+                    <p className="mb-4 font-sans text-[16px] font-semibold text-[#111B29]">
+                      API Reference
+                    </p>
+                  </div>
+                  <div className="py-3">
+                    <Separator orientation="vertical" className="mx-1" />
+                  </div>
+                  <div className="w-1/2 py-8 pl-4 pr-8">
+                    <p className="mb-4 font-sans text-[12px] font-semibold uppercase text-[#97A3B7]">
+                      Community
+                    </p>
+                    <p className="mb-5 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Open Source
+                    </p>
+                    <p className="mb-0 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Discord
+                    </p>
+                    <p className="font-sans text-[14px] font-medium text-[#00000080]">
+                      Join our community
+                    </p>
+                  </div>
+                </div>
+              </Menubar.Content>
+            </Menubar.Menu>
+            <Menubar.Menu>
+              <Menubar.Trigger>
+                <div className="relative my-auto">
+                  <div className="flex gap-x-2 text-[16px] font-semibold text-semantic-fg-secondary">
+                    <p className="my-auto">Company</p>
+                    <Icons.ChevronDown className="my-auto h-4 w-4 stroke-semantic-fg-secondary" />
+                  </div>
+                </div>
+              </Menubar.Trigger>
+              <Menubar.Content className="!rounded-sm" align="center">
+                <div className="flex h-[346px] w-[256px] flex-row">
+                  <div className="w-1/2 py-8 pl-8 pr-4">
+                    <p className="mb-4 font-sans text-[12px] font-semibold uppercase text-[#97A3B7]">
+                      Company
+                    </p>
+                    <p className="mb-4 font-sans text-[16px] font-semibold text-[#111B29]">
+                      About
+                    </p>
+                    <p className="mb-4 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Career
+                    </p>
+                    <p className="mb-4 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Blog
+                    </p>
+                  </div>
+                </div>
+              </Menubar.Content>
+            </Menubar.Menu>
+            <Menubar.Menu>
+              <Menubar.Trigger>
+                <div className="relative my-auto">
+                  <Link
+                    href="/pricing"
+                    className="flex gap-x-2 text-[16px] font-semibold text-semantic-fg-secondary"
+                  >
+                    <p className="my-auto">Pricing</p>
+                  </Link>
+                </div>
+              </Menubar.Trigger>
+              <Menubar.Content className="!rounded-sm">
+                <div className="flex h-[346px] w-[608px] flex-row">
+                  <div className="w-1/2 py-8 pl-8 pr-4">
+                    <p className="mb-4 font-sans text-[12px] font-semibold uppercase text-[#97A3B7]">
+                      Meet Instill
+                    </p>
+                    <p className="font-sans text-[16px] font-semibold text-[#111B29]">
+                      How it works
+                    </p>
+                  </div>
+                  <div className="py-3">
+                    <Separator orientation="vertical" className="mx-1" />
+                  </div>
+                  <div className="w-1/2 py-8 pl-4 pr-8">
+                    <p className="mb-4 font-sans text-[12px] font-semibold uppercase text-[#97A3B7]">
+                      Explore
+                    </p>
+                    <p className="mb-5 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Instill Hub
+                    </p>
+                    <p className="mb-0 font-sans text-[16px] font-semibold text-[#111B29]">
+                      Pipeline Components
+                    </p>
+                    <p className="font-sans text-[14px] font-medium text-[#00000080]">
+                      Check the latest pipeline components
+                    </p>
+                  </div>
+                </div>
+              </Menubar.Content>
+            </Menubar.Menu>
+          </div>
+        </Menubar.Root>
       </div>
     </React.Fragment>
   );
@@ -132,7 +369,7 @@ export const Nav = () => {
   return (
     <div
       id="navbar-test"
-      className="flex w-full flex-col bg-transparent bg-opacity-80 backdrop-blur-sm lg:sticky lg:top-0 lg:z-50"
+      className="flex w-full flex-col bg-transparent bg-opacity-80 backdrop-blur-md lg:sticky lg:top-4 lg:z-50"
     >
       <div className="flex w-full flex-col">
         <div className="hidden w-full lg:flex lg:flex-col">{desktopView}</div>
