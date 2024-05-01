b13c9cf1b093fc75c8e8df6ed7a36cf41ead8841
iamnamananand996 <iamnamananand996@gmail.com>
2024-05-02 01:31:35 +0530

    feat: pricing mobile view

--- src/components/article/StartBuildingBlock.tsx
+++ src/components/article/StartBuildingBlock.tsx
@@ -9,7 +9,7 @@ export const StartBuildingBlock = () => {
       className="rounded-b-sm bg-cover bg-no-repeat pb-16"
     >
       <ContentContainer
-        margin="my-[120px] xl:my-[18px]"
+        margin="mt-[40px] xl:my-[18px]"
         contentMaxWidth="max-w-[1127px]"
       >
         <div className="flex flex-col gap-y-5 rounded-sm bg-[#FFFFFF99] p-7 text-center shadow-lg">
@@ -19,11 +19,11 @@ export const StartBuildingBlock = () => {
           <p className="font-mono text-[16px] leading-5">
             Create an account and start building with Instill
           </p>
-          <div className="space-x-2">
-            <Button variant="primary" size="lg">
+          <div className="flex flex-row justify-center space-x-2">
+            <Button variant="primary" size="lg" className="mb-2 xl:mb-0">
               Get Started
             </Button>
-            <Button variant="secondaryGrey" size="lg">
+            <Button variant="secondaryGrey" size="lg" className="mb-2 xl:mb-0">
               Contact Sales
             </Button>
           </div>
