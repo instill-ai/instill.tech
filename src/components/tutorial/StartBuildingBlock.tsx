c546855d65ca4d5c3e004db7af9cb53a84578a82
iamnamananand996 <iamnamananand996@gmail.com>
2024-05-27 19:24:05 +0530

    feat: initial commit

--- src/components/tutorial/StartBuildingBlock.tsx
+++ src/components/tutorial/StartBuildingBlock.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import { ContentContainer } from "../ui";
 import { Button, Logo } from "@instill-ai/design-system";
 
-export const StartBuildingBlock = () => {
+export const StartBuildingBlock = ({
+  contentMaxWidth,
+}: {
+  contentMaxWidth?: string;
+}) => {
   return (
     <div
       style={{ backgroundImage: 'url("/images/articles/glow.png")' }}
       className="rounded-b-sm bg-cover bg-no-repeat pb-16"
     >
-      <ContentContainer margin="my-[20px]" contentMaxWidth="max-w-[1127px]">
+      <ContentContainer
+        margin="my-[20px]"
+        contentMaxWidth={contentMaxWidth || "max-w-[1127px]"}
+      >
         <div className="flex flex-col gap-y-5 rounded-sm bg-[#FFFFFF99] p-7 text-center shadow-lg">
           <div className="flex justify-center">
             <Logo variant="colourLogomark" width={32} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,12 @@ import {
   SelfHost,
   Social,
 } from "@/components/landing";
-import { LandingPageBase, PageHead } from "@/components/ui";
+import { ContentContainer, LandingPageBase, PageHead } from "@/components/ui";
 import { GetStaticProps } from "next";
 import { getRepoFileContent } from "@/lib/github";
 import Slide from "@/components/landing/Slide";
+import { Button, Icons } from "@instill-ai/design-system";
+import { StartBuildingBlock } from "@/components/tutorial";
 
 const HowItWorks = dynamic<HowItWorksProps>(() =>
   import("@/components/landing").then((mod) => mod.HowItWorks)
@@ -66,36 +68,105 @@ const HomePage: FC & {
         jsonLd={null}
       />
 
-      <div className="flex flex-col">
-        <div className="mx-auto flex w-full max-w-[1327px] flex-col px-4 xl:px-0">
-          <Slide>
-            <Hero />
-          </Slide>
-          <Slide>
-            <Social />
-          </Slide>
-        </div>
+      <ContentContainer margin="mt-[20px]" contentMaxWidth="max-w-[1232px]">
+        <div className="my-10 flex flex-col space-y-8 bg-black bg-opacity-5 px-[150px] py-[160px] backdrop-blur-[10px]">
+          <div className="flex justify-center gap-x-3">
+            <Button
+              variant="secondaryGrey"
+              size="sm"
+              className="rounded-sm !border-none shadow"
+            >
+              What’s new?
+            </Button>
 
-        <div className="bg-instillGrey90">
-          <div className={cn("mx-auto max-w-[1127px] px-4 xl:px-0")}>
-            <BuildAppsInAction />
+            <Button
+              variant="secondaryGrey"
+              size="sm"
+              className="gap-x-2 rounded-sm !border-none shadow"
+            >
+              Check our new feature
+              <Icons.ArrowRight className="h-4 w-4 stroke-semantic-fg-primary" />
+            </Button>
+          </div>
+          <div>
+            <p className="text-center text-[66px] font-bold leading-[78px] tracking-[-1.32px]">
+              Orchestrate Data, Models and Pipelines for Generative AI
+            </p>
+          </div>
+          <div>
+            <p className="mb-8 text-center text-[22px] font-normal leading-[30px] tracking-[-0.352px] text-semantic-fg-secondary">
+              Instill AI empowers your team to spend less time building,
+              debuggin, deploying and managing infrastructure, and focus on your
+              AI use cases.
+            </p>
+          </div>
+
+          <div className="flex justify-center gap-x-4">
+            <Button variant="primary" size="lg">
+              Get started
+            </Button>
+
+            <Button variant="tertiaryColour" size="lg" className="gap-x-2">
+              Request a Demo{" "}
+              <Icons.ArrowRight className="h-4 w-4 stroke-semantic-accent-default" />
+            </Button>
           </div>
         </div>
 
-        <div className="mx-auto flex w-full max-w-[1127px] flex-col px-4 xl:px-0">
-          <HowItWorks />
-        </div>
+        <Slide>
+          <Social />
+        </Slide>
+      </ContentContainer>
 
-        <div className="bg-instillGrey90">
-          <div className={cn("mx-auto max-w-[1127px] px-4 xl:px-0")}>
-            <Community />
+      <div className="mb-6 bg-opacity-80 bg-cover bg-no-repeat backdrop-blur-sm">
+        <img
+          src="/images/landing/section-3-bg.svg"
+          alt=""
+          sizes=""
+          className="absolute top-[180px]"
+        />
+        <ContentContainer
+          margin="mt-[20px]"
+          contentMaxWidth="max-w-[1232px] bg-[#F7F7F9] rounded-sm"
+        >
+          <div
+            style={{
+              backgroundImage: 'url("/images/landing/section-hero.svg")',
+              borderRadius: "8px",
+            }}
+            className="h-[370px]"
+          >
           </div>
-        </div>
+          <div className="flex flex-col">
+            <Button
+              variant="secondaryGrey"
+              size="sm"
+              className="mx-auto mb-2 rounded-sm !border-none !bg-[#FFFFFF80] px-3 shadow-sm"
+            >
+              Everything you need
+            </Button>
+            <p className="mb-2 text-center font-sans text-[32px] font-semibold">
+              Connect LLMs to Data, Applications and APIs
+            </p>
+            <div className="flex justify-center">
+              <p className="w-[590px] text-center font-sans text-[16px] font-normal">
+                Lorem ipsum dolor sit amet consectetur. Proin nunc consequat
+                curabitur montes. Ultrices semper orci auctor viverra ipsum ut.
+                Dis malesuada amet pulvinar.
+              </p>
+            </div>
 
-        <div className="mx-auto flex w-full max-w-[1127px] flex-col px-4 xl:px-0">
-          <SelfHost />
-        </div>
+            <Button
+              variant="secondaryColour"
+              size="md"
+              className="mx-auto mb-8 mt-6"
+            >
+              See All Integrations
+            </Button>
+          </div>
+        </ContentContainer>
       </div>
+      <StartBuildingBlock contentMaxWidth="max-w-[1232px]" />
     </>
   );
 };
