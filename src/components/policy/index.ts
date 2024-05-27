78aab2d9a8317bf02bea28f404d7e3182c8387e5
Xiaofei Du <xiaofei.du@instill.tech>
2024-05-28 00:11:13 +0100

    chore: add security page

--- src/components/policy/index.ts
+++ src/components/policy/index.ts
@@ -1,2 +1,4 @@
 export { PolicyPageLayout } from "./PolicyPageLayout";
+export { SecurityPageLayout } from "./SecurityPageLayout";
 export type { PolicyPageLayoutProps } from "./PolicyPageLayout";
+export type { SecurityPageLayoutProps } from "./SecurityPageLayout";
