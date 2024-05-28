da0f9f485757ad9d113d51e191fa7b147ff87287
iamnamananand996 <iamnamananand996@gmail.com>
2024-05-28 22:49:54 +0530

    feat: landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import Slide from "@/components/landing/Slide";
 import { Button, Icons } from "@instill-ai/design-system";
 import { StartBuildingBlock } from "@/components/tutorial";
 import { NextPageWithLayout } from "./_app";
+import Video from "next-video";
+import getStarted from "../../videos/pipelines.mp4";
 
 export const getStaticProps: GetStaticProps = async () => {
   const destinationDefinitions = await getRepoFileContent(
@@ -95,6 +97,50 @@ const HomePage: NextPageWithLayout = () => {
         </Slide>
       </ContentContainer>
 
+      <ContentContainer
+        margin="mt-[20px]"
+        contentMaxWidth="max-w-[1232px] bg-[#F7F7F9] rounded-sm"
+      >
+        <div
+          className="bg-cover bg-no-repeat py-6"
+          style={{
+            backgroundImage: 'url("/images/landing/section-2-bg.svg")',
+            borderRadius: "12px",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="flex justify-center">
+            <Button
+              variant="secondaryGrey"
+              size="sm"
+              className="rounded-3 mx-auto mb-2 !border-none !bg-[#FFFFFF80] px-3 shadow-lg"
+            >
+              Everything you need
+            </Button>
+          </div>
+
+          <div className="flex flex-col">
+            <div className="flex justify-center">
+              <p className="font-sans text-[32px] font-semibold">
+                Accelerate the End-to-end LLM Pipeline Development
+              </p>
+            </div>
+            <div className="flex justify-center">
+              <div className="mt-2 w-[580px]">
+                <p className="text-center font-sans text-[16px] font-normal text-[#4A4340]">
+                  Instill Cloud help enterprise build AI applications from
+                  prototype into production with components that link private
+                  data to the power of LLM.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-6 flex justify-center">
+            <img src="/images/landing/section-2-img.svg" alt="" sizes="" />
+          </div>
+        </div>
+      </ContentContainer>
+
       <div className="mb-6 bg-opacity-80 bg-cover bg-no-repeat backdrop-blur-sm">
         <img
           src="/images/landing/section-3-bg.svg"
@@ -142,7 +188,115 @@ const HomePage: NextPageWithLayout = () => {
           </div>
         </ContentContainer>
       </div>
-      <StartBuildingBlock contentMaxWidth="max-w-[1232px]" />
+
+      <ContentContainer
+        margin="mt-[20px]"
+        contentMaxWidth="max-w-[1232px] bg-[#F7F7F9] rounded-sm"
+      >
+        <div
+          className="bg-cover bg-no-repeat py-6"
+          style={{
+            backgroundImage: 'url("/images/landing/section-4-bg.svg")',
+            borderRadius: "12px",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="flex justify-center">
+            <Button
+              variant="secondaryGrey"
+              size="sm"
+              className="rounded-3 mx-auto mb-2 !border-none !bg-[#FFFFFF80] px-3 shadow-lg"
+            >
+              Everything you need
+            </Button>
+          </div>
+
+          <div className="flex flex-col">
+            <div className="flex justify-center">
+              <p className="font-sans text-[32px] font-semibold">
+                Deploy or Fine-tune LLMs for your Pipelines
+              </p>
+            </div>
+            <div className="flex justify-center">
+              <div className="mt-2 w-[580px]">
+                <p className="text-center font-sans text-[16px] font-normal text-[#4A4340]">
+                  Lorem ipsum dolor sit amet consectetur. Proin nunc consequat
+                  curabitur montes. Ultrices semper orci auctor viverra ipsum
+                  ut. Dis malesuada amet pulvinar.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <img src="/images/landing/section-4-img.svg" alt="" sizes="" />
+          </div>
+
+          <div className="flex justify-center">
+            <Button variant="secondaryColour" size="md" className="mx-auto">
+              See Supported Models
+            </Button>
+          </div>
+        </div>
+      </ContentContainer>
+
+      <div
+        className="bg-cover bg-no-repeat"
+        style={{
+          backgroundImage: 'url("/images/landing/section-5-bg.svg")',
+          backgroundSize: 600,
+          backgroundPosition: "center bottom",
+        }}
+      >
+        <div
+          className="bg-cover bg-no-repeat py-6"
+          style={{
+            backgroundImage: 'url("/images/landing/section-5-group-1.svg")',
+            backgroundSize: 200,
+            backgroundPosition: "right top",
+          }}
+        >
+          <div
+            className="bg-no-repeat"
+            style={{
+              backgroundImage: 'url("/images/landing/section-5-bg-2.svg")',
+              backgroundPosition: "left",
+            }}
+          >
+            <ContentContainer
+              margin="mt-[20px]"
+              contentMaxWidth="max-w-[1230px]"
+            >
+              <div className="mb-6 flex justify-center">
+                <div className="w-[600px]">
+                  <p className="text-center font-sans text-[32px] font-semibold">
+                    Accelerate the end-to-end LLM Pipeline Building from
+                    Prototype to Production
+                  </p>
+                </div>
+              </div>
+              <div className="rounded-sm p-1 shadow-lg">
+                <Video
+                  src={getStarted}
+                  poster={"./images/landing/poster.png"}
+                  accentColor={"#316fed"}
+                  // primaryColor={"#316fed"}
+                  // secondaryColor={"#6194FA"}
+                >
+                  <track
+                    kind="captions"
+                    src="/get-started.vtt"
+                    srcLang="en"
+                    label="English"
+                    default
+                  />
+                </Video>
+              </div>
+            </ContentContainer>
+          </div>
+        </div>
+
+        <StartBuildingBlock contentMaxWidth="max-w-[1232px]" />
+      </div>
     </>
   );
 };
