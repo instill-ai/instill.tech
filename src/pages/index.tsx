259e8cb2234fd2006f11774fbab1742d0fcf6b5f
Naman Anand <iamnamananand996@gmail.com>
2024-05-30 02:12:29 +0530

    feat: mobile version landing page (#868)
    Because
    
    - mobile version landing page
    
    This commit
    
    - mobile version landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,8 +48,8 @@ const HomePage: NextPageWithLayout = () => {
       />
 
       <ContentContainer margin="mt-[20px]" contentMaxWidth="max-w-[1232px]">
-        <div className="my-10 flex flex-col space-y-8 bg-black bg-opacity-5 px-[150px] py-[160px] backdrop-blur-[10px]">
-          <div className="flex justify-center gap-x-3">
+        <div className="my-10 flex flex-col space-y-8 bg-black bg-opacity-5 py-[160px] backdrop-blur-[10px] xl:px-[150px]">
+          <div className="mx-auto flex justify-center gap-x-3 rounded-sm bg-white">
             <Button
               variant="secondaryGrey"
               size="sm"
@@ -68,12 +68,12 @@ const HomePage: NextPageWithLayout = () => {
             </Button>
           </div>
           <div>
-            <p className="text-center text-[66px] font-bold leading-[78px] tracking-[-1.32px]">
+            <p className="text-center text-[42px] font-bold leading-[45px] tracking-[-1.32px] xl:text-[66px] xl:leading-[78px]">
               Orchestrate Data, Models and Pipelines for Generative AI
             </p>
           </div>
           <div>
-            <p className="mb-8 text-center text-[22px] font-normal leading-[30px] tracking-[-0.352px] text-semantic-fg-secondary">
+            <p className="mb-8 text-center text-[16px] font-normal leading-6 tracking-[-0.352px] text-semantic-fg-secondary xl:text-[22px] xl:leading-[30px]">
               Instill AI empowers your team to spend less time building,
               debuggin, deploying and managing infrastructure, and focus on your
               AI use cases.
@@ -113,7 +113,7 @@ const HomePage: NextPageWithLayout = () => {
             <Button
               variant="secondaryGrey"
               size="sm"
-              className="rounded-3 mx-auto mb-2 !border-none !bg-[#FFFFFF80] px-3 shadow-lg"
+              className="rounded-3 mx-auto mb-4 !border-none !bg-[#FFFFFF80] px-3 shadow-lg"
             >
               Everything you need
             </Button>
@@ -121,13 +121,13 @@ const HomePage: NextPageWithLayout = () => {
 
           <div className="flex flex-col">
             <div className="flex justify-center">
-              <p className="font-sans text-[32px] font-semibold">
+              <p className="text-center font-sans text-[24px] font-semibold leading-[28px] xl:text-[32px]">
                 Accelerate the End-to-end LLM Pipeline Development
               </p>
             </div>
             <div className="flex justify-center">
               <div className="mt-2 w-[580px]">
-                <p className="text-center font-sans text-[16px] font-normal text-[#4A4340]">
+                <p className="text-center font-sans text-[14px] font-normal text-[#4A4340] xl:text-[16px]">
                   Instill Cloud help enterprise build AI applications from
                   prototype into production with components that link private
                   data to the power of LLM.
@@ -136,7 +136,18 @@ const HomePage: NextPageWithLayout = () => {
             </div>
           </div>
           <div className="mt-6 flex justify-center">
-            <img src="/images/landing/section-2-img.svg" alt="" sizes="" />
+            <img
+              src="/images/landing/section-2-img.svg"
+              alt=""
+              sizes=""
+              className="hidden xl:block"
+            />
+            <img
+              src="/images/landing/section-2-img-mobile.svg"
+              alt=""
+              sizes=""
+              className="block xl:hidden"
+            />
           </div>
         </div>
       </ContentContainer>
@@ -146,32 +157,41 @@ const HomePage: NextPageWithLayout = () => {
           src="/images/landing/section-3-bg.svg"
           alt=""
           sizes=""
-          className="absolute top-[180px]"
+          className="absolute top-[180px] hidden xl:block"
         />
         <ContentContainer
           margin="mt-[20px]"
-          contentMaxWidth="max-w-[1232px] bg-[#F7F7F9] rounded-sm"
+          contentMaxWidth="max-w-[1232px] bg-[#F7F7F9] rounded-sm px-0"
         >
           <div
             style={{
               backgroundImage: 'url("/images/landing/section-hero.svg")',
               borderRadius: "8px",
+              backgroundPosition: "center",
             }}
-            className="h-[370px]"
+            className="hidden h-[370px] xl:block"
+          ></div>
+          <div
+            style={{
+              backgroundImage: 'url("/images/landing/section-hero-mobile.svg")',
+              borderRadius: "8px",
+              backgroundPosition: "center",
+            }}
+            className="block h-[370px] bg-cover bg-no-repeat xl:hidden"
           ></div>
           <div className="flex flex-col">
             <Button
               variant="secondaryGrey"
               size="sm"
-              className="mx-auto mb-2 rounded-sm !border-none !bg-[#FFFFFF80] px-3 shadow-sm"
+              className="mx-auto mb-4 rounded-sm !border-none !bg-[#FFFFFF80] px-3 shadow-sm"
             >
               Everything you need
             </Button>
-            <p className="mb-2 text-center font-sans text-[32px] font-semibold">
+            <p className="mb-2 text-center font-sans text-[24px] font-semibold leading-[28px] xl:text-[32px]">
               Connect LLMs to Data, Applications and APIs
             </p>
             <div className="flex justify-center">
-              <p className="w-[590px] text-center font-sans text-[16px] font-normal">
+              <p className="w-[590px] text-center font-sans text-[14px] font-normal text-[#4A4340] xl:text-[16px]">
                 Lorem ipsum dolor sit amet consectetur. Proin nunc consequat
                 curabitur montes. Ultrices semper orci auctor viverra ipsum ut.
                 Dis malesuada amet pulvinar.
@@ -213,13 +233,13 @@ const HomePage: NextPageWithLayout = () => {
 
           <div className="flex flex-col">
             <div className="flex justify-center">
-              <p className="font-sans text-[32px] font-semibold">
+              <p className="text-center font-sans text-[24px] font-semibold leading-[28px] xl:text-[32px]">
                 Deploy or Fine-tune LLMs for your Pipelines
               </p>
             </div>
             <div className="flex justify-center">
               <div className="mt-2 w-[580px]">
-                <p className="text-center font-sans text-[16px] font-normal text-[#4A4340]">
+                <p className="text-center font-sans text-[14px] font-normal leading-6 text-[#4A4340] xl:text-[16px]">
                   Lorem ipsum dolor sit amet consectetur. Proin nunc consequat
                   curabitur montes. Ultrices semper orci auctor viverra ipsum
                   ut. Dis malesuada amet pulvinar.
@@ -228,7 +248,18 @@ const HomePage: NextPageWithLayout = () => {
             </div>
           </div>
           <div className="flex justify-center">
-            <img src="/images/landing/section-4-img.svg" alt="" sizes="" />
+            <img
+              src="/images/landing/section-4-img.svg"
+              alt=""
+              sizes=""
+              className="hidden xl:block"
+            />
+            <img
+              src="/images/landing/section-4-img-mobile.svg"
+              alt=""
+              sizes=""
+              className="block xl:hidden"
+            />
           </div>
 
           <div className="flex justify-center">
@@ -259,7 +290,7 @@ const HomePage: NextPageWithLayout = () => {
             className="bg-no-repeat"
             style={{
               backgroundImage: 'url("/images/landing/section-5-bg-2.svg")',
-              backgroundPosition: "left",
+              backgroundPosition: "top left",
             }}
           >
             <ContentContainer
@@ -268,7 +299,7 @@ const HomePage: NextPageWithLayout = () => {
             >
               <div className="mb-6 flex justify-center">
                 <div className="w-[600px]">
-                  <p className="text-center font-sans text-[32px] font-semibold">
+                  <p className="text-center font-sans text-[24px] font-semibold leading-[28px] xl:text-[32px]">
                     Accelerate the end-to-end LLM Pipeline Building from
                     Prototype to Production
                   </p>
